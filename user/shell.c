fdaa55d83be42cb7b006b5d9764353ab54e167f6
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-11-29 08:52:51 +0000

    some primitive string parsing
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@99 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/shell.c
+++ user/shell.c
@@ -1,6 +1,48 @@
 #include <inc/syscall.h>
 #include <inc/stdio.h>
 #include <inc/lib.h>
+#include <inc/string.h>
+
+#define MAXARGS	256
+static char *cmd_argv[MAXARGS];
+static int cmd_argc;
+
+static char separators[] = " \t\n\r";
+
+static void
+parse_cmd(char *cmd)
+{
+    char *arg_base = cmd;
+    char *c = cmd;
+
+    cmd_argc = 0;
+    for (;;) {
+	int eoc = 0;
+
+	if (*c == '\0')
+	    eoc = 1;
+
+	if (eoc || strchr(separators, *c)) {
+	    *c = '\0';
+	    if (arg_base != c)
+		cmd_argv[cmd_argc++] = arg_base;
+	    arg_base = c + 1;
+	}
+
+	if (eoc)
+	    break;
+	c++;
+    }
+}
+
+static void
+run_cmd(int ac, char **av)
+{
+    if (ac == 0)
+	return;
+
+    cprintf("cmd: %s\n", av[0]);
+}
 
 int
 main(int ac, char **av)
@@ -9,6 +51,7 @@ main(int ac, char **av)
 
     for (;;) {
 	char *cmd = readline("jos> ");
-	cprintf("cmd: %s\n", cmd);
+	parse_cmd(cmd);
+	run_cmd(cmd_argc, cmd_argv);
     }
 }
