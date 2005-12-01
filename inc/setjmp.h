054a382a37077a6f1a0d9ecaad45cbc87fd1bed8
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-01 19:08:57 +0000

    longjmp is noreturn
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@126 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/setjmp.h
+++ inc/setjmp.h
@@ -4,6 +4,6 @@
 #include <machine/setjmp.h>
 
 int  setjmp(struct jmp_buf *buf);
-void longjmp(struct jmp_buf *buf, uint64_t val);
+void longjmp(struct jmp_buf *buf, uint64_t val) __attribute__((__noreturn__));
 
 #endif
