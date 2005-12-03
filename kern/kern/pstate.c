628c029bb7e7bfff9ca504d71a1efd851c31e6c9
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-03 05:05:22 +0000

    clean up load/store state machines; zero out header before changing any other data, to ensure a valid header means valid data.
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@142 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/pstate.c
+++ kern/kern/pstate.c
@@ -5,22 +5,24 @@
 #include <kern/lib.h>
 
 static char pstate_buf[PGSIZE] __attribute__((__aligned__ (PGSIZE)));
-static volatile int pstate_done;
 
 static struct {
     struct pstate_header *hdr;
 
+    uint64_t disk_page;
     int map_slot;
 
     struct kobject *ko;
     int extra_page;
-} init_state;
+
+    int done;
+} state;
 
 static void
 init_done()
 {
-    handle_counter = init_state.hdr->ph_handle_counter;
-    pstate_done = 1;
+    handle_counter = state.hdr->ph_handle_counter;
+    state.done = 1;
 }
 
 static void init_kobj();
@@ -30,33 +32,33 @@ init_kobj_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, vo
 {
     if (stat != disk_io_success) {
 	cprintf("pstate_init_cb: disk IO failure\n");
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    if (init_state.extra_page == 0) {
-	init_state.ko = buf;
-	kobject_swapin(init_state.ko);
+    if (state.extra_page == 0) {
+	state.ko = buf;
+	kobject_swapin(state.ko);
     } else {
-	kobject_swapin_page(init_state.ko, init_state.extra_page - 1, buf);
+	kobject_swapin_page(state.ko, state.extra_page - 1, buf);
     }
 
-    if (init_state.extra_page < init_state.ko->ko_extra_pages) {
+    if (state.extra_page < state.ko->ko_extra_pages) {
 	void *p;
 	int r = page_alloc(&p);
 	if (r < 0) {
 	    cprintf("init_kobj_cb: cannot alloc page: %d\n", r);
-	    pstate_done = -1;
+	    state.done = -1;
 	    return;
 	}
 
-	uint64_t offset = init_state.extra_page * PGSIZE;
-	init_state.extra_page++;
+	uint64_t offset = state.extra_page * PGSIZE;
+	state.extra_page++;
 	disk_io(op_read, p, PGSIZE,
-		init_state.hdr->ph_map[init_state.map_slot].offset + offset,
+		state.hdr->ph_map[state.map_slot].offset + offset,
 		&init_kobj_cb, 0);
     } else {
-	init_state.map_slot++;
+	state.map_slot++;
 	init_kobj();
     }
 }
@@ -64,7 +66,7 @@ init_kobj_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, vo
 static void
 init_kobj()
 {
-    if (init_state.hdr->ph_map[init_state.map_slot].offset == 0) {
+    if (state.hdr->ph_map[state.map_slot].offset == 0) {
 	init_done();
 	return;
     }
@@ -73,12 +75,12 @@ init_kobj()
     int r = page_alloc(&p);
     if (r < 0) {
 	cprintf("init_kobj: cannot alloc page: %d\n", r);
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    init_state.extra_page = 0;
-    disk_io(op_read, p, PGSIZE, init_state.hdr->ph_map[init_state.map_slot].offset, &init_kobj_cb, 0);
+    state.extra_page = 0;
+    disk_io(op_read, p, PGSIZE, state.hdr->ph_map[state.map_slot].offset, &init_kobj_cb, 0);
 }
 
 static void
@@ -86,19 +88,19 @@ init_hdr_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, voi
 {
     if (stat != disk_io_success) {
 	cprintf("pstate_init_cb: disk IO failure\n");
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    init_state.hdr = (struct pstate_header *) buf;
-    if (init_state.hdr->ph_magic != PSTATE_MAGIC ||
-	init_state.hdr->ph_version != PSTATE_VERSION) {
+    state.hdr = (struct pstate_header *) buf;
+    if (state.hdr->ph_magic != PSTATE_MAGIC ||
+	state.hdr->ph_version != PSTATE_VERSION) {
 	cprintf("pstate_init_hdr: magic/version mismatch\n");
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    init_state.map_slot = 0;
+    state.map_slot = 0;
     init_kobj();
 }
 
@@ -109,39 +111,29 @@ pstate_init()
     if (1)
 	return -1;
 
-    pstate_done = 0;
+    state.done = 0;
     disk_io(op_read, &pstate_buf[0], PGSIZE, 0, &init_hdr_cb, 0);
-    while (!pstate_done)
+    while (!state.done)
 	ide_intr();
 
-    return pstate_done;
+    return state.done;
 }
 
 //////////////////////////////////////////////////
 // Swap-out code
 //////////////////////////////////////////////////
 
-static struct {
-    struct pstate_header *hdr;
-
-    uint64_t disk_page;
-    int map_slot;
-
-    struct kobject *ko;
-    int extra_page;
-} sync_state;
-
 static void
 sync_header_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, void *arg)
 {
     if (stat != disk_io_success) {
 	cprintf("sync_header_cb: io failure\n");
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    //cprintf("sync_header_cb: all done, %ld pages, %d objects\n", sync_state.disk_page, sync_state.map_slot);
-    pstate_done = 1;
+    //cprintf("sync_header_cb: all done, %ld pages, %d objects\n", state.disk_page, state.map_slot);
+    state.done = 1;
 }
 
 static void sync_kobj();
@@ -151,20 +143,20 @@ sync_kobj_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, vo
 {
     if (stat != disk_io_success) {
 	cprintf("sync_kobj_cb: disk IO failure\n");
-	pstate_done = -1;
+	state.done = -1;
 	return;
     }
 
-    if (sync_state.ko->ko_extra_pages > sync_state.extra_page) {
-	uint64_t offset = sync_state.disk_page * PGSIZE;
-	sync_state.disk_page++;
+    if (state.ko->ko_extra_pages > state.extra_page) {
+	uint64_t offset = state.disk_page * PGSIZE;
+	state.disk_page++;
 
-	void *p = kobject_swapout_page(sync_state.ko, sync_state.extra_page);
-	sync_state.extra_page++;
+	void *p = kobject_swapout_page(state.ko, state.extra_page);
+	state.extra_page++;
 
 	disk_io(op_write, p, PGSIZE, offset, sync_kobj_cb, 0);
     } else {
-	sync_state.ko = LIST_NEXT(sync_state.ko, ko_link);
+	state.ko = LIST_NEXT(state.ko, ko_link);
 	sync_kobj();
     }
 }
@@ -172,42 +164,55 @@ sync_kobj_cb(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, vo
 static void
 sync_kobj()
 {
-    if (sync_state.ko == 0) {
-	sync_state.hdr->ph_map[sync_state.map_slot].offset = 0;
-	disk_io(op_write, sync_state.hdr, PGSIZE, 0, sync_header_cb, 0);
+    if (state.ko == 0) {
+	state.hdr->ph_map[state.map_slot].offset = 0;
+	disk_io(op_write, state.hdr, PGSIZE, 0, sync_header_cb, 0);
     } else {
-	if (sync_state.map_slot > NUM_PH_OBJECTS) {
+	if (state.map_slot > NUM_PH_OBJECTS) {
 	    cprintf("sync_kobj: too many objects\n");
-	    pstate_done = -1;
+	    state.done = -1;
 	    return;
 	}
 
-	uint64_t offset = sync_state.disk_page * PGSIZE;
-	sync_state.hdr->ph_map[sync_state.map_slot].id = sync_state.ko->ko_id;
-	sync_state.hdr->ph_map[sync_state.map_slot].offset = offset;
+	uint64_t offset = state.disk_page * PGSIZE;
+	state.hdr->ph_map[state.map_slot].id = state.ko->ko_id;
+	state.hdr->ph_map[state.map_slot].offset = offset;
 
-	sync_state.map_slot++;
-	sync_state.disk_page++;
-	sync_state.extra_page = 0;
+	state.map_slot++;
+	state.disk_page++;
+	state.extra_page = 0;
 
-	disk_io(op_write, sync_state.ko, PGSIZE, offset, sync_kobj_cb, 0);
+	disk_io(op_write, state.ko, PGSIZE, offset, sync_kobj_cb, 0);
     }
+}
+
+static void
+sync_hdr_zero(disk_io_status stat, void *buf, uint32_t count, uint64_t offset, void *arg)
+{
+    if (stat != disk_io_success) {
+	cprintf("sync_hdr_inval: io error\n");
+	state.done = -1;
+	return;
+    }
+
+    state.hdr = (void *) &pstate_buf[0];
+    state.hdr->ph_magic = PSTATE_MAGIC;
+    state.hdr->ph_version = PSTATE_VERSION;
+    state.hdr->ph_handle_counter = handle_counter;
+
+    state.disk_page = 1;
+    state.map_slot = 0;
+    state.ko = LIST_FIRST(&ko_list);
+
+    state.done = 0;
+    sync_kobj();
 }
 
 void
 pstate_sync()
 {
-    sync_state.hdr = (void *) &pstate_buf[0];
-    sync_state.hdr->ph_magic = PSTATE_MAGIC;
-    sync_state.hdr->ph_version = PSTATE_VERSION;
-    sync_state.hdr->ph_handle_counter = handle_counter;
-
-    sync_state.disk_page = 1;
-    sync_state.map_slot = 0;
-    sync_state.ko = LIST_FIRST(&ko_list);
-
-    pstate_done = 0;
-    sync_kobj();
-    while (!pstate_done)
+    memset(&pstate_buf[0], 0, PGSIZE);
+    disk_io(op_write, &pstate_buf[0], PGSIZE, 0, sync_hdr_zero, 0);
+    while (!state.done)
 	ide_intr();
 }
