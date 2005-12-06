fac5a04483c25d9ada1817da36dfb74652615579
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-06 05:43:53 +0000

    a non-cow segment_copy
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@186 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/segment.c
+++ kern/kern/segment.c
@@ -18,6 +18,34 @@ segment_alloc(struct Label *l, struct Segment **sgp)
 }
 
 int
+segment_copy(struct Segment *src, struct Label *newl, struct Segment **dstp)
+{
+    struct Segment *dst;
+    int r = segment_alloc(newl, &dst);
+    if (r < 0)
+	return r;
+
+    r = segment_set_npages(dst, src->sg_ko.ko_npages);
+    if (r < 0)
+	return r;
+
+    for (int i = 0; i < src->sg_ko.ko_npages; i++) {
+	void *srcpg, *dstpg;
+	r = kobject_get_page(&src->sg_ko, i, &srcpg);
+	if (r < 0)
+	    return r;
+	r = kobject_get_page(&dst->sg_ko, i, &dstpg);
+	if (r < 0)
+	    return r;
+
+	memcpy(dstpg, srcpg, PGSIZE);
+    }
+
+    *dstp = dst;
+    return 0;
+}
+
+int
 segment_set_npages(struct Segment *sg, uint64_t num_pages)
 {
     return kobject_set_npages(&sg->sg_ko, num_pages);
