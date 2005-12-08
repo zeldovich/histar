8219524c24594579f3c4b2e0e0afc221e356de21
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-08 10:19:35 +0000

    sufficient non-glue to make lwip pass compilation
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@235 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ lib/lwip/jos64/arch/cc.h
@@ -0,0 +1,31 @@
+#ifndef LWIP_ARCH_CC_H
+#define LWIP_ARCH_CC_H
+
+#include <inc/types.h>
+#include <inc/string.h>
+#include <inc/assert.h>
+#include <inc/endian.h>
+
+typedef uint32_t u32_t;
+typedef int32_t s32_t;
+
+typedef uint16_t u16_t;
+typedef int16_t s16_t;
+
+typedef uint8_t u8_t;
+typedef int8_t s8_t;
+
+typedef uintptr_t mem_ptr_t;
+
+#define PACK_STRUCT_FIELD(x)	x
+#define PACK_STRUCT_STRUCT
+#define PACK_STRUCT_BEGIN
+#define PACK_STRUCT_END
+
+#define LWIP_PLATFORM_DIAG(x)	cprintf(x)
+#define LWIP_PLATFORM_ASSERT(x)	panic(x)
+
+// Our runtime is so incompetent..
+#define LWIP_PROVIDE_ERRNO
+
+#endif
---
+++ lib/lwip/jos64/arch/sys_arch.h
@@ -0,0 +1,13 @@
+#ifndef LWIP_ARCH_SYS_ARCH_H
+#define LWIP_ARCH_SYS_ARCH_H
+
+#include <inc/types.h>
+
+typedef	void* sys_sem_t;
+typedef void* sys_mbox_t;
+typedef void* sys_thread_t;
+
+#define SYS_MBOX_NULL	0
+#define SYS_SEM_NULL	0
+
+#endif
