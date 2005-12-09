6ca31c2d38856f24d3eacf35dcfcb18938cfde81
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-09 02:02:35 +0000

    give programs two pages of stack (so, everyone except shell + idle)
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@251 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/elf.c
+++ lib/elf.c
@@ -70,8 +70,9 @@ elf_load(uint64_t container, struct cobj_ref seg, struct thread_entry *e)
 	return r;
     }
 
+    int stackpages = 2;
     struct cobj_ref stack;
-    r = segment_alloc(container, PGSIZE, &stack);
+    r = segment_alloc(container, stackpages * PGSIZE, &stack);
     if (r < 0) {
 	cprintf("elf_load: cannot create stack segment: %s\n", e2s(r));
 	return r;
@@ -81,9 +82,9 @@ elf_load(uint64_t container, struct cobj_ref seg, struct thread_entry *e)
     e->te_stack = stacktop;
     segmap->sm_ent[si].segment = stack;
     segmap->sm_ent[si].start_page = 0;
-    segmap->sm_ent[si].num_pages = 1;
+    segmap->sm_ent[si].num_pages = stackpages;
     segmap->sm_ent[si].writable = 1;
-    segmap->sm_ent[si].va = stacktop - PGSIZE;
+    segmap->sm_ent[si].va = stacktop - stackpages * PGSIZE;
 
     return 0;
 }
