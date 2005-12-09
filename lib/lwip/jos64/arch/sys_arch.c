35abe6bf33e27ae34317af2ff69739781d642879
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-09 04:31:18 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@260 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/lwip/jos64/arch/sys_arch.c
+++ lib/lwip/jos64/arch/sys_arch.c
@@ -11,20 +11,20 @@ sys_init()
 sys_mbox_t
 sys_mbox_new()
 {
-    //cprintf("lwiparch: sys_mbox_new\n");
+    cprintf("lwiparch: sys_mbox_new\n");
     return 0;
 }
 
 void
 sys_mbox_free(sys_mbox_t mbox)
 {
-    //cprintf("lwiparch: sys_mbox_free\n");
+    cprintf("lwiparch: sys_mbox_free\n");
 }
 
 void
 sys_mbox_post(sys_mbox_t mbox, void *msg)
 {
-    //cprintf("lwiparch: sys_mbox_post\n");
+    cprintf("lwiparch: sys_mbox_post\n");
 }
 
 sys_sem_t
@@ -56,20 +56,20 @@ sys_arch_sem_wait(sys_sem_t sem, u32_t tm_msec)
 u32_t
 sys_arch_mbox_fetch(sys_mbox_t mbox, void **msg, u32_t tm_msec)
 {
-    //cprintf("lwiparch: sys_arch_mbox_fetch\n");
+    cprintf("lwiparch: sys_arch_mbox_fetch\n");
     return SYS_ARCH_TIMEOUT;
 }
 
 sys_thread_t
 sys_thread_new(void (* thread)(void *arg), void *arg, int prio)
 {
-    //cprintf("lwiparch: sys_thread_new\n");
+    cprintf("lwiparch: sys_thread_new\n");
     return 0;
 }
 
 struct sys_timeouts *
 sys_arch_timeouts(void)
 {
-    //cprintf("lwiparch: sys_arch_timeouts\n");
-    return 0;
+    static struct sys_timeouts tmo;
+    return &tmo;
 }
