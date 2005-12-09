6a1db12c56bc35c6dead6735a397f7ff8d500f1c
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2005-12-09 01:56:24 +0000

    embed netd
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@250 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/uinit.c
+++ kern/kern/uinit.c
@@ -252,7 +252,7 @@ user_bootstrap()
     EMBED_DECLARE(uregtest);
     EMBED_DECLARE(thread_test);
     EMBED_DECLARE(netwatch);
-    //EMBED_DECLARE(netd);
+    EMBED_DECLARE(netd);
     EMBED_DECLARE(shell);
 
     // root handle and a label
