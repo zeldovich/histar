7f24350068cc561839d58e48eeded07dd72e378d
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-01-25 21:29:51 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@407 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/stack.h
+++ inc/stack.h
@@ -1,5 +1,5 @@
-#ifndef _JOS_INC_STACK_H
-#define _JOS_INC_STACK_H
+#ifndef JOS_INC_STACK_H
+#define JOS_INC_STACK_H
 
 void stack_switch(uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4,
 		  void *stacktop, void *fn) __attribute__((noreturn));
