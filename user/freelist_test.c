97ee64a4e1e799fad9b21ed3f1e5afbdf1872c04
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-01-29 19:10:57 +0000

    can pass 0 as the va-pointer, in which case the segment won't be mapped into our address space (and we don't need it to be)
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@474 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/freelist_test.c
+++ user/freelist_test.c
@@ -18,26 +18,21 @@ test0()
 {
     int r ;
     uint64_t ct = start_env->container;
-    void *va = 0;
     
 	int i = 0 ;
-	for (; i < 10 ; i++) {
-		va = 0 ;
-		if ((r = segment_alloc(ct, PGSIZE, &segs[i], &va)) < 0)
+	for (; i < 100 ; i++)
+		if ((r = segment_alloc(ct, PGSIZE, &segs[i], 0)) < 0)
 			panic("error test0 %s", e2s(r)) ;
-	}
 
-	for (i = 0 ; i < 10 ; i+=2)
+	for (i = 0 ; i < 100 ; i+=2)
 		if ((r = sys_obj_unref(segs[i])) < 0)
 			panic("error test0 %s\n", e2s(r)) ;
 
-	for (i = 0 ; i < 10 ; i+=2) {
-		va = 0 ;
-		if ((r = segment_alloc(ct, PGSIZE, &segs[i], &va)) < 0) 
+	for (i = 0 ; i < 100 ; i+=2)
+		if ((r = segment_alloc(ct, PGSIZE, &segs[i], 0)) < 0) 
 			panic("error test0 %s\n", e2s(r)) ;
-	}
 	
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 100 ; i++)
 		if ((r = sys_obj_unref(segs[i])) < 0)
 			panic("error test0 %s\n", e2s(r)) ;
 }
