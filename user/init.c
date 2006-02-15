0fe1fb4485bdae792bd74aa6c29b30a117ce669e
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-02-15 01:16:56 +0000

    restructure code, create a scratch directory /x
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@680 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/init.c
+++ user/init.c
@@ -30,16 +30,9 @@ spawn_fs(int fd, const char *pn, int drop_root_handle)
     label_free(l);
 }
 
-int
-main(int ac, char **av)
+static void
+init_env(uint64_t c_root, uint64_t c_self)
 {
-    uint64_t c_self = start_arg0;
-    uint64_t c_root = start_arg1;
-
-    assert(0 == opencons(c_self));
-    assert(1 == dup(0, 1));
-    assert(2 == dup(0, 2));
-
     start_env = 0;
     struct cobj_ref sa;
     assert(0 == segment_alloc(c_self, PGSIZE, &sa, (void**) &start_env,
@@ -47,11 +40,14 @@ main(int ac, char **av)
 
     start_env->container = c_self;
     start_env->root_container = c_root;
+    start_env->parent_container = c_root;
 
+    // set the filesystem root to be the same as the container root
     int r = fs_get_root(c_root, &start_env->fs_root);
     if (r < 0)
 	panic("fs_get_root: %s", e2s(r));
 
+    // mount binaries on /bin
     int64_t fs_bin_id = container_find(c_root, kobj_container, "fs root");
     if (fs_bin_id < 0)
 	panic("cannot find fs /bin directory: %s", e2s(fs_bin_id));
@@ -65,9 +61,19 @@ main(int ac, char **av)
     if (r < 0)
 	panic("fs_mount for /bin: %s", e2s(r));
 
-    start_env->fs_cwd = start_env->fs_root;
-    printf("JOS: init (root container %ld)\n", c_root);
+    // create a scratch container
+    struct fs_inode scratch;
+    r = fs_mkdir(start_env->fs_root, "x", &scratch);
+    if (r < 0)
+	panic("creating /x: %s", e2s(r));
 
+    // start out in the root directory
+    start_env->fs_cwd = start_env->fs_root;
+}
+
+static int
+init_console()
+{
     // Now that we're set up a reasonable environment,
     // create a shared console fd.
     struct ulabel *label = label_get_current();
@@ -81,6 +87,12 @@ main(int ac, char **av)
     assert(cons >= 0);
     segment_set_default_label(0);
 
+    return cons;
+}
+
+static void
+init_procs(int cons)
+{
     // netd_mom should be the only process that needs our root handle at *,
     // in order to create an appropriately-labeled netdev object.
     spawn_fs(cons, "/bin/netd_mom", 0);
@@ -91,7 +103,24 @@ main(int ac, char **av)
     //spawn_fs(cons, "/bin/telnetd", 1);
     //spawn_fs(cons, "/bin/freelist_test", 1);
     spawn_fs(cons, "/bin/httpd", 1);
+}
+
+int
+main(int ac, char **av)
+{
+    uint64_t c_self = start_arg0;
+    uint64_t c_root = start_arg1;
+
+    assert(0 == opencons(c_self));
+    assert(1 == dup(0, 1));
+    assert(2 == dup(0, 2));
+
+    printf("JOS: init (root container %ld)\n", c_root);
+
+    init_env(c_root, c_self);
+    int cons = init_console();
+    init_procs(cons);
 
     for (;;)
-	sys_thread_sleep(1000);
+	sys_thread_sleep(100000);
 }
