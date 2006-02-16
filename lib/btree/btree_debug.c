c9e6f7d253f76eba2b70df38e6f9d657a55eda8e
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-02-16 07:34:43 +0000

    changed to use btree_node_destroy properly
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@737 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/btree/btree_debug.c
+++ lib/btree/btree_debug.c
@@ -3,19 +3,18 @@
 #include <inc/assert.h>
 
 static void
-btree_leaf_count1(struct btree *tree, struct btree_node *root, uint64_t *count)
+btree_leaf_count1(struct btree *tree, offset_t root, uint64_t *count)
 {
-	struct btree_node *node ;
+	struct btree_node *root_node = btree_read_node(tree, root);
 	
-	if (BTREE_IS_LEAF(root))
+	if (BTREE_IS_LEAF(root_node))
 		*count = *count + 1 ;
 	else {
-		for (int i = 0 ; i <= root->keyCount ; i++) {
-			node = btree_read_node(tree, root->children[i]) ;
-			btree_leaf_count1(tree, node, count) ;
-			//btree_unpin_node(tree, node) ;
+		for (int i = 0 ; i <= root_node->keyCount ; i++) {
+			btree_leaf_count1(tree, root_node->children[i], count) ;
 		}
 	}
+	btree_destroy_node(root_node) ;
 }
 
 static uint64_t
@@ -26,14 +25,12 @@ btree_leaf_count2(struct btree *tree)
 	struct btree_node *node ;
 	
 	while (next_off) {
-		//cprintf("next_off %ld\n", next_off) ;
 		node = btree_read_node(tree, next_off) ;
 		count++ ;
-		//next_off = node->children[node->keyCount] ;
 		next_off = *btree_value(node->children, 
 											  node->keyCount, 
 											  tree->s_value);
-		//btree_unpin_node(tree, node) ;
+		btree_destroy_node(node) ;
 	}
 	
 	return count ;
@@ -52,29 +49,48 @@ btree_size_calc(struct btree *tree)
 		next_off = *btree_value(node->children, 
 											  node->keyCount, 
 											  tree->s_value);
-		//btree_unpin_node(tree, node) ;
+		btree_destroy_node(node) ;
 	}
 	
 	return size ;	
+}
+
+static void
+btree_integrity_check(struct btree *tree, offset_t root)
+{
+	struct btree_node *root_node = btree_read_node(tree, root) ;
+	
+	if (root_node->keyCount == 0)
+		panic("node %ld: keyCount is 0", root) ;
+	
+	if (!BTREE_IS_LEAF(root_node)) {
+		for (int i = 0 ; i <= root_node->keyCount ; i++) {
+			if (root_node->children[i] == 0) 
+				panic("node %ld: child is 0 when it shouldn't be", root) ;	
+		}
+	}
+	
+	btree_destroy_node(root_node) ;
 }
 
 void
 btree_sanity_check(struct btree *tree)
 {
 	uint64_t count1 = 0 ;
-	if (tree->root)
-		btree_leaf_count1(tree, btree_read_node(tree, tree->root), &count1) ;
+	if (tree->root) 
+		btree_leaf_count1(tree, tree->root, &count1) ;
 
 	uint64_t count2 = btree_leaf_count2(tree) ;
 	
 	if (count1 != count2) 
 		panic("btree_sanity_check: count mismatch: %ld %ld\n", count1, count2) ;
 	
-	cprintf("count1 %ld count2 %ld\n", count1, count2) ;
-	
 	uint64_t size1 = tree->size ;
 	uint64_t size2 = btree_size_calc(tree) ;
 	
 	if (size1 != size2)
 		panic("btree_sanity_check: size mismatch: %ld %ld\n", size1, size2) ;
+	
+	if (tree->root) 
+		btree_integrity_check(tree, tree->root) ;
 }
