600e936a4d7f5fd54b0de8b0e01150aaaca3e36d
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-02-17 01:09:16 +0000

    tune a bunch of lwip knobs for less sucky performance
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@752 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/lwip/jos64/lwipopts.h
+++ lib/lwip/jos64/lwipopts.h
@@ -3,5 +3,27 @@
 
 #define LWIP_DHCP		1
 #define LWIP_COMPAT_SOCKETS	0
+#define LWIP_STATS_DISPLAY	1
+
+// Various tuning knobs, partly from jos32's lwipopts.h
+#define MEM_ALIGNMENT		4
+
+#define MEMP_SANITY_CHECK	1
+#define MEMP_NUM_PBUF		64
+#define MEMP_NUM_UDP_PCB	8
+#define MEMP_NUM_TCP_PCB	18
+#define MEMP_NUM_TCP_PCB_LISTEN	16
+#define MEMP_NUM_TCP_SEG	36
+
+#define PER_TCP_PCB_BUFFER      (16 * 4096)
+#define MEM_SIZE		(PER_TCP_PCB_BUFFER*MEMP_NUM_TCP_SEG + 4096*MEMP_NUM_TCP_SEG)
+
+#define PBUF_POOL_SIZE		256
+#define PBUF_POOL_BUFSIZE	2000
+
+#define TCP_MSS			1440
+#define TCP_SND_BUF		2880
+#define TCP_SND_QUEUELEN	(8 * TCP_SND_BUF/TCP_MSS)
+#define TCP_WND			(32*TCP_MSS)
 
 #endif
