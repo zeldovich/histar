eec5f12939c368c21d37ee070ffbbf6d9f6710d2
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-02-23 20:48:47 +0000

    further reduce stack use
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@851 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/libmain.c
+++ lib/libmain.c
@@ -11,57 +11,59 @@ start_env_t *start_env;
 
 #define MAXARGS	16
 
-static int argc = 0;
+static int argc;
 static const char *argv[MAXARGS];
+
+static void
+__attribute__((noinline))
+setup_env(uint64_t envaddr)
+{
+    // This process has enough of an environment,
+    // unlike a bootstrap process.
+    start_env = (start_env_t *) envaddr;
+
+    const char *p = &start_env->args[0];
+    for (int i = 0; i < MAXARGS; i++) {
+	size_t len = strlen(p);
+	if (len == 0)
+	    break;
+
+	argv[argc] = p;
+	p += len + 1;
+	argc++;
+    }
+
+    struct ulabel *l = label_get_current();
+    assert(l);
+    segment_set_default_label(l);
+
+    struct cobj_ref start_env_seg;
+    int r = segment_lookup(start_env, &start_env_seg, 0);
+    if (r < 0 || r == 0)
+	panic("libmain: cannot find start_env segment: %s", e2s(r));
+
+    void *start_env_ro = (void *) USTARTENVRO;
+    r = segment_map(start_env_seg, SEGMAP_READ, &start_env_ro, 0);
+    if (r < 0)
+	panic("libmain: cannot map start_env_ro: %s", e2s(r));
+
+    int64_t id = sys_mlt_create(start_env->container, "dynamic taint");
+    if (id < 0)
+	panic("libmain: cannot create dynamic taint MLT: %s", e2s(id));
+
+    start_env->taint_mlt = COBJ(start_env->container, id);
+}
 
 void
 libmain(uint64_t arg0, uint64_t arg1)
 {
-    memset(&argv, 0, sizeof(argv));
-
     start_arg0 = arg0;
     start_arg1 = arg1;
 
-    if (start_arg1 == 0) {
-	// This process has enough of an environment,
-	// unlike a bootstrap process.
-	start_env = (start_env_t *) start_arg0;
+    if (start_arg1 == 0)
+	setup_env(start_arg0);
 
-	const char *p = &start_env->args[0];
-	for (int i = 0; i < MAXARGS; i++) {
-	    size_t len = strlen(p);
-	    if (len == 0)
-		break;
-
-	    argv[argc] = p;
-	    p += len + 1;
-	    argc++;
-	}
-
-	struct ulabel *l = label_get_current();
-	assert(l);
-	segment_set_default_label(l);
-
-	struct cobj_ref start_env_seg;
-	int r = segment_lookup(start_env, &start_env_seg, 0);
-	if (r < 0 || r == 0)
-	    panic("libmain: cannot find start_env segment: %s", e2s(r));
-
-	void *start_env_ro = (void *) USTARTENVRO;
-	r = segment_map(start_env_seg, SEGMAP_READ, &start_env_ro, 0);
-	if (r < 0)
-	    panic("libmain: cannot map start_env_ro: %s", e2s(r));
-
-	int64_t id = sys_mlt_create(start_env->container, "dynamic taint");
-	if (id < 0)
-	    panic("libmain: cannot create dynamic taint MLT: %s", e2s(id));
-
-	start_env->taint_mlt = COBJ(start_env->container, id);
-    }
-
-    int r = main(argc, &argv[0]);
-
-    exit(r);
+    exit(main(argc, &argv[0]));
 }
 
 void
