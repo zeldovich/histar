86e2422b795e28830c49785f88e19685b1511a44
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-01 19:14:38 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@968 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/prof.c
+++ kern/kern/prof.c
@@ -1,4 +1,5 @@
 #include <machine/trapcodes.h>
+#include <lib/hashtable.h>
 #include <kern/prof.h>
 #include <kern/timer.h>
 #include <inc/string.h>
@@ -17,16 +18,54 @@ struct entry user_table[1];
 static struct periodic_task timer ;
 static int prof_print_enable = 0;
 
-void 
+static struct periodic_task timer2 ;
+
+// for profiling using gcc's -finstrument-functions
+static struct history
+{
+#define HISTORY_SIZE PGSIZE
+        void *func_enter[HISTORY_SIZE] ;
+        int enter_size ;
+
+} cyg_history ;
+
+#define NUM_SYMS 800
+static struct
+{
+        int count[NUM_SYMS] ;
+        int size ;     
+} cyg_data ;
+
+static int cyg_prof_print_enable = 0 ;
+static int cyg_prof_enable = 0 ;
+
+
+static struct hashentry back[NUM_SYMS] ;
+static struct hashtable cyg_lookup ;
+
+
+
+void __attribute__((no_instrument_function))
 prof_init(void) 
 {
 	memset(sysc_table, 0, sizeof(sysc_table)) ;
 	memset(trap_table, 0, sizeof(trap_table)) ;
 	
+        hash_init(&cyg_lookup, back, NUM_SYMS) ;
+        memset(&cyg_data, 0, sizeof(cyg_data)) ;
+        
+        
 	timer.pt_fn = &prof_print ;
 	timer.pt_interval_ticks = kclock_hz * 10;
 	if (prof_print_enable)
-		timer_add_periodic(&timer) ;		
+		timer_add_periodic(&timer) ;
+
+        timer2.pt_fn = &cyg_profile_print ;
+        timer2.pt_interval_ticks = kclock_hz * 10 ;
+        if (cyg_prof_print_enable)
+                timer_add_periodic(&timer2) ;	
+                
+        cyg_prof_enable = 1 ;
 }
 
 void 
@@ -87,4 +126,78 @@ prof_print(void)
 	print_entry(&user_table[0], 0, "user");
 
 	prof_reset();
+}
+
+// XXX
+// This doesn't work properly, due to multiple threads and 
+// functions that may not return...
+static void __attribute__((no_instrument_function))
+cyg_profile_kludge(void *func_addr)
+{
+        uint64_t func = (uint64_t) func_addr ;
+        uint64_t val ;
+        
+        cyg_prof_enable = 0 ;
+        if (hash_get(&cyg_lookup, func, &val) < 0) {
+                val = cyg_data.size++ ;
+                if (hash_put(&cyg_lookup, func, val) < 0)
+                        return ;
+        }
+        cyg_prof_enable = 1 ;
+        
+        cyg_data.count[val]++ ;
+        return ;
+}
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_enter(void *this_fn, void *call_site)
+{
+        if (!cyg_prof_enable)
+                return ;
+        
+        if (!(cyg_history.enter_size < HISTORY_SIZE)) {
+                cyg_prof_enable = 0 ;
+                cprintf("__cyg_profile_func_enter: no more space\n") ;
+                cyg_prof_enable = 1 ;
+                return ; 
+        }
+        
+        cyg_history.func_enter[cyg_history.enter_size] = this_fn ;
+        cyg_history.enter_size++ ;
+}        
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_exit(void *this_fn, void *call_site)
+{
+        if (!cyg_prof_enable)
+                return ;
+
+        // XXX
+        if (cyg_history.enter_size == 0)
+                return ;
+        else if (cyg_history.func_enter[cyg_history.enter_size - 1] != this_fn) {
+                cyg_history.enter_size = 0 ;
+                return ;
+        }
+                   
+        cyg_history.enter_size-- ;
+        cyg_profile_kludge(this_fn) ;
+}
+
+// for kobject_alloc lookup
+#include <kern/kobj.h>
+
+void __attribute__((no_instrument_function))
+cyg_profile_print(void) 
+{
+        cyg_prof_enable = 0 ;
+        
+        uint64_t val ;
+        if (hash_get(&cyg_lookup, (uint64_t)&kobject_alloc, &val) < 0) {
+                cprintf("no hashtable mapping\n") ;
+                cyg_prof_enable = 1 ;
+                return ;
+        }
+        cprintf("cyg_data.count[val] %d\n", cyg_data.count[val]) ;
+        cyg_prof_enable = 1 ;
 }
--- kern/kern/prof.h
+++ kern/kern/prof.h
@@ -10,4 +10,9 @@ void prof_trap(int num, uint64_t time) ;
 void prof_user(uint64_t time) ;
 void prof_print(void) ;
 
+void __cyg_profile_func_enter(void *this_fn, void *call_site) ;
+void __cyg_profile_func_exit(void *this_fn, void *call_site) ;
+void cyg_profile_print(void) ;
+
+
 #endif /*PROF_H_*/
