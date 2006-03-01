27279d2c12361091649b15cbbc9617ef90c72a24
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-01 23:08:56 +0000

    add a skeleton admind
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@982 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ user/admind.cc
@@ -0,0 +1,58 @@
+#include <inc/gatesrv.hh>
+
+static void
+admind_dispatch(struct cobj_ref *parm)
+{
+    printf("admind_dispatch\n");
+}
+
+static void __attribute__((noreturn))
+admind_entry(void *x, struct cobj_ref parm, gatesrv_return *r)
+{
+    try {
+	admind_dispatch(&parm);
+    } catch (std::exception &e) {
+	printf("admind_entry: %s\n", e.what());
+    }
+
+    try {
+	label *cs = new label(LB_LEVEL_STAR);
+	label *ds = new label(3);
+	label *dr = new label(0);
+	r->ret(parm, cs, ds, dr);
+    } catch (std::exception &e) {
+	printf("admind_entry: ret: %s\n", e.what());
+    }
+
+    thread_halt();
+}
+
+int
+main(int ac, char **av)
+try
+{
+    if (ac != 2) {
+	printf("Usage: %s adm-handle\n", av[0]);
+	exit(-1);
+    }
+
+    uint64_t adm_handle;
+    error_check(strtoull(av[1], 0, 10, &adm_handle));
+
+    label send(LB_LEVEL_STAR);
+    label recv(1);
+
+    // for now, allow anyone to make admin calls.
+    // the sticking point is how to tell the shell that you want to pass
+    // { adm_handle:* } when running admctl, but not for other things..
+    //recv.set(adm_handle, 0);
+
+    gatesrv g(start_env->root_container, "admgate", &send, &recv);
+    g.set_entry_container(start_env->container);
+    g.set_entry_function(&admind_entry, 0);
+    g.enable();
+
+    thread_halt();
+} catch (std::exception &e) {
+    printf("admind: %s\n", e.what());
+}
--- user/init.cc
+++ user/init.cc
@@ -10,22 +10,24 @@ extern "C" {
 }
 
 #include <inc/cpplabel.hh>
+#include <inc/error.hh>
 
 static void
-spawn_fs(int fd, const char *pn, label *tl)
+spawn_fs(int fd, const char *pn, const char *arg, label *tl)
 {
     struct fs_inode ino;
     int r = fs_namei(pn, &ino);
     if (r < 0)
-	panic("cannot fs_lookup %s: %s\n", pn, e2s(r));
+	throw error(r, "cannot fs_lookup %s", pn);
 
-    const char *argv[] = { pn };
+    const char *argv[] = { pn, arg };
     int64_t ct = spawn(start_env->root_container, ino,
-		       fd, fd, fd, 1, &argv[0],
+		       fd, fd, fd,
+		       arg ? 2 : 1, &argv[0],
 		       tl->to_ulabel(), tl->to_ulabel(),
 		       0);
     if (ct < 0)
-	panic("cannot spawn %s: %s\n", pn, e2s(ct));
+	throw error(ct, "cannot spawn %s", pn);
 
     printf("init: spawned %s with label %s\n", pn, tl->to_string());
 }
@@ -35,7 +37,7 @@ init_env(uint64_t c_root, uint64_t c_self)
 {
     start_env = 0;
     struct cobj_ref sa;
-    assert(0 == segment_alloc(c_self, PGSIZE, &sa, (void**) &start_env,
+    error_check(segment_alloc(c_self, PGSIZE, &sa, (void**) &start_env,
 			      0, "init env"));
 
     start_env->container = c_self;
@@ -44,27 +46,27 @@ init_env(uint64_t c_root, uint64_t c_self)
     // set the filesystem root to be the same as the container root
     int r = fs_get_root(c_root, &start_env->fs_root);
     if (r < 0)
-	panic("fs_get_root: %s", e2s(r));
+	throw error(r, "fs_get_root");
 
     // mount binaries on /bin
     int64_t fs_bin_id = container_find(c_root, kobj_container, "fs root");
     if (fs_bin_id < 0)
-	panic("cannot find fs /bin directory: %s", e2s(fs_bin_id));
+	throw error(fs_bin_id, "cannot find /bin");
 
     struct fs_inode bin_dir;
     r = fs_get_root(fs_bin_id, &bin_dir);
     if (r < 0)
-	panic("fs_get_root for /bin: %s", e2s(r));
+	throw error(r, "fs_get_root for /bin");
 
     r = fs_mount(start_env->fs_root, "bin", bin_dir);
     if (r < 0)
-	panic("fs_mount for /bin: %s", e2s(r));
+	throw error(r, "fs_mount /bin");
 
     // create a scratch container
     struct fs_inode scratch;
     r = fs_mkdir(start_env->fs_root, "x", &scratch);
     if (r < 0)
-	panic("creating /x: %s", e2s(r));
+	throw error(r, "creating /x");
 
     // start out in the root directory
     start_env->fs_cwd = start_env->fs_root;
@@ -79,7 +81,7 @@ init_console(uint64_t h_root)
     assert(label);
 
     label->ul_default = 1;
-    assert(0 == label_set_level(label, h_root, 0, 1));
+    error_check(label_set_level(label, h_root, 0, 1));
     segment_set_default_label(label);
 
     int cons = opencons(start_env->root_container);
@@ -92,30 +94,41 @@ init_console(uint64_t h_root)
 static void
 init_procs(int cons, uint64_t h_root)
 {
+    label star(LB_LEVEL_STAR);
     label without_hroot(1);
     label with_hroot(1);
     with_hroot.set(h_root, LB_LEVEL_STAR);
 
-    // netd_mom should be the only process that needs our root handle at *,
+    int64_t h_adm = sys_handle_create();
+    error_check(h_adm);
+
+    char h_adm_buf[32];
+    snprintf(&h_adm_buf[0], sizeof(h_adm_buf), "%lu", h_adm);
+
+    // netd_mom should be the only process that needs { h_root:* },
     // in order to create an appropriately-labeled netdev object.
-    spawn_fs(cons, "/bin/netd_mom", &with_hroot);
+    spawn_fs(cons, "/bin/netd_mom", 0, &with_hroot);
 
-    //spawn_fs(cons, "/bin/shell", &with_hroot);
-    spawn_fs(cons, "/bin/shell", &without_hroot);
+    // admin server gets { * }
+    spawn_fs(cons, "/bin/admind", &h_adm_buf[0], &star);
 
-    //spawn_fs(cons, "/bin/telnetd", &without_hroot);
-    //spawn_fs(cons, "/bin/httpd", &without_hroot);
+    //spawn_fs(cons, "/bin/shell", 0, &with_hroot);
+    spawn_fs(cons, "/bin/shell", 0, &without_hroot);
+
+    //spawn_fs(cons, "/bin/telnetd", 0, &without_hroot);
+    //spawn_fs(cons, "/bin/httpd", 0, &without_hroot);
 }
 
 int
 main(int ac, char **av)
+try
 {
     uint64_t c_root = start_arg0;
     uint64_t h_root = start_arg1;
 
     int64_t c_self = container_find(c_root, kobj_container, "init");
     if (c_self < 0)
-	panic("cannot find init container: %s", e2s(c_self));
+	throw error(c_self, "cannot find init container");
 
     assert(0 == opencons(c_self));
     assert(1 == dup(0, 1));
@@ -129,4 +142,6 @@ main(int ac, char **av)
 
     for (;;)
 	thread_sleep(100000);
+} catch (std::exception &e) {
+    printf("init: %s\n", e.what());
 }
