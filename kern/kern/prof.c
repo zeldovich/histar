3b76c36d63afe8d312b55f98524cb272f00832a5
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-02 04:49:52 +0000

    cleaner
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@998 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/prof.c
+++ kern/kern/prof.c
@@ -22,11 +22,11 @@ static int prof_print_enable = 0;
 static struct periodic_task timer2 ;
 
 // for profiling using gcc's -finstrument-functions
-static int cyg_prof_print_enable = 1 ;
-static int cyg_prof_enable = 1 ;
+static int cyg_prof_print_enable = 0 ;
+static int cyg_prof_enable = 0 ;
 
-#define NUM_PROFILED 2 
-static uint64_t cyg_profiled_func[NUM_PROFILED] = {
+#define NUM_PROFS_PRINTED 2 
+static uint64_t cyg_profs_printed[NUM_PROFS_PRINTED] = {
         (uint64_t)&memset,
         (uint64_t)&memcpy,
 } ;
@@ -39,7 +39,7 @@ struct func_stamp
 
 struct cyg_stack
 {
-        uint64_t stack_base ;
+        uint64_t rsp ;
         
         int size ;     
         struct func_stamp func_stamp[PGSIZE] ;       
@@ -50,7 +50,6 @@ static struct
         char enable ;
 
 #define NUM_STACKS 16
-        // map from sp base to stack holding enters
         struct cyg_stack stack[NUM_STACKS] ;
 
 #define NUM_SYMS 800
@@ -71,7 +70,6 @@ prof_init(void)
 	
         memset(&cyg_data, 0, sizeof(cyg_data)) ;
         hash_init(&cyg_data.stats_lookup, cyg_data.stats_lookup_back, NUM_SYMS) ;
-        
         
 	timer.pt_fn = &prof_print ;
 	timer.pt_interval_ticks = kclock_hz * 10;
@@ -147,6 +145,32 @@ prof_print(void)
 	prof_reset();
 }
 
+//////////////////
+// cyg_profile 
+//////////////////
+
+static struct cyg_stack* __attribute__ ((no_instrument_function))
+stack_for(uint64_t sp)
+{
+        for (int i = 0 ; i < NUM_STACKS ; i++) {
+                if (cyg_data.stack[i].rsp == sp)
+                        return &cyg_data.stack[i] ;       
+        }
+        return 0 ;       
+}
+
+static struct cyg_stack* __attribute__ ((no_instrument_function))
+stack_alloc(uint64_t sp)
+{
+        for (int i = 0 ; i < NUM_STACKS ; i++) {       
+                if (cyg_data.stack[i].size == 0) {
+                        cyg_data.stack[i].rsp = sp ;
+                        return &cyg_data.stack[i] ;
+                }
+        }       
+        return 0 ;
+}
+
 static void __attribute__((no_instrument_function))
 cyg_profile_reset(void)
 {
@@ -154,7 +178,7 @@ cyg_profile_reset(void)
 }
 
 static void __attribute__((no_instrument_function))
-cyg_profile_func(void *func_addr, uint64_t time)
+cyg_profile_data(void *func_addr, uint64_t time)
 {
         uint64_t func = (uint64_t) func_addr ;
         uint64_t val ;
@@ -171,76 +195,6 @@ cyg_profile_func(void *func_addr, uint64_t time)
         return ;
 }
 
-void __attribute__((no_instrument_function))
-__cyg_profile_func_enter(void *this_fn, void *call_site)
-{
-        if (!cyg_data.enable)
-                return ;
-        
-        cyg_data.enable = 0 ;
-        uint64_t sp = ROUNDUP(read_rsp(), PGSIZE) ;
-        int i = 0 ;
-        for (; i < NUM_STACKS ; i++) {
-                if (cyg_data.stack[i].stack_base == sp)
-                        break ;                               
-        }
-        
-        if (i == NUM_STACKS) {
-                for (i = 0 ; i < NUM_STACKS ; i++) {       
-                        if (cyg_data.stack[i].stack_base == 0) {
-                                cyg_data.stack[i].stack_base = sp ;
-                                break ;       
-                        }
-                }
-                if (i == NUM_STACKS)
-                        panic("__cyg_profile_func_enter: out of func addr stacks") ;
-        }
-
-        cyg_data.stack[i].func_stamp[cyg_data.stack[i].size].func_addr = (uint64_t) this_fn ;
-        cyg_data.stack[i].func_stamp[cyg_data.stack[i].size].tsc = read_tsc() ;
-        cyg_data.stack[i].size++ ;
-        
-        if (cyg_data.stack[i].size == PGSIZE)
-                panic("__cyg_profile_func_enter: overflow func addr stack") ;
-        
-        cyg_data.enable = 1 ;
-}        
-
-void __attribute__((no_instrument_function))
-__cyg_profile_func_exit(void *this_fn, void *call_site)
-{
-        if (!cyg_data.enable)
-                return ;
-
-        cyg_data.enable = 0 ;
-        uint64_t sp = ROUNDUP(read_rsp(), PGSIZE) ;
-        int i = 0 ;
-        for (; i < NUM_STACKS ; i++) {
-                if (cyg_data.stack[i].stack_base == sp)
-                        break ;                               
-        }
-
-        assert(i < NUM_STACKS) ;
-
-        while (1) {
-                cyg_data.stack[i].size-- ;
-                if (cyg_data.stack[i].func_stamp[cyg_data.stack[i].size].func_addr == (uint64_t)this_fn)
-                        break ;
-                        
-                if (cyg_data.stack[i].size == 0)
-                        panic("__cyg_profile_func_exit: func addr stack bottomed out") ;
-        }
-       
-        if (cyg_data.stack[i].size == 0)
-                cyg_data.stack[i].stack_base = 0 ;
-
-
-        uint64_t time = read_tsc() - cyg_data.stack[i].func_stamp[cyg_data.stack[i].size].tsc ;
-        cyg_profile_func(this_fn, time) ;
-
-        cyg_data.enable = 1 ;
-}
-
 void __attribute__ ((no_instrument_function))
 cyg_profile_free_stack(uint64_t sp)
 {
@@ -249,13 +203,12 @@ cyg_profile_free_stack(uint64_t sp)
         
         sp = ROUNDUP(sp, PGSIZE) ;
         
-        for (int i = 0 ; i < NUM_STACKS ; i++) {
-                if (cyg_data.stack[i].stack_base == sp) {
-                        cyg_data.stack[i].stack_base = 0 ;                               
-                        cyg_data.stack[i].size = 0 ;
-                        return ;
-                }
-        }        
+        struct cyg_stack *s ;
+        if ((s = stack_for(sp)) != 0) {
+                s->rsp = 0 ;
+                s->size = 0 ;
+        }
+        return ;
 }
 
 void __attribute__((no_instrument_function))
@@ -267,14 +220,63 @@ cyg_profile_print(void)
         cyg_data.enable = 0 ;
         
         cprintf("cyg_profile_print: selected functions\n");
-        for (int i = 0 ; i < NUM_PROFILED ; i++) {
+        for (int i = 0 ; i < NUM_PROFS_PRINTED ; i++) {
                 uint64_t val ;
                 char buf[32] ;
-                if (hash_get(&cyg_data.stats_lookup, cyg_profiled_func[i], &val) == 0) {
-                        sprintf(buf, "%lx", cyg_profiled_func[i]) ;
+                if (hash_get(&cyg_data.stats_lookup, cyg_profs_printed[i], &val) == 0) {
+                        sprintf(buf, "%lx", cyg_profs_printed[i]) ;
                         print_entry(cyg_data.stat, val, buf) ;
                 }
         }
         cyg_profile_reset() ;
+        cyg_data.enable = 1 ;
+}
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_enter(void *this_fn, void *call_site)
+{
+        if (!cyg_data.enable)
+                return ;
+        
+        cyg_data.enable = 0 ;
+        uint64_t sp = ROUNDUP(read_rsp(), PGSIZE) ;
+        
+        struct cyg_stack *stack ;
+        if ((stack = stack_for(sp)) == 0)
+                assert((stack = stack_alloc(sp)) != 0) ; 
+                // out of func addr stacks
+
+        stack->func_stamp[stack->size].func_addr = (uint64_t) this_fn ;
+        stack->func_stamp[stack->size].tsc = read_tsc() ;
+        stack->size++ ;
+        
+        // overflow func addr stack
+        assert(stack->size != PGSIZE) ;
+
+        cyg_data.enable = 1 ;
+}    
+
+void __attribute__((no_instrument_function))
+__cyg_profile_func_exit(void *this_fn, void *call_site)
+{
+        if (!cyg_data.enable)
+                return ;
+
+        cyg_data.enable = 0 ;
+        uint64_t sp = ROUNDUP(read_rsp(), PGSIZE) ;
+        
+        struct cyg_stack *stack = stack_for(sp) ;
+        
+        while (1) {
+                stack->size-- ;
+                if (stack->func_stamp[stack->size].func_addr == (uint64_t)this_fn)
+                        break ;
+                // bottom out func addr stack
+                assert(stack->size != 0) ;        
+        }
+       
+        uint64_t time = read_tsc() - stack->func_stamp[stack->size].tsc ;
+        cyg_profile_data(this_fn, time) ;
+
         cyg_data.enable = 1 ;
 }
