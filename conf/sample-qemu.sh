1ccdf5e5f90ba464f9a9f96391363940109cd92f
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-04 02:04:01 +0000

    sample of how to redirect tcp ports with qemu
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1038 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/sample-qemu.sh
+++ conf/sample-qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 -serial stdio -hda ./obj/kern/bochs.img -hdb ./obj/fs/fs.img -m 64
+qemu-system-x86_64 -serial stdio -hda ./obj/kern/bochs.img -hdb ./obj/fs/fs.img -m 64 -redir tcp:9923::23 -redir tcp:9980::80
