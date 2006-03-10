8fca381aef16159aec6434049015fa91ecf1a1e0
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-10 04:43:22 +0000

    siglongjmp
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1157 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/signal.c
+++ pkg/uclibc/libc/sysdeps/jos64/signal.c
@@ -1,7 +1,11 @@
+#include <inc/syscall.h>
+#include <inc/lib.h>
 #include <inc/stdio.h>
 #include <errno.h>
 #include <signal.h>
 #include <unistd.h>
+#include <setjmp.h>
+#include <inc/setjmp.h>
 
 // BSD compat
 const char *sys_signame[_NSIG];
@@ -26,7 +30,7 @@ raise(int sig)
     if (sig == SIGABRT) {
 	cprintf("raising SIGABRT\n");
 	process_report_exit(-SIGABRT);
-	return sys_thread_halt();
+	thread_halt();
     }
 
     cprintf("raise(%d)\n", sig);
@@ -45,4 +49,17 @@ killpg(int pgrp, int sig)
 {
     __set_errno(ENOSYS);
     return -1;
+}
+
+int
+__sigsetjmp(jmp_buf __env, int __savemask)
+{
+    jos_setjmp((struct jos_jmp_buf *) __env);
+    return 0;
+}
+
+void
+siglongjmp(sigjmp_buf env, int val)
+{
+    jos_longjmp((struct jos_jmp_buf *) env, val ? : 1);
 }
