9f18b9e7d99e9bf336e71ff529cfcc84ff5b680e
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-10 04:34:15 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1154 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/proc.c
+++ pkg/uclibc/libc/sysdeps/jos64/proc.c
@@ -1,5 +1,6 @@
 #include <inc/lib.h>
 #include <unistd.h>
+#include <errno.h>
 
 pid_t
 getpid()
@@ -17,4 +18,30 @@ int
 nice(int n)
 {
     return 0;
+}
+
+int
+setpgid(pid_t pid, pid_t pgid)
+{
+    __set_errno(ENOSYS);
+    return -1;
+}
+
+pid_t
+getpgid(pid_t pid)
+{
+    __set_errno(ENOSYS);
+    return -1;
+}
+
+int
+setpgrp(void)
+{
+    return setpgid(0, 0);
+}
+
+pid_t
+getpgrp(void)
+{
+    return getpgid(0);
 }
