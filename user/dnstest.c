bc3f464e69f15cc64f2872a2f5f27ddc9c6ef87a
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-14 11:17:04 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1257 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/dnstest.c
+++ user/dnstest.c
@@ -3,16 +3,24 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <unistd.h>
+
+static void 
+lookup(void)
+{
+    struct hostent *hp = gethostbyname("suif.stanford.edu");
+    if (hp == 0) {
+        herror("gethostbyname error");
+    } else {
+        struct in_addr addr;
+        memcpy(&addr, hp->h_addr, sizeof(addr));
+        printf("ip addr %s\n", inet_ntoa(addr));
+    }
+}
 
 int
 main(int ac, char **av)
 {
-    struct hostent *hp = gethostbyname("suif.stanford.edu");
-    if (hp == 0) {
-	herror("gethostbyname");
-    } else {
-	struct in_addr addr;
-	memcpy(&addr, hp->h_addr, sizeof(addr));
-	printf("ip addr %s\n", inet_ntoa(addr));
-    }
+    if (fork() == 0)
+        lookup() ;
 }
