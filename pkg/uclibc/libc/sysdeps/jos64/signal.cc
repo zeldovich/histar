a18870e91d052eb7984f14f7953783438b5142e4
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-14 23:40:07 +0000

    attempt at cross-process signaling
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1304 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/signal.cc
+++ pkg/uclibc/libc/sysdeps/jos64/signal.cc
@@ -10,10 +10,17 @@ extern "C" {
 #include <string.h>
 #include <inc/setjmp.h>
 #include <inc/utrap.h>
+#include <inc/assert.h>
 #include <machine/trapcodes.h>
 
 #include <bits/unimpl.h>
 }
+
+#include <inc/gateparam.hh>
+#include <inc/gateclnt.hh>
+#include <inc/gatesrv.hh>
+#include <inc/cpplabel.hh>
+#include <inc/labelutil.hh>
 
 // BSD compat
 const char *sys_signame[_NSIG];
@@ -79,6 +86,9 @@ signal_utrap(struct UTrapframe *utf)
     if (utf->utf_trap_src == UTRAP_SRC_HW) {
 	si.si_addr = (void *) utf->utf_trap_arg;
 	if (utf->utf_trap_num == T_PGFLT) {
+	    cprintf("signal_utrap: pagefault, va=0x%lx, rip=0x%lx, rsp=0x%lx\n",
+		    utf->utf_trap_arg, utf->utf_rip, utf->utf_rsp);
+
 	    si.si_signo = SIGSEGV;
 	    si.si_code = SEGV_ACCERR;	// maybe use segment_lookup()
 	} else {
@@ -115,11 +125,51 @@ signal_utrap_init(void)
 }
 
 // Signal gates
+static void
+signal_gate_entry(void *arg, gate_call_data *gcd, gatesrv_return *gr)
+{
+    siginfo_t *si = (siginfo_t *) &gcd->param_buf[0];
+    static_assert(sizeof(*si) <= sizeof(gcd->param_buf));
+
+    uint32_t signo = si->si_signo;
+    if (signo < _NSIG) {
+	siginfos[signo] = *si;
+	int64_t id = container_find(start_env->proc_container, kobj_thread, 0);
+	if (id >= 0) {
+	    struct cobj_ref tobj = COBJ(start_env->proc_container, id);
+	    int r = sys_thread_trap(tobj, 0, signo);
+	    if (r < 0)
+		cprintf("signal_gate_entry: trap main thread: %s\n", e2s(r));
+	}
+    }
+
+    gr->ret(0, 0, 0);
+}
+
 void
 signal_gate_create(void)
 {
     signal_utrap_init();
-    // XXX create a gate to dispatch signals
+
+    static gatesrv *gs;
+    if (gs) {
+	try {
+	    delete gs;
+	} catch (...) {}
+	gs = 0;
+    }
+
+    try {
+	label tl, tc;
+	thread_cur_label(&tl);
+	thread_cur_clearance(&tc);
+	gs = new gatesrv(start_env->shared_container, "signal", &tl, &tc);
+	gs->set_entry_container(start_env->proc_container);
+	gs->set_entry_function(&signal_gate_entry, 0);
+	gs->enable();
+    } catch (std::exception &e) {
+	cprintf("signal_gate_create: %s\n", e.what());
+    }
 }
 
 // System calls emulated in jos64
@@ -133,8 +183,15 @@ sigprocmask(int how, const sigset_t *set, sigset_t *oldset) __THROW
 extern "C" int
 __syscall_sigaction(int signum, const struct sigaction *act, struct sigaction *oldact)
 {
-    __set_errno(ENOSYS);
-    return -1;
+    if (signum < 0 || signum >= _NSIG) {
+	__set_errno(EINVAL);
+	return -1;
+    }
+
+    if (oldact)
+	*oldact = sigactions[signum];
+    sigactions[signum] = *act;
+    return 0;
 }
 
 int
@@ -152,12 +209,35 @@ kill(pid_t pid, int sig) __THROW
 	siginfo_t si;
 	memset(&si, 0, sizeof(si));
 	si.si_signo = sig;
+	si.si_pid = self;
 	signal_dispatch(&si);
 	return 0;
     }
 
-    set_enosys();
-    return -1;
+    uint64_t ct = pid;
+    int64_t gate_id = container_find(ct, kobj_gate, "signal");
+    if (gate_id < 0) {
+	cprintf("kill: cannot find signal gate in %ld: %s\n", ct, e2s(gate_id));
+	__set_errno(ESRCH);
+	return -1;
+    }
+
+    struct gate_call_data gcd;
+    memset(&gcd, 0, sizeof(gcd));
+
+    siginfo_t *si = (siginfo_t *) &gcd.param_buf[0];
+    si->si_pid = self;
+    si->si_signo = sig;
+
+    try {
+	gate_call(COBJ(ct, gate_id), &gcd, 0, 0, 0);
+    } catch (std::exception &e) {
+	cprintf("kill: gate_call: %s\n", e.what());
+	__set_errno(EPERM);
+	return -1;
+    }
+
+    return 0;
 }
 
 int
@@ -169,5 +249,5 @@ __sigsetjmp(jmp_buf __env, int __savemask) __THROW
 void
 siglongjmp(sigjmp_buf env, int val) __THROW
 {
-    jos_longjmp((struct jos_jmp_buf *) env, val ? : 1);
+    jos_longjmp((struct jos_jmp_buf *) env, val);
 }
