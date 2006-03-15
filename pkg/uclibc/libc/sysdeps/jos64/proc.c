5ef1262ec5eaf61207d3d56c4de5a03803ff9346
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-15 02:34:06 +0000

    a not very useful wait4 impl
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1311 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/proc.c
+++ pkg/uclibc/libc/sysdeps/jos64/proc.c
@@ -1,6 +1,10 @@
 #include <inc/lib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <inc/syscall.h>
+#include <string.h>
 
 #include <bits/unimpl.h>
 
@@ -30,7 +34,7 @@ setpgid(pid_t pid, pid_t pgid)
 }
 
 pid_t
-getpgid(pid_t pid)
+__getpgid(pid_t pid)
 {
     set_enosys();
     return -1;
@@ -39,12 +43,54 @@ getpgid(pid_t pid)
 pid_t
 getpgrp(void)
 {
-    return getpgid(0);
+    return __getpgid(0);
 }
 
 pid_t
-wait4(pid_t pid, int *status, int options, struct rusage *rusage)
+wait4(pid_t pid, int *statusp, int options, struct rusage *rusage)
 {
-    set_enosys();
-    return -1;
+    if (pid <= 0) {
+	set_enosys();
+	return -1;
+    }
+
+    uint64_t ct = pid;
+    int64_t id = container_find(ct, kobj_segment, "exit status");
+    if (id < 0) {
+	__set_errno(ESRCH);
+	return -1;
+    }
+
+    uint64_t proc_status = PROCESS_RUNNING;
+    uint64_t exit_code;
+
+    while (proc_status == PROCESS_RUNNING) {
+	struct cobj_ref psobj = COBJ(ct, id);
+	struct process_state *ps = 0;
+	int r = segment_map(psobj, SEGMAP_READ, (void **) &ps, 0);
+	if (r < 0) {
+	    __set_errno(EPERM);
+	    return -1;
+	}
+
+	if (ps->status == PROCESS_RUNNING && (options & WNOHANG)) {
+	    segment_unmap(ps);
+	    __set_errno(ECHILD);
+	    return -1;
+	}
+
+	sys_sync_wait(&ps->status, PROCESS_RUNNING, sys_clock_msec() + 10000);
+	proc_status = ps->status;
+	exit_code = ps->exit_code;
+	segment_unmap(ps);
+    }
+
+    union wait wstat;
+    memset(&wstat, 0, sizeof(wstat));
+    wstat.w_termsig = 0;
+    wstat.w_retcode = exit_code;
+
+    if (statusp)
+	*statusp = wstat.w_status;
+    return 0;
 }
