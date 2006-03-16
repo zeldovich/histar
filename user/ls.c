4768b7a6bc47a6bcb2b5d6901c792d54bfc3eb68
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-16 01:51:30 +0000

    disallow open("")
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1352 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/ls.c
+++ user/ls.c
@@ -7,21 +7,21 @@
 int
 main(int ac, char **av)
 {
-    const char *dname;
+    int r;
+    struct fs_inode dir;
+
     if (ac == 1) {
-	dname = "";
+	dir = start_env->fs_cwd;
     } else if (ac == 2) {
-	dname = av[1];
+	const char *dname = av[1];
+	r = fs_namei(dname, &dir);
+	if (r < 0) {
+	    printf("cannot lookup %s: %s\n", dname, e2s(r));
+	    return r;
+	}
     } else {
 	printf("Usage: %s [dirname]\n", av[0]);
 	return -1;
-    }
-
-    struct fs_inode dir;
-    int r = fs_namei(dname, &dir);
-    if (r < 0) {
-	printf("cannot lookup %s: %s\n", dname, e2s(r));
-	return r;
     }
 
     struct fs_readdir_state s;
