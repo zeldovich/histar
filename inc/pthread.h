10f5868ab08003a2ae6c924a87a6adcc93409243
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-17 03:38:31 +0000

    a much better pthread_mutex that avoids all syscalls in common case
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1412 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/pthread.h
+++ inc/pthread.h
@@ -22,6 +22,7 @@ pthread_mutex_lock(pthread_mutex_t *mu)
 	if (cur == 0)
 	    break;
 
+	atomic_compare_exchange64(mu, 1, 2);
 	sys_sync_wait(&mu->counter, cur, ~0UL);
     }
     return 0;
@@ -30,11 +31,15 @@ pthread_mutex_lock(pthread_mutex_t *mu)
 static __inline __attribute__((always_inline)) int
 pthread_mutex_unlock(pthread_mutex_t *mu)
 {
-    if (atomic_read(mu) == 0)
+    uint64_t was = atomic_compare_exchange64(mu, 1, 0);
+    if (was == 0)
 	panic("pthread_mutex_unlock: %p not locked", mu);
 
-    atomic_set(mu, 0);
-    sys_sync_wakeup(&mu->counter);
+    if (was == 2) {
+	atomic_set(mu, 0);
+	sys_sync_wakeup(&mu->counter);
+    }
+
     return 0;
 }
 
