c41d7b766dc9ce4142b13fbc033804102f2b0710
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-17 03:17:51 +0000

    a good memset makes a surprising difference
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1410 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/memset.c
+++ lib/memset.c
@@ -1,9 +1,12 @@
+/*	$OpenBSD: memset.c,v 1.5 2004/08/07 00:38:32 deraadt Exp $	*/
+/*	$NetBSD: memset.c,v 1.6 1998/03/27 05:35:47 cgd Exp $	*/
+
 /*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
  *
  * This code is derived from software contributed to Berkeley by
- * Chris Torek.
+ * Mike Hibler and Chris Torek.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,16 +37,65 @@
 #include <kern/lib.h>
 #endif
 
+#define	wsize	sizeof(uint64_t)
+#define	wmask	(wsize - 1)
+
 void *
-memset(void *dst, int c, size_t n)
+memset(void *dst0, int c0, size_t length)
 {
+	size_t t;
+	uint64_t c;
+	uint8_t *dst;
 
-  if (n != 0) {
-    char *d = dst;
+	dst = dst0;
+	/*
+	 * If not enough words, just fill bytes.  A length >= 2 words
+	 * guarantees that at least one of them is `complete' after
+	 * any necessary alignment.  For instance:
+	 *
+	 *	|-----------|-----------|-----------|
+	 *	|00|01|02|03|04|05|06|07|08|09|0A|00|
+	 *	          ^---------------------^
+	 *		 dst		 dst+length-1
+	 *
+	 * but we use a minimum of 3 here since the overhead of the code
+	 * to do word writes is substantial.
+	 */ 
+	if (length < 3 * wsize) {
+		while (length != 0) {
+			*dst++ = c0;
+			--length;
+		}
+		return dst0;
+	}
 
-    do
-      *d++ = c;
-    while (--n != 0);
-  }
-  return (dst);
+	if ((c = (uint8_t)c0) != 0) {	/* Fill the word. */
+		c = (c << 8) | c;	/* uint64_t is 16 bits. */
+		c = (c << 16) | c;	/* uint64_t is 32 bits. */
+		c = (c << 32) | c;	/* uint64_t is 64 bits. */
+	}
+
+	/* Align destination by filling in bytes. */
+	if ((t = (uint64_t)dst & wmask) != 0) {
+		t = wsize - t;
+		length -= t;
+		do {
+			*dst++ = c0;
+		} while (--t != 0);
+	}
+
+	/* Fill words.  Length was >= 2*words so we know t >= 1 here. */
+	t = length / wsize;
+	do {
+		*(uint64_t *)dst = c;
+		dst += wsize;
+	} while (--t != 0);
+
+	/* Mop up trailing bytes, if any. */
+	t = length & wmask;
+	if (t != 0)
+		do {
+			*dst++ = c0;
+		} while (--t != 0);
+	return dst0;
 }
