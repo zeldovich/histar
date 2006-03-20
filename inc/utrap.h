a0f6afb52e16a914233c92d927e837ad822b6bf8
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-03-20 20:40:54 +0000

    avoid c++-style comments in c header files
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1517 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/utrap.h
+++ inc/utrap.h
@@ -3,12 +3,12 @@
 
 #include <machine/utrap.h>
 
-// Assembly stubs
+/* Assembly stubs */
 void utrap_stub(void);
 void utrap_stub_end(void);
 void utrap_ret(struct UTrapframe *utf) __attribute__((noreturn));
 
-// C fault handler
+/* C fault handler */
 void utrap_entry(struct UTrapframe *utf) __attribute__((noreturn));
 int  utrap_init(void);
 void utrap_set_handler(void (*fn)(struct UTrapframe *));
