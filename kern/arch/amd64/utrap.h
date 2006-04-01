642c5a1f8debc1d64956e023cd8db838fe878b5a
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-01 09:36:30 +0000

    backtraces across utrap
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1775 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/amd64/utrap.h
+++ kern/arch/amd64/utrap.h
@@ -3,6 +3,9 @@
 
 #include <inc/types.h>
 
+// The layout of this structure has to match the DWARF2 hints
+// in lib/amd64/trapstub.S
+
 struct UTrapframe {
     uint64_t utf_rax;
     uint64_t utf_rbx;
