064bb35ad603a82c943ecf5fd41bf0153cb50452
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-07 09:08:56 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1878 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/init.cc
+++ user/init.cc
@@ -86,10 +86,11 @@ init_env(uint64_t c_root, uint64_t c_self, uint64_t h_root)
     struct fs_inode etc;
     error_check(fs_mkdir(start_env->fs_root, "etc", &etc, 0));
 
-    struct fs_inode hosts, resolv, passwd;
+    struct fs_inode hosts, resolv, passwd, groups;
     error_check(fs_create(etc, "hosts", &hosts, 0));
     error_check(fs_create(etc, "resolv.conf", &resolv, 0));
     error_check(fs_create(etc, "passwd", &passwd, 0));
+    error_check(fs_create(etc, "groups", &groups, 0));
 
     const char *resolv_conf = "nameserver 171.66.3.11\n";
     error_check(fs_pwrite(resolv, resolv_conf, strlen(resolv_conf), 0));
