db3a47ba6604212cd9383e862d4e71cbd92ec36f
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-11 02:34:12 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@1947 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/clamwrap.cc
+++ user/clamwrap.cc
@@ -11,7 +11,10 @@ int
 main(int ac, char **av)
 try
 {
+    int64_t clam_taint;
+    error_check(clam_taint = sys_handle_create());
 
+    
 } catch (std::exception &e) {
     printf("clamwrap: %s\n", e.what());
 }
