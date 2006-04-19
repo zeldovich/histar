7b9e1537fd6ab1de065528033070fc3b6e531670
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-19 00:22:51 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2161 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/elf.c
+++ lib/elf.c
@@ -134,7 +134,7 @@ elf_load(uint64_t container, struct cobj_ref seg, struct thread_entry *e,
     struct u_address_space uas = { .nent = si, .ents = &sm_ents[0] };
     int64_t as_id = sys_as_create(container, label, &elfname[0]);
     if (as_id < 0) {
-	cprintf("elf_load: cannot create address space: %s\n", e2s(r));
+	cprintf("elf_load: cannot create address space: %s\n", e2s(as_id));
 	return as_id;
     }
 
