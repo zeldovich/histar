045c12da4480e68ff8ab3a71c15ab7adcdaadf6a
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-19 01:57:08 +0000

    hmm
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2170 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/taint.c
+++ lib/taint.c
@@ -6,15 +6,31 @@
 #include <inc/error.h>
 #include <inc/stdio.h>
 
-enum {
-    taint_debug = 0,
-};
+enum { taint_debug = 0 };
 
 // Copy the writable pieces of the address space
 enum {
     taint_cow_label_ents = 32,
     taint_cow_as_ents = 32,
 };
+
+static void
+taint_cow_cprint_label(struct ulabel *l)
+{
+    level_t def = l->ul_default;
+    cprintf("{ ");
+    for (uint32_t i = 0; i < l->ul_nent; i++) {
+	uint64_t h = LB_HANDLE(l->ul_ent[i]);
+	level_t lv = label_get_level(l, h);
+	if (lv != def) {
+	    if (lv == LB_LEVEL_STAR)
+		cprintf("%ld:*, ", h);
+	    else
+		cprintf("%ld:%d, ", h, lv);
+	}
+    }
+    cprintf("%d }", def);
+}
 
 static void
 taint_cow_compute_label(struct ulabel *cur_label, struct ulabel *obj_label)
@@ -67,6 +83,16 @@ taint_cow(uint64_t taint_container, struct cobj_ref declassify_gate)
 
     // if we can write to the address space, that's "good enough"
     int r = label_compare(&cur_label, &obj_label, label_leq_starlo);
+
+    if (taint_debug) {
+	cprintf("taint_cow: thread label ");
+	taint_cow_cprint_label(&cur_label);
+	cprintf("\n");
+	cprintf("taint_cow: old AS label ");
+	taint_cow_cprint_label(&obj_label);
+	cprintf("\n");
+    }
+
     if (r == 0) {
 	if (taint_debug)
 	    cprintf("taint_cow: no need to cow\n");
@@ -78,6 +104,11 @@ taint_cow(uint64_t taint_container, struct cobj_ref declassify_gate)
 				    start_env_ro->proc_container), &obj_label));
 
     taint_cow_compute_label(&cur_label, &obj_label);
+    if (taint_debug) {
+	cprintf("taint_cow: new container label ");
+	taint_cow_cprint_label(&obj_label);
+	cprintf("\n");
+    }
 
     int64_t mlt_ct_id = sys_container_alloc(taint_container, &obj_label,
 					    "taint_cow container", 0, CT_QUOTA_INF);
@@ -104,12 +135,16 @@ taint_cow(uint64_t taint_container, struct cobj_ref declassify_gate)
 	if (r == 0)
 	    continue;
 
-	if (taint_debug)
-	    cprintf("taint_cow: trying to copy segment %ld.%ld\n",
-		    uas.ents[i].segment.container,
-		    uas.ents[i].segment.object);
-
 	taint_cow_compute_label(&cur_label, &obj_label);
+
+	if (taint_debug) {
+	    cprintf("taint_cow: trying to copy segment %ld.%ld, VA %p, label ",
+		    uas.ents[i].segment.container,
+		    uas.ents[i].segment.object,
+		    uas.ents[i].va);
+	    taint_cow_cprint_label(&obj_label);
+	    cprintf("\n");
+	}
 
 	ERRCHECK(sys_obj_get_name(uas.ents[i].segment, &namebuf[0]));
 
@@ -131,7 +166,6 @@ taint_cow(uint64_t taint_container, struct cobj_ref declassify_gate)
     }
 
     ERRCHECK(sys_obj_get_label(cur_as, &obj_label));
-
     taint_cow_compute_label(&cur_label, &obj_label);
 
     ERRCHECK(sys_obj_get_name(cur_as, &namebuf[0]));
@@ -144,8 +178,11 @@ taint_cow(uint64_t taint_container, struct cobj_ref declassify_gate)
     ERRCHECK(sys_self_set_as(new_as));
     segment_as_switched();
 
-    if (taint_debug)
-	cprintf("taint_cow: new as: %lu.%lu\n", new_as.container, new_as.object);
+    if (taint_debug) {
+	cprintf("taint_cow: new as: %lu.%lu, label: ", new_as.container, new_as.object);
+	taint_cow_cprint_label(&obj_label);
+	cprintf("\n");
+    }
 
     start_env->proc_container = mlt_ct;
     start_env->shared_container = taint_container;
