547a206a662f4ff813619e96642427f7ca2c9899
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-23 03:01:32 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2321 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/clamwrap.cc
+++ user/clamwrap.cc
@@ -1,20 +1,46 @@
 extern "C" {
 #include <inc/syscall.h>
+#include <inc/fd.h>
 
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
 }
 
 #include <inc/cpplabel.hh>
 #include <inc/error.hh>
+#include <inc/labelutil.hh>
 
 int
 main(int ac, char **av)
 try
 {
+    if (!start_env->user_taint)
+	throw basic_exception("Missing user taint handle -- not logged in?");
+
     int64_t clam_taint;
     error_check(clam_taint = handle_alloc());
 
-    
+    int fds[2];
+    if (pipe(&fds[0]) < 0)
+	throw basic_exception("cannot create pipe: %s", strerror(errno));
+
+    label clear;
+    thread_cur_clearance(&clear);
+    clear.set(start_env->user_taint, 3);
+    clear.set(clam_taint, 3);
+    thread_set_clearance(&clear);
+
+    label taint(0);
+    taint.set(start_env->user_taint, 3);
+    taint.set(clam_taint, 3);
+
+    error_check(fd_make_public(fds[0], taint.to_ulabel()));
+    error_check(fd_make_public(fds[1], taint.to_ulabel()));
+
+    // ...
+
 } catch (std::exception &e) {
     printf("clamwrap: %s\n", e.what());
 }
