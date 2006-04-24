c8b2432cbe766333b273517a9a5b0c5e0182a102
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-24 05:25:54 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2352 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/clamwrap.cc
+++ user/clamwrap.cc
@@ -6,14 +6,16 @@ extern "C" {
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 }
 
 #include <inc/cpplabel.hh>
 #include <inc/error.hh>
 #include <inc/labelutil.hh>
+#include <inc/spawn.hh>
 
 int
-main(int ac, char **av)
+main(int ac, const char **av)
 try
 {
     if (!start_env->user_taint)
@@ -26,6 +28,10 @@ try
     if (pipe(&fds[0]) < 0)
 	throw basic_exception("cannot create pipe: %s", strerror(errno));
 
+    int nullfd = open("/dev/null", O_RDONLY);
+    if (nullfd < 0)
+	throw basic_exception("cannot open /dev/null: %s", strerror(errno));
+
     label clear;
     thread_cur_clearance(&clear);
     clear.set(start_env->user_taint, 3);
@@ -36,11 +42,38 @@ try
     taint.set(start_env->user_taint, 3);
     taint.set(clam_taint, 3);
 
+    label taint_star(LB_LEVEL_STAR);
+    taint_star.set(start_env->user_taint, 3);
+    taint_star.set(clam_taint, 3);
+
     error_check(fd_make_public(fds[0], taint.to_ulabel()));
     error_check(fd_make_public(fds[1], taint.to_ulabel()));
+    error_check(fd_make_public(nullfd, taint.to_ulabel()));
 
-    // ...
+    fs_inode clamscan;
+    error_check(fs_namei("/bin/clamscan", &clamscan));
 
+    av[0] = "clamscan";
+    child_process cp =
+	spawn(start_env->shared_container, clamscan,
+	      nullfd, fds[1], fds[1],
+	      ac, &av[0],
+	      0, 0,
+	      &taint_star, 0, 0, &taint, 0);
+    close(fds[1]);
+    close(nullfd);
+
+    char buf[512];
+    for (;;) {
+	ssize_t cc = read(fds[0], &buf[0], sizeof(buf));
+	if (cc < 0)
+	    throw basic_exception("cannot read: %s", strerror(errno));
+
+	if (cc == 0)
+	    break;
+
+	write(1, &buf[0], cc);
+    }
 } catch (std::exception &e) {
     printf("clamwrap: %s\n", e.what());
 }
