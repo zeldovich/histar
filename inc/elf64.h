54a2c5b000cf1b2ae22fac3edce6bca2c3b23929
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-04-26 19:49:55 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2408 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/elf64.h
+++ inc/elf64.h
@@ -9,10 +9,10 @@ typedef uint64_t Elf64_Addr;
 typedef uint64_t Elf64_Off;
 typedef uint16_t Elf64_Half;
 typedef uint32_t Elf64_Word;
-typedef int32_t Elf64_Sword;
+typedef  int32_t Elf64_Sword;
 typedef uint64_t Elf64_Xword;
-typedef int64_t Elf64_Sxword;
-typedef int16_t Elf64_Section;
+typedef  int64_t Elf64_Sxword;
+typedef  int16_t Elf64_Section;
 
 typedef struct {
   Elf64_Word      e_magic;
