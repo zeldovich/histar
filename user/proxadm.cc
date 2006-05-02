8cb66fb4c02cd679db6b37204da80a4e3b13f699
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-02 06:51:48 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2455 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ user/proxadm.cc
@@ -0,0 +1,8 @@
+#include <lib/dis/proxydclnt.hh>
+
+int
+main (int ac, char **av)
+{
+    proxyd_addmapping(0, 0);
+    return 0;    
+}
---
+++ user/proxyd.cc
@@ -0,0 +1,23 @@
+#include <lib/dis/proxydsrv.hh>
+
+#include <inc/cpplabel.hh>
+#include <inc/labelutil.hh>
+#include <inc/error.hh>
+
+extern "C" {
+#include <inc/lib.h>    
+#include <stdio.h>
+}
+
+int
+main (int ac, char **av)
+try {
+    label th_ctm, th_clr;
+    thread_cur_label(&th_ctm);
+    thread_cur_clearance(&th_clr);
+    proxydsrv_create(start_env->shared_container, "proxyd", &th_ctm, &th_clr);
+    thread_halt();
+} catch (std::exception &e) {
+    printf("proxyd: %s\n", e.what());
+    return -1;
+}
