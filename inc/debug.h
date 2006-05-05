217fba107015de22bcbb96356b4c06a5ef1c7c40
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-05 09:02:52 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2467 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ inc/debug.h
@@ -0,0 +1,10 @@
+#ifndef JOS_INC_DEBUG_H_
+#define JOS_INC_DEBUG_H_
+
+#define debug_print(__exp, __frmt, __args...) \
+    do {                    \
+    if (__exp)                \
+        printf("%s: " __frmt "\n", __FUNCTION__, ##__args);   \
+    } while (0)
+
+#endif 
