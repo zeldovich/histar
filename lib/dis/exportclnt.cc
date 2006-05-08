2c98a9c3699f9c24ea577b7be90d6cad41c67c28
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-08 05:12:29 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2485 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ lib/dis/exportclnt.cc
@@ -0,0 +1,9 @@
+extern "C" {
+#include <inc/types.h>
+}
+
+int
+export_grant(uint64_t handle) 
+{
+    return -1;    
+}
---
+++ lib/dis/exportclnt.hh
@@ -0,0 +1,6 @@
+#ifndef JOS_INC_EXPORTCLNT_HH_
+#define JOS_INC_EXPORTCLNT_HH_
+
+int export_grant(uint64_t handle);
+
+#endif /*JOS_INC_EXPORTCLNT_HH_*/
