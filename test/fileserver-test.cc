9f799c3f3cc4af28d7e64b49162de5f2e854d299
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-09 20:57:48 +0000

    crud so test server compiles and runs on my machine (32-bit)
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2511 927ebb28-5305-0410-b3ca-c4b640df69b7

--- test/fileserver-test.cc
+++ test/fileserver-test.cc
@@ -10,6 +10,46 @@ extern "C" {
 
 #include <lib/dis/fileserver.hh>
 
+extern "C" {
+#include <netinet/in.h>
+#include <stdio.h>    
+#include <string.h>
+#include <assert.h>
+}
+
+#include <lib/dis/globallabel.hh>
+#include <lib/dis/proxydclnt.hh>
+#include <lib/dis/proxydsrv.hh>
+
+#include <inc/error.hh>
+
+global_label::global_label(const char *path) : serial_(0), string_(0)
+{
+    static const uint32_t  bufsize = 256;
+    char buf[bufsize];
+
+    ((uint32_t*)buf)[0] = htonl(0);
+    buf[4] = 1;
+    
+    serial_ = new char[5];
+    memcpy(serial_, buf, 5);
+    serial_len_ = 5;
+}
+
+const char *
+global_label::serial(void) const 
+{
+    return serial_;
+}
+    
+int         
+global_label::serial_len(void) const
+{
+    return serial_len_;    
+}
+
+
+global_label *fileserver_new_global(char *a) { return new global_label(a); }
 void fileserver_acquire(char *a, int b) { }
 
 static void 
