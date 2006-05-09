567e493fa6aeba3e06ef7f43822728a45c76afeb
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-09 00:16:05 +0000

    acquire handles for write
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2494 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/dis/fileserver_util.cc
+++ lib/dis/fileserver_util.cc
@@ -1,6 +1,7 @@
 extern "C" {
 #include <inc/fs.h>
 #include <stdio.h>
+#include <fcntl.h>
 }
 
 #include <inc/error.hh>
@@ -19,31 +20,37 @@ fileserver_acquire(char *path, int mode)
     label th_label;
     thread_cur_label(&th_label);
     
-    printf("seg_label %s\n", seg_label.to_string());
-    printf("th_label %s\n", th_label.to_string());
+    //printf("seg_label %s\n", seg_label.to_string());
+    //printf("th_label %s\n", th_label.to_string());
     
-    //int r = seg_label.compare(&th_label, label_leq_starhi);
-    //if (r < 0) {
-
     int r;
     for (uint64_t i = 0; i < seg_label.to_ulabel()->ul_nent; i++) {
         uint64_t h = LB_HANDLE(seg_label.to_ulabel()->ul_ent[i]);
         r = label_leq_starhi(seg_label.get(h), th_label.get(h));
-        if (r < 0) {
-            printf("h %ld\n", h);
+        if (r < 0)
             export_acquire(h);
-        }
     }
 
     for (uint64_t i = 0; i < th_label.to_ulabel()->ul_nent; i++) {
         uint64_t h = LB_HANDLE(th_label.to_ulabel()->ul_ent[i]);
         r = label_leq_starhi(seg_label.get(h), th_label.get(h));
-        if (r < 0) {
-            printf("h2 %ld\n", h);
+        if (r < 0)
             export_acquire(h);
-        }
     }
-    if (r & !r)
-    throw basic_exception("unable to aquire handles for %s", path);
-    //}
+    
+    if ((O_WRONLY & mode) || (O_RDWR & mode)) { 
+        for (uint64_t i = 0; i < th_label.to_ulabel()->ul_nent; i++) {
+            uint64_t h = LB_HANDLE(th_label.to_ulabel()->ul_ent[i]);
+            r = label_leq_starlo(th_label.get(h), seg_label.get(h));
+            if (r < 0)
+                export_acquire(h);
+        }
+    
+        for (uint64_t i = 0; i < seg_label.to_ulabel()->ul_nent; i++) {
+            uint64_t h = LB_HANDLE(seg_label.to_ulabel()->ul_ent[i]);
+            r = label_leq_starlo(th_label.get(h), seg_label.get(h));
+            if (r < 0)
+                export_acquire(h);
+        }    
+    }
 }
