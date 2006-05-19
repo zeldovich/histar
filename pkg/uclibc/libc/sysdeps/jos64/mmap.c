a92c6aefce9c74c1ad2750fb8b2ce47292c75376
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-19 04:29:00 +0000

    partial munmap() support
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2565 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/mmap.c
+++ pkg/uclibc/libc/sysdeps/jos64/mmap.c
@@ -49,43 +49,78 @@ mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset)
 int
 munmap(void *start, size_t length)
 {
-    struct u_segment_mapping usm;
-    int r = segment_lookup(start, &usm);
+    if ((length % PGSIZE) != 0) {
+	cprintf("munmap: unaligned unmap, va %p, length %ld\n", start, length);
+	__set_errno(EINVAL);
+	return -1;
+    }
+
+    struct u_segment_mapping omap;
+    int r = segment_lookup(start, &omap);
     if (r < 0) {
 	__set_errno(EINVAL);
 	return -1;
     }
 
-    struct cobj_ref seg = usm.segment;
-    uint64_t npage = usm.num_pages;
-    uint64_t flags = usm.flags;
-
-    if (!(flags & SEGMAP_ANON_MMAP)) {
+    if (!(omap.flags & SEGMAP_ANON_MMAP)) {
 	set_enosys();
 	return -1;
     }
 
-    if (ROUNDUP(length, PGSIZE) != npage * PGSIZE) {
-	cprintf("munmap: unmapping different length: length=%ld, npage=%ld\n",
-		length, npage);
-	__set_errno(EINVAL);
-	return -1;
+    /*
+     * original mapping (omap) can extend past [start .. start+length):
+     *
+     *    omap.va     start   start+length   omap.end
+     *       |----------|-----------|-----------|
+     *	         left                   right
+     */
+    uint64_t left_pages = (((uintptr_t)start) - ((uintptr_t)omap.va)) / PGSIZE;
+    uint64_t right_pages = omap.num_pages - length / PGSIZE - left_pages;
+
+    void *right_va = 0;
+    if (right_pages) {
+	uint64_t nbytes = right_pages * PGSIZE;
+	right_va = start + length;
+	r = segment_map(omap.segment,
+			(omap.start_page + left_pages) * PGSIZE + length,
+			omap.flags, &right_va, &nbytes,
+			SEG_MAPOPT_OVERLAP);
+	if (r < 0) {
+	    cprintf("munmap: unable to map right chunk: %s\n", e2s(r));
+	    __set_errno(EPERM);
+	    return -1;
+	}
     }
 
-    r = segment_unmap(start);
+    if (left_pages) {
+	uint64_t nbytes = left_pages * PGSIZE;
+	void *va = omap.va;
+	r = segment_map(omap.segment, omap.start_page * PGSIZE,
+			omap.flags, &va, &nbytes,
+			SEG_MAPOPT_REPLACE);
+    } else {
+	r = segment_unmap(omap.va);
+    }
+
     if (r < 0) {
 	cprintf("munmap: unable to unmap: %s\n", e2s(r));
+	if (right_va)
+	    segment_unmap(right_va);
+
 	__set_errno(EPERM);
 	return -1;
     }
 
-    sys_obj_unref(seg);
+    r = segment_lookup_obj(omap.segment.object, 0);
+    if (r == 0)
+	sys_obj_unref(omap.segment);
+
     return 0;
 }
 
 void *
 mremap(void *old_address, size_t old_size, size_t new_size, int may_move)
 {
-    set_enosys();
+    __set_errno(ENOMEM);
     return MAP_FAILED;
 }
