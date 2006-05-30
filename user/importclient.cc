2b5fc273ba2e6c9c4c7abf486edd627002fe33c7
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-05-30 20:24:28 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2616 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/importclient.cc
+++ user/importclient.cc
@@ -26,31 +26,12 @@ main (int ac, char **av)
     static char buffer[128];
 
     try {
-        /*
-        
-        
-        auth_agent aa = auth_agent("cad");
-        sign_agent sa = aa.sign_agent_new("bob signer", grant);
-        ver_agent va = aa.ver_agent_new("server ver", grant);
-        //sa.secret_is("bob", "some secret", strlen("some secret"));
-        
-        va.verify(0, 0, 0, 0);
-
-        struct cobj_ref test_seg;
-        error_check(segment_alloc(start_env->shared_container, 10,
-                         &test_seg, 0, 0, "test"));
-        va.taint(test_seg);
-        */
-        
         int64_t export_ct, manager_gt;
         error_check(export_ct = container_find(start_env->root_container, kobj_container, "exportd"));
         error_check(manager_gt = container_find(export_ct, kobj_gate, "manager"));
         uint64_t grant = handle_alloc();
                 
         import_managerc manager(COBJ(export_ct, manager_gt));
-        //export_clientc client = manager.client_new((char*)"bob", grant);
-        //export_segmentc seg = client.segment_new(host, port, path);
-
         import_segmentc seg = manager.segment_new(host, port, path, grant);
        
         int r = seg.read(buffer, sizeof(buffer), 0);
