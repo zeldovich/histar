b81e82ac224af37bcc55df545de33ecca02ebea5
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-07-13 22:56:31 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2676 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ inc/dis/omclient.hh
@@ -0,0 +1,21 @@
+#ifndef JOS_INC_DIS_OMCLIENT_H
+#define JOS_INC_DIS_OMCLIENT_H
+
+#include <inc/dis/omparam.h>
+#include <inc/container.h>
+
+class om_client
+{
+public:
+    om_client(const char *name);
+    bool observable(om_res *res, palid k);
+    bool modifiable(om_res *res, palid k);
+    
+private:
+    cobj_ref om_gate(void) const;
+    int om_test(om_res *res, palid k, bool observe);
+
+    char *name_;
+};
+
+#endif
---
+++ inc/dis/omparam.h
@@ -0,0 +1,12 @@
+#ifndef JOS_INC_DIS_OMPARAM_H
+#define JOS_INC_DIS_OMPARAM_H
+
+struct om_res {
+    char buf[32];
+};
+
+struct palid {
+    int id;
+};
+
+#endif
