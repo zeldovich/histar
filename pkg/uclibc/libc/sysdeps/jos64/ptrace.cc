c6b9033c3d597938fbea3f22aa71dbbe6440a489
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-07-18 20:25:18 +0000

    va_arg
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2692 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/ptrace.cc
+++ pkg/uclibc/libc/sysdeps/jos64/ptrace.cc
@@ -6,6 +6,7 @@ extern "C" {
 #include <inc/signal.h>
 #include <inc/debug_gate.h>
 
+#include <stdarg.h>
 #include <stdio.h>
 }
 
@@ -21,11 +22,23 @@ ptrace_on_signal(struct sigaction *sa, siginfo_t *si, struct sigcontext *sc)
 long int
 ptrace(enum __ptrace_request request, ...) __THROW
 {
+    pid_t pid;
+    void *addr, *data;
+
+    va_list ap;
+    va_start(ap, request);
+    pid = va_arg(ap, pid_t);
+    addr = va_arg(ap, void*);
+    data = va_arg(ap, void*);
+    va_end(ap);
+    
     switch (request) {
         case PTRACE_TRACEME:
 	    ptrace_traceme = 1;
 	    return 0;
+        case PTRACE_GETREGS:
         default:
+	    cprintf("ptrace: unknown request %d\n", request);
 	    print_backtrace();
 	    set_enosys();
 	    return -1;
