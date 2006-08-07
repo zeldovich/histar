aec0be333f6e948c682f3d6e8fb30d5e97963dc4
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-08-07 19:38:11 +0000

    fake dev stuff
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2735 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ user/devpt.cc
@@ -0,0 +1,155 @@
+extern "C" {
+#include <inc/lib.h>
+#include <inc/stdio.h>
+#include <inc/gateparam.h>
+#include <inc/gatefilesrv.h>
+#include <inc/syscall.h>
+#include <inc/devpt.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+}
+
+#include <inc/gatesrv.hh>
+#include <inc/labelutil.hh>
+#include <inc/pthread.hh>
+
+# define DEVPTS_SUPER_MAGIC	0x1cd1
+
+static uint64_t pts_ct = 0;
+static struct cobj_ref devptm_gt = COBJ(0, 0);
+
+static const uint64_t pts_table_entries = 16;
+
+static struct {
+    char inuse;
+    struct cobj_ref seg;
+    struct cobj_ref gate;
+} pts_table[pts_table_entries];
+
+pthread_mutex_t pts_table_mu;
+
+static void __attribute__((noreturn))
+pts_gate(void *arg, struct gate_call_data *parm, gatesrv_return *gr)
+{
+    struct pts_gate_args *args = (struct pts_gate_args *)parm->param_buf;
+    uint64_t id = (uint64_t)arg;
+    
+    try {
+	scoped_pthread_lock lock(&pts_table_mu);
+	switch(args->pts_args.op) {
+	case gf_call_open: {
+	    struct gatefd_args *args2 = (struct gatefd_args *)parm->param_buf;
+	    args2->ret.obj0 = pts_table[id].gate;
+	    args2->ret.obj1 =  pts_table[id].seg;
+	    args2->ret.op = gf_ret_pts;
+	    break;
+	}
+	case pts_op_seg:
+	    pts_table[id].seg = args->pts_args.arg;
+	    args->pts_args.ret = 0;
+	    break;
+	case pts_op_close: {
+	    struct cobj_ref gt = pts_table[id].gate;
+	    memset(&pts_table[id], 0, sizeof(pts_table[id]));
+	    error_check(sys_obj_unref(gt));
+	    args->pts_args.ret = 0;
+	    break;
+	}
+	default:
+	    args->pts_args.ret = -1;
+	    break;
+	}
+    } catch (basic_exception e) {
+	cprintf("pts_gate: %s\n", e.what());
+	args->pts_args.ret = -1;
+    }
+    gr->ret(0,0,0);
+}
+
+static void
+ptm_handle_open(struct gatefd_args *args)
+{
+    label ver(1);
+    thread_cur_verify(&ver);
+    
+    scoped_pthread_lock lock(&pts_table_mu);
+    for (uint64_t i = 0; i < pts_table_entries; i++) {
+	if (!pts_table[i].inuse) {
+	    pts_table[i].inuse = 1;
+	    
+	    char buf[32];
+	    snprintf(&buf[0], sizeof(buf), "%ld", i);
+	    
+	    label th_l, th_cl;
+	    thread_cur_label(&th_l);
+	    thread_cur_clearance(&th_cl);
+	    struct cobj_ref pts_gt = gate_create(pts_ct, buf, &th_l, &th_cl, 
+						    &pts_gate, (void *) i);
+	    pts_table[i].gate = pts_gt;
+		    
+	    args->ret.obj0 = devptm_gt;
+	    args->ret.obj1 = pts_gt;
+	    args->ret.op = gf_ret_ptm;
+	    return;
+	}
+    }
+    
+    args->ret.op = gf_ret_error;
+    return;
+}
+
+static void __attribute__((noreturn))
+ptmx_gate(void *arg, struct gate_call_data *parm, gatesrv_return *gr)
+{
+    struct gatefd_args *args = (struct gatefd_args *)parm->param_buf;
+    
+    switch(args->call.op) {
+    case gf_call_open:
+	ptm_handle_open(args);
+	break;
+    default:
+	break;
+    }
+    gr->ret(0, 0, 0);
+}
+
+int
+main (int ac, char **av)
+{
+    if (ac < 2) {
+	cprintf("usage: %s container [category]\n", av[0]);
+	exit(-1);
+    }
+    uint64_t ct = atol(av[1]);
+    memset(pts_table, 0, sizeof(pts_table));
+    pthread_mutex_init(&pts_table_mu, 0);
+    
+    try {
+	label th_l, th_cl;
+	thread_cur_label(&th_l);
+	thread_cur_clearance(&th_cl);
+	devptm_gt = gate_create(ct, "ptmx", &th_l, &th_cl, &ptmx_gate, 0);
+	
+	label l(1);
+	uint64_t ct2 = 0;
+	error_check(ct2 = sys_container_alloc(ct, l.to_ulabel(), "pts", 0, CT_QUOTA_INF));
+	pts_ct = ct2;
+
+	struct fs_object_meta m;
+
+	error_check(sys_obj_get_meta(COBJ(ct, pts_ct), &m));
+	m.f_type = DEVPTS_SUPER_MAGIC;
+	error_check(sys_obj_set_meta(COBJ(ct, pts_ct), 0, &m));
+    } catch (basic_exception e) {
+	cprintf("devpt fatal: %s\n", e.what());
+	exit(-1);
+    }
+    
+    if (ac == 3)
+	thread_drop_star(atol(av[2]));
+    
+    thread_halt();
+}
