9804126b22deec7848b4ff3fc5c2069cf20cad10
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-09-04 23:13:12 +0000

    histar -l option
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2954 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/ls.c
+++ user/ls.c
@@ -3,27 +3,66 @@
 #include <inc/lib.h>
 #include <inc/error.h>
 #include <inc/fs.h>
+#include <inc/syscall.h>
+#include <string.h>
+
+const char *kobj_str[] = {
+    "c",
+    "t",
+    "g",
+    "s",
+    "a",
+    "n",
+    "l",
+    "d",
+    "error ntypes",
+    "error any",
+};
+
+static struct fs_inode dir = {{0, 0}};
+static char long_mode = 0;
+
+static int
+on_option(const char *str)
+{
+    if (!strcmp(str, "l")) {
+	long_mode = 1;
+	return 0;
+    } 
+    cprintf("unknown option: %s\n", str);
+    return -E_INVAL;
+}
+
+
+static int
+on_dirname(const char *str)
+{
+    int r = fs_namei(str, &dir);
+    if (r < 0) {
+	printf("cannot lookup %s: %s\n", str, e2s(r));
+	return r;
+    }
+    return 0;
+}
 
 int
 main(int ac, char **av)
 {
     int r;
-    struct fs_inode dir;
 
-    if (ac == 1) {
-	dir = start_env->fs_cwd;
-    } else if (ac == 2) {
-	const char *dname = av[1];
-	r = fs_namei(dname, &dir);
-	if (r < 0) {
-	    printf("cannot lookup %s: %s\n", dname, e2s(r));
-	    return r;
+    for (int i = 1; i < ac; i++) {
+	if (av[i][0] == '-') {
+	    if (on_option(&av[i][1]) < 0)
+		return -1;
+	} else {
+	    if (on_dirname(av[i]) < 0)
+		return -1;
 	}
-    } else {
-	printf("Usage: %s [dirname]\n", av[0]);
-	return -1;
     }
-
+    
+    if (dir.obj.object == 0)
+	dir = start_env->fs_cwd;
+    
     struct fs_readdir_state s;
     r = fs_readdir_init(&s, dir);
     if (r < 0) {
@@ -41,6 +80,28 @@ main(int ac, char **av)
 	if (r == 0)
 	    break;
 
-	printf("%s\n", de.de_name);
+	if (long_mode) {
+	    const char *type = 0;
+	    kobject_type_t kobj = sys_obj_get_type(de.de_inode.obj);
+	    if ((int32_t)kobj < 0)
+		type = "?";
+	    else
+	    type = kobj_str[kobj];
+	    
+	    struct ulabel *l = label_alloc();
+	    const char *label = 0;
+	    r = sys_obj_get_label(de.de_inode.obj, l);
+	    if (r < 0)
+		label = "?";
+	    else
+		label = label_to_string(l);
+	    label_free(l);
+
+	    char id[16];
+	    snprintf(id, 32, "%ld", de.de_inode.obj.object);
+	    printf("%-20.20s %-1.1s %-8.8s %s\n", de.de_name, type, id, label);
+	} else {
+	    printf("%s\n", de.de_name);
+	}
     }
 }
