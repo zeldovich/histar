49927fef6151e0c9225bb31393f92eb5ad04541d
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-09-04 23:15:45 +0000

    sshd init
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@2955 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ user/sshdi.cc
@@ -0,0 +1,66 @@
+extern "C" {
+#include <inc/lib.h>
+#include <inc/stdio.h>
+
+#include <stdlib.h>
+}
+
+#include <inc/error.hh>
+#include <inc/labelutil.hh>
+#include <inc/spawn.hh>
+
+int
+main (int ac, char **av)
+{
+    try {
+	if (ac < 2) {
+	    cprintf("usage: %s grant-category\n", av[0]);
+	    exit(-1);
+	}
+	uint64_t grant_root = atol(av[1]);
+	
+	int r;
+	int64_t exit_code;
+	struct fs_inode ino;
+	
+	label ds(3);
+	ds.set(grant_root, LB_LEVEL_STAR);
+	start_env->fs_cwd = start_env->fs_root;
+	
+	r = fs_namei("/bin/tar", &ino);
+	if (r < 0)
+	    throw error(r, "cannot fs_lookup /bin/tar");
+	
+	const char *argv0[] = { "/bin/tar", "xvf", "/bin/ssh.tar" };
+	child_process cp = spawn(start_env->shared_container, ino,
+				 0, 0, 0,
+				 3, &argv0[0],
+				 0, 0,
+				 0, &ds, 0, 0, 0);
+	error_check(process_wait(&cp, &exit_code));
+	error_check(exit_code);
+	
+	r = fs_namei("/bin/sshd", &ino);
+	if (r < 0)
+	    throw error(r, "cannot fs_lookup /bin/sshd");
+	
+	// XXX need login stuff to work with sshd
+	//ds.set(grant_root, 1);
+
+	const char *argv1[] = { "/bin/sshd", "-f", "/etc/sshd_config", "-r" };
+	cp = spawn(start_env->shared_container, ino,
+		   0, 0, 0,
+		   4, &argv1[0],
+		   0, 0,
+		   0, &ds, 0, 0, 0);
+	error_check(process_wait(&cp, &exit_code));
+	error_check(exit_code);
+	
+	return 0;
+    } catch (std::exception &e) {
+	cprintf("sshdi: main: %s\n", e.what());
+	return -1;
+    }
+}
+
+    
