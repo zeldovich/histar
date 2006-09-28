ebf110891fae797f3a11a32d9a2370f999bc77cb
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-09-28 06:09:13 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3051 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/devel.mk
+++ conf/embedbin/devel.mk
@@ -4,8 +4,10 @@ EMBEDBIN_INIT = conf/embedbin/devel.init
 KERN_BINFILES += user/init.init
 KERN_BINFILES += user/devinit user/devpt user/devnull user/reboot
 KERN_BINFILES += user/netdi
-KERN_BINFILES += user/sshd user/ssh.tar user/tar user/sshdi 
-KERN_BINFILES += user/vim user/wget
+KERN_BINFILES += user/sshd user/ssh.tar user/sshdi 
+KERN_BINFILES += user/vim user/wget user/tar user/gzip
 KERN_BINFILES += user/bash
+KERN_BINFILES += user/gcc.tar.gz
+KERN_BINFILES += user/make
 
 include conf/embedbin/embedbins.mk
