e1fc78f9c60febeec28cb639378f77e1d8be1f6f
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-11-17 03:48:40 +0000

    avoid conflicting with the builtin log() function
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3213 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/log.c
+++ kern/kern/log.c
@@ -21,7 +21,7 @@ static struct {
     uint64_t on_disk;
     struct hashtable disk_map;
     struct hashentry map_back[LOG_PAGES];
-} log;
+} the_log;
 
 static int
 log_write_to_disk(struct node_list *nodes, uint64_t * count)
@@ -31,8 +31,8 @@ log_write_to_disk(struct node_list *nodes, uint64_t * count)
 
     struct btree_node *node;
     TAILQ_FOREACH(node, nodes, node_log_link) {
-	assert(node->block.offset < log.byteoff ||
-	       node->block.offset >= log.byteoff + log.npages * PGSIZE);
+	assert(node->block.offset < the_log.byteoff ||
+	       node->block.offset >= the_log.byteoff + the_log.npages * PGSIZE);
 
 	s = stackwrap_disk_io(op_write, node, BTREE_BLOCK_SIZE,
 			      node->block.offset);
@@ -51,21 +51,21 @@ log_write_to_log(struct node_list *nodes, uint64_t * count, offset_t off)
 {
     disk_io_status s;
     uint64_t n = 0;
-    assert(log.byteoff <= off);
+    assert(the_log.byteoff <= off);
 
     struct btree_node *node;
     TAILQ_FOREACH(node, nodes, node_log_link) {
 	if (!node->block.is_dirty)
 	    continue;
 
-	assert(off + n * PGSIZE < log.npages * PGSIZE + log.byteoff);
+	assert(off + n * PGSIZE < the_log.npages * PGSIZE + the_log.byteoff);
 	s = stackwrap_disk_io(op_write, node, BTREE_BLOCK_SIZE,
 			      off + n * PGSIZE);
 	if (!SAFE_EQUAL(s, disk_io_success)) {
 	    *count = n;
 	    return -E_IO;
 	}
-	hash_put(&log.disk_map, node->block.offset, off + n * PGSIZE);
+	hash_put(&the_log.disk_map, node->block.offset, off + n * PGSIZE);
 	node->block.is_dirty = 0;
 	n++;
     }
@@ -130,7 +130,7 @@ int
 log_try_read(offset_t byteoff, void *page)
 {
     struct btree_node *node;
-    TAILQ_FOREACH(node, &log.nodes, node_log_link) {
+    TAILQ_FOREACH(node, &the_log.nodes, node_log_link) {
 	if (byteoff == node->block.offset) {
 	    memcpy(page, node, PGSIZE);
 	    return 0;
@@ -139,7 +139,7 @@ log_try_read(offset_t byteoff, void *page)
 
     // look in on-disk log
     offset_t log_off;
-    if (hash_get(&log.disk_map, byteoff, &log_off) < 0)
+    if (hash_get(&the_log.disk_map, byteoff, &log_off) < 0)
 	return -E_NOT_FOUND;
     disk_io_status s =
 	stackwrap_disk_io(op_read, page, BTREE_BLOCK_SIZE, log_off);
@@ -156,30 +156,30 @@ log_write(struct btree_node *node)
     char found = 0;
     struct btree_node *store;
 
-    TAILQ_FOREACH(store, &log.nodes, node_log_link) {
+    TAILQ_FOREACH(store, &the_log.nodes, node_log_link) {
 	if (store->block.offset == node->block.offset) {
-	    TAILQ_REMOVE(&log.nodes, store, node_log_link);
+	    TAILQ_REMOVE(&the_log.nodes, store, node_log_link);
 	    found = 1;
 	    break;
 	}
     }
     if (!found) {
-	if (log.in_mem == log.max_mem) {
+	if (the_log.in_mem == the_log.max_mem) {
 	    if ((r = log_flush()) < 0)
 		return r;
-	    uint64_t count = log_free_list(&log.nodes);
-	    log.in_mem -= count;
-	    assert(log.in_mem == 0);
+	    uint64_t count = log_free_list(&the_log.nodes);
+	    the_log.in_mem -= count;
+	    assert(the_log.in_mem == 0);
 	}
 	if ((r = page_alloc((void **) &store)) < 0)
 	    return r;
 	memset(store, 0, PGSIZE);
-	log.in_mem++;
+	the_log.in_mem++;
     }
 
     memcpy(store, node,
 	   BTREE_NODE_SIZE(node->tree->order, node->tree->s_key));
-    TAILQ_INSERT_TAIL(&log.nodes, store, node_log_link);
+    TAILQ_INSERT_TAIL(&the_log.nodes, store, node_log_link);
     store->block.is_dirty = 1;
     return 0;
 }
@@ -188,23 +188,23 @@ void
 log_free(offset_t byteoff)
 {
     struct btree_node *node;
-    TAILQ_FOREACH(node, &log.nodes, node_log_link) {
+    TAILQ_FOREACH(node, &the_log.nodes, node_log_link) {
 	if (byteoff == node->block.offset) {
-	    TAILQ_REMOVE(&log.nodes, node, node_log_link);
+	    TAILQ_REMOVE(&the_log.nodes, node, node_log_link);
 	    page_free(node);
-	    log.in_mem--;
+	    the_log.in_mem--;
 	    break;
 	}
     }
 
     offset_t log_off;
-    if (hash_get(&log.disk_map, byteoff, &log_off) >= 0) {
-	// We freed some block that's present in our on-disk log..
+    if (hash_get(&the_log.disk_map, byteoff, &log_off) >= 0) {
+	// We freed some block that's present in our on-disk the_log..
 	// To avoid overwriting any new data that will be written to
 	// this soon-to-be-free space, the log must be flushed right
 	// after the free list is committed.
-	log.must_apply = 1;
-	hash_del(&log.disk_map, byteoff);
+	the_log.must_apply = 1;
+	hash_del(&the_log.disk_map, byteoff);
     }
 }
 
@@ -213,18 +213,18 @@ log_flush(void)
 {
     int r;
 
-    if (log.npages <= log.in_mem + log.on_disk + 1)
+    if (the_log.npages <= the_log.in_mem + the_log.on_disk + 1)
 	panic("log_flush: out of log space");
 
     uint64_t count;
-    uint64_t off = log.byteoff + log.on_disk * PGSIZE;
-    if ((r = log_write_to_log(&log.nodes, &count, off)) < 0)
+    uint64_t off = the_log.byteoff + the_log.on_disk * PGSIZE;
+    if ((r = log_write_to_log(&the_log.nodes, &count, off)) < 0)
 	return r;
 
-    assert(count <= log.in_mem);
-    log.on_disk += count;
+    assert(count <= the_log.in_mem);
+    the_log.on_disk += count;
 
-    return log.on_disk;
+    return the_log.on_disk;
 }
 
 int
@@ -233,10 +233,10 @@ log_apply_disk(uint64_t n_nodes)
     int r;
     struct node_list nodes;
     uint64_t count, n;
-    offset_t off = log.byteoff;
+    offset_t off = the_log.byteoff;
 
     while (n_nodes) {
-	n = MIN(n_nodes, log.max_mem);
+	n = MIN(n_nodes, the_log.max_mem);
 	n_nodes -= n;
 	TAILQ_INIT(&nodes);
 	if ((r = log_read_log(off, n, &nodes)) < 0) {
@@ -261,21 +261,21 @@ log_apply_mem(void)
 {
     int r;
     uint64_t count;
-    if ((r = log_write_to_disk(&log.nodes, &count)) < 0)
+    if ((r = log_write_to_disk(&the_log.nodes, &count)) < 0)
 	return r;
 
     struct btree_node *node;
-    TAILQ_FOREACH(node, &log.nodes, node_log_link)
-	hash_del(&log.disk_map, node->block.offset);
+    TAILQ_FOREACH(node, &the_log.nodes, node_log_link)
+	hash_del(&the_log.disk_map, node->block.offset);
 
-    assert(count == log_free_list(&log.nodes));
-    log.in_mem -= count;
-    assert(log.in_mem == 0);
+    assert(count == log_free_list(&the_log.nodes));
+    the_log.in_mem -= count;
+    assert(the_log.in_mem == 0);
 
-    if (log.disk_map.size) {
+    if (the_log.disk_map.size) {
 	struct node_list in_log;
 	TAILQ_INIT(&in_log);
-	log_read_map(&log.disk_map, &in_log);
+	log_read_map(&the_log.disk_map, &in_log);
 	log_write_to_disk(&in_log, &count);
 	log_free_list(&in_log);
     }
@@ -287,24 +287,24 @@ log_apply_mem(void)
 int
 log_must_apply()
 {
-    return log.must_apply;
+    return the_log.must_apply;
 }
 
 void
 log_init(void)
 {
-    if (!log.tailq_inited) {
-	TAILQ_INIT(&log.nodes);
-	log.tailq_inited = 1;
+    if (!the_log.tailq_inited) {
+	TAILQ_INIT(&the_log.nodes);
+	the_log.tailq_inited = 1;
     }
 
-    log_free_list(&log.nodes);
-    memset(&log.map_back[0], 0, sizeof(log.map_back));
-    hash_init(&log.disk_map, log.map_back, LOG_PAGES);
-    log.in_mem = 0;
-    log.on_disk = 0;
-    log.byteoff = LOG_OFFSET * PGSIZE;
-    log.npages = LOG_PAGES;
-    log.max_mem = LOG_MEMORY;
-    log.must_apply = 0;
+    log_free_list(&the_log.nodes);
+    memset(&the_log.map_back[0], 0, sizeof(the_log.map_back));
+    hash_init(&the_log.disk_map, the_log.map_back, LOG_PAGES);
+    the_log.in_mem = 0;
+    the_log.on_disk = 0;
+    the_log.byteoff = LOG_OFFSET * PGSIZE;
+    the_log.npages = LOG_PAGES;
+    the_log.max_mem = LOG_MEMORY;
+    the_log.must_apply = 0;
 }
