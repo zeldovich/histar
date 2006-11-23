39d15e3c95594e24eac83f3bc14e642d515c3f4c
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-11-23 05:48:15 +0000

    cool, side-by-side execution of two kernels in a single linux process
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3258 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/lnx64/driver.c
+++ kern/arch/lnx64/driver.c
@@ -7,21 +7,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+extern void __k1_lnx64_init();
+extern void __k2_lnx64_init();
+
+extern void __k1_user_init();
+extern void __k2_user_init();
+
+extern void __k1_schedule();
+extern void __k2_schedule();
+
+extern void __k1_thread_run();
+extern void __k2_thread_run();
+
+extern void *__k1_cur_thread;
+extern void *__k2_cur_thread;
+
 int
 main(int ac, char **av)
 {
-    if (ac != 3) {
-	printf("Usage: %s disk-file kernel-args\n", av[0]);
+    if (ac != 2) {
+	printf("Usage: %s disk-file\n", av[0]);
 	exit(-1);
     }
 
     const char *disk_pn = av[1];
-    const char *cmdline = av[2];
+    const char *cmdline = "pstate=discard";
 
-    lnx64_init(disk_pn, cmdline, 64 * 1024 * 1024);
+    // Technically this is pretty bad, because they're sharing the disk.
+    // But, we aren't using the disk for anything yet, so whatever..
+    __k1_lnx64_init(disk_pn, cmdline, 64 * 1024 * 1024);
+    __k2_lnx64_init(disk_pn, cmdline, 64 * 1024 * 1024);
 
-    user_init();
+    __k1_user_init();
+    __k2_user_init();
+
     printf("Hmm, this probably isn't going to work...\n");
-    schedule();
-    thread_run(cur_thread);
+    __k1_schedule();
+    __k2_schedule();
+
+    __k1_thread_run(__k1_cur_thread);
 }
