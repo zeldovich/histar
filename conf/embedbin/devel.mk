23659f2a9d4dfcefba96946230c283f8b2c6cc6a
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-11-27 19:43:11 +0000

    add ureboot, scp
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3272 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/devel.mk
+++ conf/embedbin/devel.mk
@@ -2,8 +2,8 @@ EMBEDBIN_ID = conf/embedbin/devel.mk
 EMBEDBIN_INIT = conf/embedbin/devel.init
 
 KERN_BINFILES += user/init.init
-KERN_BINFILES += user/devinit user/devpt user/reboot
-KERN_BINFILES += user/sshd user/ssh.tar
+KERN_BINFILES += user/devinit user/devpt user/reboot user/ureboot
+KERN_BINFILES += user/sshd user/ssh.tar user/scp
 KERN_BINFILES += user/vim 
 KERN_BINFILES += user/bash
 KERN_BINFILES += user/sshdi
