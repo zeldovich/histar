17be1eba27f25035276d88f9ef77fdcdbfbfea25
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-11-30 01:30:24 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3296 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/lnx64/setjmp.h
+++ kern/arch/lnx64/setjmp.h
@@ -4,6 +4,7 @@
 #include <inc/types.h>
 
 struct jos_jmp_buf {
+#if __WORDSIZE == 64
     uint64_t jb_rip;
     uint64_t jb_rsp;
     uint64_t jb_rbp;
@@ -12,6 +13,14 @@ struct jos_jmp_buf {
     uint64_t jb_r13;
     uint64_t jb_r14;
     uint64_t jb_r15;
+#else
+    uint32_t jb_rip;
+    uint32_t jb_rsp;
+    uint32_t jb_ebp;
+    uint32_t jb_ebx;
+    uint32_t jb_esi;
+    uint32_t jb_edi;
+#endif
 };
 
 #endif
