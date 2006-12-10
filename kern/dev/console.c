d9659eabd51a08e3b3461d842402d95634591e57
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-12-10 04:10:19 +0000

    per eddie on asbestos cvs
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3370 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/dev/console.c
+++ kern/dev/console.c
@@ -376,7 +376,7 @@ static uint8_t shiftmap[256] = {
   NO, NO, NO, NO, NO, NO, NO, '7',	// 0x40
   '8', '9', '-', '4', '5', '6', '+', '1',
   '2', '3', '0', '.', NO, NO, NO, NO,	// 0x50
-  [0x97] KEY_HOME,[0x9C] '\n' /*KP_Enter */ ,
+  [0xC7] KEY_HOME,[0x9C] '\n' /*KP_Enter */ ,
   [0xB5] '/' /*KP_Div */ ,[0xC8] KEY_UP,
   [0xC9] KEY_PGUP,[0xCB] KEY_LF,
   [0xCD] KEY_RT,[0xCF] KEY_END,
@@ -394,7 +394,7 @@ static uint8_t ctlmap[256] = {
   C ('D'), C ('F'), C ('G'), C ('H'), C ('J'), C ('K'), C ('L'), NO,
   NO, NO, NO, C ('\\'), C ('Z'), C ('X'), C ('C'), C ('V'),
   C ('B'), C ('N'), C ('M'), NO, NO, C ('/'), NO, NO,
-  [0x97] KEY_HOME,
+  [0xC7] KEY_HOME,
   [0xB5] C ('/'),[0xC8] KEY_UP,
   [0xC9] KEY_PGUP,[0xCB] KEY_LF,
   [0xCD] KEY_RT,[0xCF] KEY_END,
