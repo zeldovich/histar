de44a70eecf5d215801c0d9192d462a811c7b7bb
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-12-11 22:42:36 +0000

    remove crud
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3373 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/ssldclnt.hh
+++ inc/ssldclnt.hh
@@ -1,16 +1,6 @@
 #ifndef JOS_INC_SSLDCLNT_HH
 #define JOS_INC_SSLDCLNT_HH
 
-struct cobj_ref ssld_shared_server(void);
-struct cobj_ref ssld_new_server(uint64_t ct, const char *server_pem, 
-				const char *password, const char *calist_pem, 
-				const char *dh_pem, label *cs, label *ds, 
-				label *cr, label *dr, label *co);
-
-struct cobj_ref ssld_shared_cow(void);
-struct cobj_ref ssld_cow_call(struct cobj_ref gate, uint64_t ct, 
-			      label *cs, label *ds, label *dr);
-
 void ssld_taint_cow(struct cobj_ref cow_gate, 
 		    struct cobj_ref cipher_biseg, struct cobj_ref plain_biseg,
 		    uint64_t root_ct, uint64_t taint);
