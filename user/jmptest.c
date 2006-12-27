15c11e202e8f08ea367f99698c22d28a3bc1428b
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2006-12-27 03:34:44 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3381 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/jmptest.c
+++ user/jmptest.c
@@ -1,18 +1,24 @@
 #include <inc/syscall.h>
-#include <inc/stdio.h>
 #include <inc/setjmp.h>
+
+#include <stdio.h>
 
 int
 main(int ac, char **av)
 {
-    struct jmp_buf jb;
+    struct jos_jmp_buf jb;
 
-    int r = setjmp(&jb);
+    int r = jos_setjmp(&jb);
     printf("jmptest: setjmp(): %d\n", r);
 
     if (r == 0) {
 	printf("jmptest: longjmp'ing back\n");
-	longjmp(&jb, 7);
+	jos_longjmp(&jb, 0);
+    }
+
+    if (r == 1) {
+	printf("jmptest: longjmp'ing back again\n");
+	jos_longjmp(&jb, 2);
     }
 
     return 0;
