e836fc6f3e25fc5054fe97d4f363ced5fc28d6f1
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-13 21:19:27 +0000

    print panic messages to stderr rather than console
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3440 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/panic.c
+++ lib/panic.c
@@ -1,21 +1,22 @@
-#include <inc/stdio.h>
-#include <inc/syscall.h>
 #include <inc/assert.h>
 #include <inc/lib.h>
+#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 void
 _panic(const char *file, int line, const char *fmt, ...)
 {
+	extern char *__progname;
 	va_list ap;
 
 	va_start(ap, fmt);
 
 	// Print the panic message
-	cprintf("user panic at %s:%d: ", file, line);
-	vcprintf(fmt, ap);
-	cprintf("\n");
+	fprintf(stderr, "%s: user panic at %s:%d: ",
+		__progname ? : "unknown", file, line);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
 
 	print_backtrace();
 
