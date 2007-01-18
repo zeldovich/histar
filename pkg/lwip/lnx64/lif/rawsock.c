7e7ba4bcd2c5425f2be59161150d3e9b94760136
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-18 08:26:26 +0000

    promisc mode
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3481 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/lwip/lnx64/lif/rawsock.c
+++ pkg/lwip/lnx64/lif/rawsock.c
@@ -16,7 +16,56 @@
 #include <string.h>
 #include <stdio.h>
 #include <errno.h>
+#include <stdlib.h>
 
+// XXX
+static struct ifreq ifr_cleanup;
+static int sock_cleanup;
+
+static void
+raw_cleanup(void)
+{
+    if (ioctl(sock_cleanup, SIOCSIFFLAGS, &ifr_cleanup) < 0)
+	perror("linux_cleanup: ioctl");
+}
+
+static int
+raw_enable_permisc(int s, const char *iface_alias)
+{
+    struct ifreq ifr;
+    strncpy(ifr.ifr_name, iface_alias, IFNAMSIZ);
+    if (ioctl(s, SIOCGIFINDEX, &ifr) < 0) {
+	printf("ioctl error: %s", strerror(errno));
+	return -1;
+    }
+
+    // why doesn't the 'new' way work?
+    struct packet_mreq pr;
+    memset(&pr, 0, sizeof(pr));
+    pr.mr_ifindex = ifr.ifr_ifindex;
+    pr.mr_type = PACKET_MR_PROMISC;
+    if (setsockopt(s, SOL_PACKET, PACKET_ADD_MEMBERSHIP,
+		   &pr, sizeof(pr) < 0)) {
+	
+	// try old fashion way
+	struct ifreq ifr2;
+	strncpy(ifr2.ifr_name, iface_alias, IFNAMSIZ);
+	if (ioctl(s, SIOCGIFFLAGS, &ifr2) < 0) {
+	    printf("ioctl error: %s", strerror(errno));
+	    return -1;
+	}
+	memcpy(&ifr_cleanup, &ifr2, sizeof(ifr_cleanup));
+	sock_cleanup = s;
+	ifr2.ifr_flags |= IFF_PROMISC;
+	if (ioctl(s, SIOCSIFFLAGS, &ifr2) < 0) {
+	    printf("ioctl error: %s", strerror(errno));
+	    return -1;
+	}
+	atexit(raw_cleanup);
+    }
+
+    return 0;
+}
 
 int
 raw_socket(const char *iface_alias, char *mac_addr)
@@ -36,6 +85,9 @@ raw_socket(const char *iface_alias, char *mac_addr)
     sll.sll_ifindex = ifr.ifr_ifindex;
     sll.sll_protocol = htons(ETH_P_ALL);
     if (bind(s, (struct sockaddr *) &sll, sizeof(sll)) < 0)
+	return -1;
+
+    if (raw_enable_permisc(s, iface_alias) < 0)
 	return -1;
     
     return s;
