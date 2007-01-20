b8325f1395cf18261a085e15dc6122f5adbf6afb
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-20 00:07:38 +0000

    tried to cleanup the network benchmark situation...
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3499 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ lib/netbench.cc
@@ -0,0 +1,35 @@
+extern "C" {
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+}
+
+#include <inc/error.hh>
+#include <inc/netbench.hh>
+
+void
+nb_read(int s, void *b, int count)
+{
+    while (count) {
+	int r = read(s, b, count);
+	if (r < 0)
+	    throw basic_exception("read: %s", strerror(errno));
+	if (r == 0)
+	    throw basic_exception("read: EOF");
+	count -= r;
+    }
+}
+
+void
+nb_write(int s, void *b, int count)
+{
+    while (count) {
+	int r = write(s, b, count);
+	if (r < 0)
+	    throw basic_exception("write: %s", strerror(errno));
+	if (r == 0)
+	    throw basic_exception("write: unable to write");
+	count -= r;
+    }
+}
--- user/sock_bench.cc
+++ user/sock_bench.cc
@@ -6,6 +6,12 @@
 #define LINUX 1
 #endif
 
+#ifdef LWIP_LIB
+#define LWIP 1
+#else
+#define LWIP 0
+#endif
+
 extern "C" {
 #if JOS64
 #include <inc/lib.h>
@@ -19,31 +25,29 @@ extern "C" {
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
+#if LWIP
+#include <lif/socket.h>
+#include <lwip/inet.h>
+#include <lif/init.h>
+#include <arch/sys_arch.h>
+#else
 #include <sys/socket.h>
 #include <netinet/in.h>
+#endif
 
 #if LINUX
 #include <pthread.h>
 #endif
 }
 
-#if JOS64
-#include <inc/error.hh>
-#endif
+#include <inc/netbench.hh>
+#include <inc/errno.hh>
 
-static char buf[4096];
-static uint64_t byte_count = 0;
-
-static const char threaded = 0;
-
-#define err_exit(__exp, __frmt, __args...)				\
-    do {								\
-	if (__exp) {							\
-	    printf("error %s: " __frmt "\n", __FUNCTION__, ##__args);	\
-	    exit(1);							\
-	}								\
-    } while (0)
+static char threaded = 1;
+static int byte_count = 100;
+static int iter_count = 5;
 
 #if JOS64
 static void
@@ -52,13 +56,40 @@ static void*
 #endif
 server(void *arg)
 {
+    static char buf[4096];
     int s = (int)(int64_t)arg;
+    
     if (byte_count) {
-	int r = write(s, buf, byte_count);
-	if (r < 0 || (uint32_t) r < byte_count)
-	    err_exit(1, "write error: %s\n", strerror(errno));
+	try {
+	    for (int i = 0; i < iter_count; i++) {
+		nb_write(s, buf, byte_count);
+		nb_read(s, buf, byte_count);
+	    }
+	} catch (std::exception &e) {
+	    printf("server error: %s\n", e.what());
+	}
     }
     close(s);
+}
+
+static void
+start_server_thread(int s)
+{
+    int r;
+#if JOS64
+    struct cobj_ref t;
+    r = thread_create(start_env->proc_container, &server,
+		      (void*) (int64_t) s, &t, "server");
+    if (r < 0) {
+	printf("cannot spawn client thread: %s\n", e2s(r));
+	close(s);
+    } else {
+	fd_give_up_privilege(s);
+    }
+#else 
+    pthread_t t;
+    r = pthread_create(&t, 0, server, (void *)(int64_t)s);
+#endif
 }
 
 int
@@ -66,29 +97,47 @@ main (int ac, char **av)
 {
     uint16_t port = 9999;
 
+#if LWIP
+    // VMWare MAC
+    const char mac_addr[6] = { 0x00, 0x50, 0x56, 0xC0, 0x00, 0x10 };
+    char *iface = av[1];
+    printf("sock_bench: lwip_init, iface %s, MAC %02X:%02X:%02X:%02X:%02X:%02X\n",
+	   iface, mac_addr[0] & 0xFF, mac_addr[1] & 0xFF, mac_addr[2] & 0xFF, 
+	   mac_addr[3] & 0xFF, mac_addr[4] & 0xFF, mac_addr[5] & 0xFF);
+    error_check(lwip_init(iface, mac_addr));
+#endif
+
     int c;
-    while ((c = getopt(ac, av, "b:")) != -1) {
+    while ((c = getopt(ac, av, "p:b:i:t:")) != -1) {
 	switch(c) {
 	case 'b':
 	    byte_count = atoi(optarg);
 	    break;
+	case 'i':
+	    iter_count = atoi(optarg);
+	    break;
+	case 'p':
+	    port = atoi(optarg);
+	    break;
+	case 't':
+	    threaded = atoi(optarg);
+	    break;
 	}
     }
     
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    err_exit(s < 0, "cannot create socket: %s", strerror(errno));
+    int s;
+    errno_check(s = socket(AF_INET, SOCK_STREAM, 0));
     
     struct sockaddr_in sin;
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = htonl(INADDR_ANY);
     sin.sin_port = htons(port);
-    int r = bind(s, (struct sockaddr *)&sin, sizeof(sin));
-    err_exit(r < 0, "cannot bind socket: %s", strerror(errno));
+    errno_check(bind(s, (struct sockaddr *)&sin, sizeof(sin)));
+
+    errno_check(listen(s, 5));
     
-    r = listen(s, 5);
-    err_exit(r < 0, "cannot listen on socket: %s", strerror(r));
-    
-    printf("sock_bench: server on port %d\n", port);
+    printf("sock_bench: byte count %d, iter count %d, port %d\n", 
+	   byte_count, iter_count, port);
     for (;;) {
         socklen_t socklen = sizeof(sin);
 	
@@ -98,22 +147,9 @@ main (int ac, char **av)
             continue;
         }
 	
-	if (threaded) {
-#if JOS64
-	    struct cobj_ref t;
-	    r = thread_create(start_env->proc_container, &server,
-			      (void*) (int64_t) ss, &t, "server");
-	    if (r < 0) {
-		printf("cannot spawn client thread: %s\n", e2s(r));
-		close(ss);
-	    } else {
-		fd_give_up_privilege(ss);
-	    }
-#else 
-	    pthread_t t;
-	    r = pthread_create(&t, 0, server, (void *)(int64_t)ss);
-#endif
-	} else
+	if (threaded)
+	    start_server_thread(ss);
+	else
 	    server((void *) (int64_t)ss);	
     }
     return 0;
