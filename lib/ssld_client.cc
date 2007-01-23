d77477639b743d987fd5a8213dd91f3e4b7662d0
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-23 21:08:59 +0000

    pass args on thread stack
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3534 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/ssld_client.cc
+++ lib/ssld_client.cc
@@ -21,18 +21,19 @@ extern "C" {
 #include <inc/labelutil.hh>
 #include <inc/gateinvoke.hh>
 
+struct worker_args {
+    struct cobj_ref cipher_biseg;
+    struct cobj_ref plain_biseg;
+    struct cobj_ref cow_gate;
+    uint64_t root_ct;
+    uint64_t taint;
+};
+
 static void __attribute__((noreturn))
 ssld_worker_setup(void *b)
 {
-    // XXX
-    struct args {
-	struct cobj_ref cipher_biseg;
-	struct cobj_ref plain_biseg;
-	struct cobj_ref cow_gate;
-	uint64_t root_ct;
-	uint64_t taint;
-    } *a = (struct args *)b;
-    
+    struct worker_args *a = (struct worker_args *)b;
+        
     struct cobj_ref cow_gate = a->cow_gate;
     uint64_t taint = a->taint;
 
@@ -41,8 +42,6 @@ ssld_worker_setup(void *b)
     d->plain_biseg = a->plain_biseg;
     d->root_ct = a->root_ct;
 
-    free(a);    
-    
     label tgt_label, tgt_clear;
     obj_get_label(cow_gate, &tgt_label);
     gate_get_clearance(cow_gate, &tgt_clear);
@@ -50,7 +49,6 @@ ssld_worker_setup(void *b)
     tgt_clear.set(taint, 3);
 
     gate_invoke(cow_gate, &tgt_label, &tgt_clear, 0, 0);
-    
 }
 
 void
@@ -59,25 +57,16 @@ ssld_taint_cow(struct cobj_ref cow_gate,
 	       uint64_t root_ct, uint64_t taint,
 	       thread_args *ta)
 {
-    // XXX
-    struct args {
-	struct cobj_ref cipher_biseg;
-	struct cobj_ref plain_biseg;
-	struct cobj_ref cow_gate;
-	uint64_t root_ct;
-	uint64_t taint;
-    } *a = (struct args *)malloc(sizeof(*a));
-    a->cipher_biseg = cipher_biseg;
-    a->plain_biseg = plain_biseg;
-    a->cow_gate = cow_gate;
-    a->root_ct = root_ct;
-    a->taint = taint;
+    struct worker_args a;    
+    a.cipher_biseg = cipher_biseg;
+    a.plain_biseg = plain_biseg;
+    a.cow_gate = cow_gate;
+    a.root_ct = root_ct;
+    a.taint = taint;
 
     struct cobj_ref t;
     int r = thread_create_option(root_ct, &ssld_worker_setup,
-				 a, 0, &t, "ssld-worker", ta, 0);
-    error_check(r);
-
-    
-    
+				 &a, sizeof(a), 
+				 &t, "ssld-worker", ta, THREAD_OPT_ARGS);
+    error_check(r);    
 }
