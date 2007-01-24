91de7b2e0ec9af28b755bc02d88f16cbcbfd7a39
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-24 04:27:21 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3539 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/types.h
+++ inc/types.h
@@ -15,6 +15,17 @@
  * Would be nice to unify them..
  */
 
+// 64-bit constants
+#if __LONG_MAX__==9223372036854775807L
+# define UINT64(x) x##UL
+# define CAST64(x) ((unsigned long) (x))
+#elif __LONG_LONG_MAX__==9223372036854775807LL
+# define UINT64(x) x##ULL
+# define CAST64(x) ((unsigned long long) (x))
+#else
+# error Missing 64-bit type
+#endif
+
 /*
  * Rounding operations (efficient when n is a power of 2)
  * Round down to the nearest multiple of n
