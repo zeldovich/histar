b1e68c4cd4596362c3611a9b138fa311dd90e5b8
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-29 23:13:12 +0000

    oops, we use this header file in 32-bit boot code
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3602 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/amd64/types.h
+++ kern/arch/amd64/types.h
@@ -19,8 +19,15 @@ typedef short int16_t;
 typedef unsigned short uint16_t;
 typedef int int32_t;
 typedef unsigned int uint32_t;
+#if __LONG_MAX__==9223372036854775807L
 typedef long int64_t;
 typedef unsigned long uint64_t;
+#elif __LONG_LONG_MAX__==9223372036854775807LL
+typedef long long int64_t;
+typedef unsigned long long uint64_t;
+#else
+#error Missing 64-bit type
+#endif
 typedef __int128_t int128_t;
 typedef __uint128_t uint128_t;
 typedef uint64_t __uint64_t;
