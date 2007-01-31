da741a8d00df98cda7927588f19497f0cb2f0805
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-01-31 23:51:01 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3640 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -16,5 +16,9 @@ KERN_BINFILES	+= user/auth_log user/auth_dir user/auth_user user/login user/addu
 #KERN_BINFILES	+= user/devinit user/devpt user/sshd user/ssh.tar user/scp user/sshdi
 #KERN_BINFILES	+= user/vim user/bash
 
+## httpd
+#KERN_BINFILES	+= user/server.pem user/dh.pem user/servkey.pem
+#KERN_BINFILES	+= user/httpd user/httpd_mom user/ssld user/ssl_eprocd
+
 INITTAB_ENTRIES	:= /bin/netd_mom:root /bin/devinit:root /bin/sshdi:root /bin/httpd_mom:root
 
