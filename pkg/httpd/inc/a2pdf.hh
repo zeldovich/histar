f8965ca5871b4d90270d7ce44b113114b7edc00e
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-07 03:33:05 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3734 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/httpd/inc/a2pdf.hh
+++ pkg/httpd/inc/a2pdf.hh
@@ -1,7 +1,6 @@
 #ifndef JOS_HTTPD_INC_A2PDF_HH
 #define JOS_HTTPD_INC_A2PDF_HH
 
-#include <inc/cpplabel.hh>
 #include <iostream>
 
 uint64_t a2pdf(int fd, std::ostringstream &pdf_out, uint64_t taint);
