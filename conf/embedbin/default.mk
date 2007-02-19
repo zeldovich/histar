7166e575b9c597a80e51d85443902104a2e608ec
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-19 10:21:27 +0000

    use new pty stuff w/ sshd
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3923 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -13,12 +13,12 @@ KERN_BINFILES	+= user/auth_log user/auth_dir user/auth_user user/login user/addu
 
 #KERN_BINFILES	+= user/tar user/gzip user/gunzip
 #KERN_BINFILES	+= user/gcc.tar.gz user/make user/kern.tar.gz user/setup.sh
-#KERN_BINFILES	+= user/devinit user/devpt user/sshd user/ssh.tar user/scp user/sshdi
+#KERN_BINFILES	+= user/ptyd user/sshd user/ssh.tar user/scp user/sshdi
 #KERN_BINFILES	+= user/vim user/bash
 
 ## httpd
 #KERN_BINFILES	+= user/server.pem user/dh.pem user/servkey.pem
 #KERN_BINFILES	+= user/httpd user/httpd_mom user/ssld user/ssl_eprocd
 
-INITTAB_ENTRIES	:= /bin/netd_mom:root /bin/devinit:root /bin/sshdi:root /bin/httpd_mom:root
+INITTAB_ENTRIES	:= /bin/netd_mom:root /bin/ptyd:root /bin/sshdi:root /bin/httpd_mom:root
 
