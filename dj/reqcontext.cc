3794bcc16edec4d13f6c12adff0932785bfb9029
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-20 00:23:31 +0000

    checkpoint some stuff
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3941 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ dj/reqcontext.cc
@@ -0,0 +1,54 @@
+#include <inc/scopeguard.hh>
+#include <inc/labelutil.hh>
+#include <dj/reqcontext.hh>
+#include <dj/djops.hh>
+
+bool
+verify_label_reqctx::can_read(cobj_ref o)
+{
+    try {
+	if (o.container != o.object && !can_read(COBJ(o.container, o.container)))
+	    return false;
+
+	label l;
+	obj_get_label(o, &l);
+	error_check(l.compare(&vl_, label::leq_starhi));
+	return true;
+    } catch (std::exception &e) {
+	warn << "verify_label_reqctx::can_read " << o
+	     << ": " << e.what() << "\n";
+	return false;
+    }
+}
+
+bool
+verify_label_reqctx::can_rw(cobj_ref o)
+{
+    try {
+	if (o.container != o.object && !can_read(COBJ(o.container, o.container)))
+	    return false;
+
+	label l;
+	obj_get_label(o, &l);
+	error_check(vl_.compare(&l, label::leq_starlo));
+	error_check(l.compare(&vl_, label::leq_starhi));
+	return true;
+    } catch (std::exception &e) {
+	warn << "verify_label_reqctx::can_rw " << o
+	     << ": " << e.what() << "\n";
+	return false;
+    }
+}
+
+void
+verify_label_reqctx::read_seg(cobj_ref o, str *buf)
+{
+    if (!can_read(o))
+	throw basic_exception("vl_reqctx::read_seg: cannot read object");
+
+    void *data_map = 0;
+    uint64_t data_len = 0;
+    error_check(segment_map(o, 0, SEGMAP_READ, &data_map, &data_len, 0));
+    scope_guard2<int, void*, int> unmap(segment_unmap_delayed, data_map, 1);
+    buf->setbuf((const char *) data_map, data_len);
+}
