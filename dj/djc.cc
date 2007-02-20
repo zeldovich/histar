092ac06ae6d45ed51c953d17525ed1249c7fa807
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-20 05:57:23 +0000

    hmm, actually sending a message through the gate seems to work..
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@3953 927ebb28-5305-0410-b3ca-c4b640df69b7

--- dj/djc.cc
+++ dj/djc.cc
@@ -1,87 +1,6 @@
-#include <async.h>
-#include <crypt.h>
-#include <wmstr.h>
-#include <arpc.h>
-#include <dj/dis.hh>
+#include <dj/djprot.hh>
 #include <dj/djops.hh>
-#include <dj/djfs.h>
-#include <dj/djgatecall.hh>
-
-static void
-readcb(dj_reply_status stat, const djcall_args *args)
-{
-    if (stat != REPLY_DONE) {
-	warn << "readcb: stat " << stat << "\n";
-	return;
-    }
-
-    djfs_reply reply;
-    if (!str2xdr(reply, args->data)) {
-	warn << "readcb: cannot decode\n";
-	return;
-    }
-
-    if (reply.d->op != DJFS_READ) {
-	warn << "readcb: wrong reply op\n";
-	return;
-    }
-
-    str d(reply.d->read->data.base(), reply.d->read->data.size());
-    warn << "read: " << d << "\n";
-}
-
-static void
-readdircb(dj_reply_status stat, const djcall_args *args)
-{
-    if (stat == REPLY_DONE) {
-	djfs_reply reply;
-	if (!str2xdr(reply, args->data)) {
-	    warn << "readdircb: cannot decode\n";
-	} else {
-	    if (reply.err) {
-		warn << "readdircb: err " << reply.err << "\n";
-		return;
-	    }
-
-	    if (reply.d->op != DJFS_READDIR) {
-		warn << "readdircb: wrong reply op " << reply.d->op << "\n";
-		return;
-	    }
-
-	    for (uint32_t i = 0; i < reply.d->readdir->ents.size(); i++)
-		warn << "readdir: " << reply.d->readdir->ents[i] << "\n";
-	}
-    } else {
-	warn << "readdircb: status " << stat << "\n";
-    }
-}
-
-static void
-dostuff(djgate_caller *dc, str node_pk, const dj_gatename &gate)
-{
-    djfs_request req;
-
-    djcall_args args;
-    args.taint = label(1);
-    args.grant = label(3);
-
-    dj_reply_status stat;
-    djcall_args res;
-
-    req.set_op(DJFS_READDIR);
-    req.readdir->pn = str("/bin");
-    args.data = xdr2str(req);
-
-    stat = dc->call(node_pk, gate, args, &res);
-    readdircb(stat, &res);
-
-    req.set_op(DJFS_READ);
-    req.read->pn = str("/etc/passwd");
-    args.data = xdr2str(req);
-
-    stat = dc->call(node_pk, gate, args, &res);
-    readcb(stat, &res);
-}
+#include <dj/gatesender.hh>
 
 int
 main(int ac, char **av)
@@ -93,16 +12,31 @@ main(int ac, char **av)
 
     cobj_ref djd_gate;
     djd_gate <<= av[1];
+
     const char *pk16 = av[2];
-
-    dj_gatename djgate;
-    djgate <<= av[3];
-
-    dj_esign_pubkey k;
+    dj_pubkey k;
     k.n = bigint(pk16, 16);
     k.k = 8;
-    str nodepk = xdr2str(k);
 
-    djgate_caller dc(djd_gate);
-    dostuff(&dc, xdr2str(k), djgate);
+    dj_message_endpoint ep;
+    ep.set_type(EP_GATE);
+    *ep.gate <<= av[3];
+
+    gate_sender gs(djd_gate);
+
+    dj_delegation_set dset;
+    dj_catmap cm;
+    dj_message m;
+
+    m.target = ep;
+    m.msg_ct = 1;
+    m.token = 0;
+    m.taint.deflevel = 1;
+    m.glabel.deflevel = 3;
+    m.gclear.deflevel = 0;
+    m.msg = "Hello world!";
+
+    uint64_t token = 0;
+    dj_delivery_code c = gs.send(k, 1, dset, cm, m, &token);
+    printf("code = %d, token = %ld\n", c, token);
 }
