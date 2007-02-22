c9f1895e50fe41fde42b1825d52e467055c3fc03
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-22 08:40:48 +0000

    auto-locate djd via container names from the root
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4008 927ebb28-5305-0410-b3ca-c4b640df69b7

--- dj/djc.cc
+++ dj/djc.cc
@@ -8,26 +8,23 @@
 int
 main(int ac, char **av)
 {
-    if (ac != 5) {
-	printf("Usage: %s djd-gate-ct.id host-pk call-ct gate-ct.id\n", av[0]);
+    if (ac != 4) {
+	printf("Usage: %s host-pk call-ct gate-ct.id\n", av[0]);
 	exit(-1);
     }
 
-    cobj_ref djd_gate;
-    djd_gate <<= av[1];
-
-    const char *pk16 = av[2];
+    const char *pk16 = av[1];
     dj_pubkey k;
     k.n = bigint(pk16, 16);
     k.k = 8;
 
-    uint64_t call_ct = atoi(av[3]);
+    uint64_t call_ct = atoi(av[2]);
 
     dj_message_endpoint ep;
     ep.set_type(EP_GATE);
-    *ep.gate <<= av[4];
+    *ep.gate <<= av[3];
 
-    gate_sender gs(djd_gate);
+    gate_sender gs;
 
     dj_delegation_set dset;
     dj_catmap cm;
--- dj/gatesender.hh
+++ dj/gatesender.hh
@@ -3,12 +3,23 @@
 
 extern "C" {
 #include <inc/container.h>
+#include <inc/lib.h>
 }
 
+#include <inc/error.hh>
 #include <dj/djprotx.h>
 
 class gate_sender {
  public:
+    gate_sender() {
+	int64_t gate_ct, gate_id;
+	error_check(gate_ct = container_find(start_env->root_container,
+					     kobj_container, "djd"));
+	error_check(gate_id = container_find(gate_ct,
+					     kobj_gate, "djd-incoming"));
+	g_ = COBJ(gate_ct, gate_id);
+    }
+
     gate_sender(cobj_ref djd_gate) : g_(djd_gate) {}
 
     dj_delivery_code send(const dj_pubkey &node, time_t timeout,
