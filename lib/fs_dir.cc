fa11e28994523d8877a4543b311c834bac461413
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-02-23 20:16:19 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4027 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/fs_dir.cc
+++ lib/fs_dir.cc
@@ -202,7 +202,6 @@ fs_lookup_path(struct fs_inode dir, const char *pn, struct fs_inode *o)
 int
 fs_namei(const char *pn, struct fs_inode *o)
 {
-    scoped_prof _d;
     struct fs_inode d = pn[0] == '/' ? start_env->fs_root : start_env->fs_cwd;
     return fs_lookup_path(d, pn, o);
 }
