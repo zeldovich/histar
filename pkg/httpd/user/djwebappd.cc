225645c1cdb753605f67864b202de0d09326877a
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-03-03 21:21:16 +0000

    works at least for cat
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4221 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/httpd/user/djwebappd.cc
+++ pkg/httpd/user/djwebappd.cc
@@ -1,18 +1,30 @@
+extern "C" {
+#include <inc/syscall.h>
+}
+
 #include <sstream>
 #include <inc/module.hh>
+#include <inc/labelutil.hh>
 #include <dj/djsrpc.hh>
 #include <dj/djops.hh>
 #include <dj/djlabel.hh>
 #include <dj/miscx.h>
 
+enum { webapp_debug = 0 };
+
+static fs_inode httpd_root_ino;
+
 static void
-handle_req(const fs_inode &httpd_root_ino, const char *req,
-	   uint64_t ut, uint64_t ug, std::ostringstream &header)
+handle_req(const char *req, uint64_t ut, uint64_t ug,
+	   std::ostringstream &header)
 {
     char *tmp;
     char strip_req[256];
     strncpy(strip_req, req, sizeof(strip_req) - 1);
     strip_req[sizeof(strip_req) - 1] = 0;
+
+    start_env->user_grant = ug;
+    start_env->user_taint = ut;
 
     if (tmp = strchr(strip_req, '?')) {
         *tmp = 0;
@@ -43,10 +55,6 @@ handle_req(const fs_inode &httpd_root_ino, const char *req,
 static bool
 webapp_service(const dj_message &m, const str &s, dj_rpc_reply *r)
 try {
-    static fs_inode httpd_root_ino;
-    if (!httpd_root_ino.obj.object)
-	error_check(fs_namei("/www", &httpd_root_ino));
-
     webapp_arg arg;
     webapp_res res;
 
@@ -62,8 +70,17 @@ try {
 	return false;
     }
 
+    if (webapp_debug) {
+	label tl, tc;
+	thread_cur_label(&tl);
+	thread_cur_clearance(&tc);
+	warn << "webapp_service: ug " << ug << ", ut " << ut << "\n";
+	warn << "tl = " << tl.to_string() << "\n";
+	warn << "tc = " << tc.to_string() << "\n";
+    }
+
     std::ostringstream obuf;
-    handle_req(httpd_root_ino, arg.reqpath, ut, ug, obuf);
+    handle_req(arg.reqpath, ut, ug, obuf);
 
     std::string reply = obuf.str();
     res.httpres = str(reply.data(), reply.size());
@@ -84,6 +101,38 @@ gate_entry(void *arg, gate_call_data *gcd, gatesrv_return *r)
 int
 main(int ac, char **av)
 {
+    for (int retry = 0; ; retry++) {
+	int r = fs_namei("/www", &httpd_root_ino);
+	if (r >= 0)
+	    break;
+
+	if (retry >= 10) {
+	    printf("djwebappd: tired of waiting for /www to appear\n");
+	    exit(1);
+	}
+	sleep(1);
+    }
+
+    int64_t mtab_id = sys_segment_copy(start_env->fs_mtab_seg,
+				       start_env->shared_container,
+				       0, "djwebappd mtab");
+    error_check(mtab_id);
+    start_env->fs_mtab_seg = COBJ(start_env->shared_container, mtab_id);
+
+    /* Replicate the mounted directory structure that inetd creates for /www */
+    fs_inode home_dir, etc_dir, share_dir, bin_dir, dev_dir;
+    error_check(fs_namei("/home", &home_dir));
+    error_check(fs_namei("/etc", &etc_dir));
+    error_check(fs_namei("/share", &share_dir));
+    error_check(fs_namei("/bin", &bin_dir));
+    error_check(fs_namei("/dev", &dev_dir));
+    error_check(fs_mount(start_env->fs_mtab_seg, httpd_root_ino, "home", home_dir));
+    error_check(fs_mount(start_env->fs_mtab_seg, httpd_root_ino, "etc", etc_dir));
+    error_check(fs_mount(start_env->fs_mtab_seg, httpd_root_ino, "share", share_dir));
+    error_check(fs_mount(start_env->fs_mtab_seg, httpd_root_ino, "bin", bin_dir));
+    error_check(fs_mount(start_env->fs_mtab_seg, httpd_root_ino, "dev", dev_dir));
+    start_env->fs_root = httpd_root_ino;
+
     gatesrv_descriptor gd;
     gd.gate_container_ = start_env->shared_container;
     gd.name_ = "djwebappd";
