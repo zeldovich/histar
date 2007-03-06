b8857646ca862d660f341da5b479123c44ff45c2
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-03-06 06:51:03 +0000

    add option to xhelper to serve files to dhelper
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4273 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ pkg/httpd/user/dhelper.cc
@@ -0,0 +1,61 @@
+extern "C" {
+#include <inc/stdio.h>
+#include <inc/lib.h>
+#include <inc/fs.h>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <assert.h>
+}
+
+#include <inc/error.hh>
+#include <inc/errno.hh>
+#include <inc/nethelper.hh>
+
+int
+main(int ac, char **av)
+{
+    static uint16_t port = 1234;
+    if (ac < 3) {
+	printf("usage: %s ip req\n", av[0]);
+	return -1;
+    }
+    
+    tcpconn conn(av[1], port);
+    conn.write(av[2], strlen(av[2]));
+    conn.write("\r\n", 2);
+    lineparser lp(&conn);
+
+    for (;;) {
+	const char *fn;
+	if (!(fn = lp.read_line()))
+	    break;
+	
+	char buf[4096] = "/www/";
+	strcat(buf, fn);
+
+	int fd = open(buf, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	
+	printf("creating %s...\n", fn);
+
+	uint32_t count = atoi(lp.read_line());
+	assert(sizeof(buf) > count);
+
+	uint32_t cc = count;
+	char *ptr = buf;
+	while (cc) {
+	    int r = lp.read(ptr, cc) ;
+	    ptr += r;
+	    cc -= r;
+	}
+	*ptr = 0;
+
+	write(fd, buf, count);
+	printf("wrote %d bytes!\n", count);
+	
+	close(fd);
+    }
+    return 0;
+}
--- pkg/httpd/user/xhelper.cc
+++ pkg/httpd/user/xhelper.cc
@@ -5,8 +5,16 @@ extern "C" {
 
 #include <inc/error.hh>
 #include <inc/errno.hh>
+#include <inc/nethelper.hh>
 #include <dj/gatesender.hh>
 #include <dj/djops.hh>
+
+// for 'server' mode
+static strbuf gs_key;
+static strbuf callct;
+static strbuf authgate;
+static strbuf fsgate;
+static strbuf appgate;
 
 static void
 write_to_file(const char *pn, const strbuf &sb)
@@ -19,33 +27,96 @@ write_to_file(const char *pn, const strbuf &sb)
     close(fd);
 }
 
+static void
+write_to_tcpconn(tcpconn &tc, const char *name, strbuf sb)
+{
+    char buf[128];
+    str s(sb);
+    
+    strncpy(buf, name, sizeof(buf));
+    strcat(buf, "\r\n");
+
+    tc.write(buf, strlen(buf));
+    snprintf(buf, sizeof(buf), "%lu\r\n", s.len());
+    tc.write(buf, strlen(buf));
+    tc.write(s.cstr(), s.len());
+}
+
+static void
+server(void)
+{
+    static uint16_t port = 1234;
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0)
+        panic("cannot create socket: %d\n", s);
+    
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_port = htons(port);
+    int r = bind(s, (struct sockaddr *)&sin, sizeof(sin));
+    if (r < 0)
+        panic("cannot bind socket: %d\n", r);
+
+    r = listen(s, 5);
+    if (r < 0)
+        panic("cannot listen on socket: %d\n", r);
+
+    printf("server on port %d\n", port);
+    for (;;) {
+        socklen_t socklen = sizeof(sin);
+        int ss = accept(s, (struct sockaddr *)&sin, &socklen);
+        if (ss < 0) {
+            printf("cannot accept client: %d\n", ss);
+            continue;
+        }
+	printf("client connected...\n");
+
+	tcpconn tc(ss, 0);
+	lineparser lp(&tc);
+	
+	const char *req;
+	if ((req = lp.read_line())) {
+	    printf("req %s\n", req);
+	    if (!strcmp(req, "stop"))
+		return;
+	    else if(!strcmp(req, "user")) {
+		write_to_tcpconn(tc, "dj_user_host", gs_key);
+		write_to_tcpconn(tc, "dj_user_ct", callct);
+		write_to_tcpconn(tc, "dj_user_authgate", authgate);
+		write_to_tcpconn(tc, "dj_user_fsgate", fsgate);
+	    } else if (!strcmp(req, "app")) {
+		write_to_tcpconn(tc, "dj_app_host", gs_key);
+		write_to_tcpconn(tc, "dj_app_ct", callct);
+		write_to_tcpconn(tc, "dj_app_gate", appgate);
+	    }
+	}
+	close(ss);
+    }
+}
+
 int
 main(int ac, char **av)
 {
     gate_sender gs;
 
-    strbuf gs_key;
     gs_key << gs.hostkey();
 
     int64_t ct, id;
     error_check(ct = container_find(start_env->root_container, kobj_container, "djechod"));
     error_check(id = container_find(ct, kobj_container, "public call"));
-    strbuf callct;
     callct << id << "\n";
 
     error_check(ct = container_find(start_env->root_container, kobj_container, "djauthproxy"));
     error_check(id = container_find(ct, kobj_gate, "authproxy"));
-    strbuf authgate;
     authgate << ct << "." << id << "\n";
 
     error_check(ct = container_find(start_env->root_container, kobj_container, "djwebappd"));
     error_check(id = container_find(ct, kobj_gate, "djwebappd"));
-    strbuf appgate;
     appgate << ct << "." << id << "\n";
 
     error_check(ct = container_find(start_env->root_container, kobj_container, "djfsd"));
     error_check(id = container_find(ct, kobj_gate, "djfsd"));
-    strbuf fsgate;
     fsgate << ct << "." << id << "\n";
 
     write_to_file("/www/dj_user_host", gs_key);
@@ -59,4 +130,9 @@ main(int ac, char **av)
     system("adduser alice foo");
     system("adduser bob bar");
     warn << "All done, alice/foo, bob/bar\n";
+    
+    if (ac >= 2 && av[1][0] == 's') {
+	warn << "starting server...\n";
+	server();
+    }
 }
