06ca93664b974726aac7a3a968db234b38dec237
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-03-14 07:41:32 +0000

    interface for tossing a thread to a gate, or something
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4495 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ inc/goblegateclnt.hh
@@ -0,0 +1,71 @@
+#ifndef JOS_INC_GOBLEGATECLNT_HH
+#define JOS_INC_GOBLEGATECLNT_HH
+
+extern "C" {
+#include <inc/lib.h>
+#include <inc/gateparam.h>
+}
+
+#include <inc/gateclnt.hh>
+#include <inc/cpplabel.hh>
+#include <inc/error.hh>
+
+class goblegate_call 
+{
+public:
+    struct leftovers {
+	leftovers(void) : 
+	    gc_(0), vl_(0), vc_(0) {}
+				
+	gate_call *gc_;
+	label *vl_;
+	label *vc_;
+
+	thread_args ta_;
+	gate_call_data gcd_;
+	
+	void cleanup(void) {
+	    if (gc_)
+		delete gc_;
+	    if (vl_)
+		delete vl_;
+	    if (vc_)
+		delete vc_;
+	    if (ta_.container)
+		thread_cleanup(&ta_);
+	}
+    };
+
+    goblegate_call(cobj_ref gate, 
+		   const label *cs, const label *ds, const label *dr) {
+	lo_.gc_ = new gate_call(gate, cs, ds, dr);
+    }
+
+    void call(gate_call_data *gcd_param, const label *vl, const label *vc) {
+	cobj_ref t;
+	uint64_t thread_ct = lo_.gc_->call_taint_ct();
+	memcpy(&lo_.gcd_, gcd_param, sizeof(lo_.gcd_));
+	int r = thread_create_option(thread_ct, &goblegate_stub,
+				     &lo_, sizeof(lo_), 
+				     &t, "goblegate_stub", 
+				     &lo_.ta_, THREAD_OPT_ARGCOPY);
+	if (r < 0)
+	    throw error(r, "cannot start thread");
+    }
+
+    uint64_t call_ct(void) { return lo_.gc_->call_ct(); }
+    
+    leftovers lo(void) {
+	return lo_;
+    }
+
+    static void goblegate_stub(void *a) {
+	leftovers *lo = (leftovers *)a;
+	lo->gc_->call(&lo->gcd_, lo->vl_, lo->vc_);
+    }
+    
+private:
+    leftovers lo_;    
+};
+
+#endif
