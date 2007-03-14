6b9f2f86b8d9679512a2ee3eb51932f35d09a728
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-03-14 10:32:36 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4497 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ dj/gatecallstatus.cc
@@ -0,0 +1,51 @@
+extern "C" {
+#include <inc/lib.h>
+#include <inc/syscall.h>
+}
+
+#include <inc/segmentutil.hh>
+#include <inc/cpplabel.hh>
+#include <dj/gatecallstatus.h>
+
+enum { gatecall_incomplete = 0, gatecall_complete };
+struct gatecall_status {
+    uint64_t signal;
+    uint64_t res;
+};
+
+cobj_ref
+gatecall_status_alloc(uint64_t ct, label *l)
+{
+    gatecall_status *gcs = 0;
+    cobj_ref gcs_obj;
+    int r = segment_alloc(ct, sizeof(*gcs), &gcs_obj,
+			  (void **)&gcs, l->to_ulabel(), "gatecall status");
+    if (r < 0)
+	throw error(r, "unable to alloc gatecall status seg");
+    
+    memset(gcs, 0, sizeof(*gcs));
+    segment_unmap_delayed(gcs, 1);
+    return gcs_obj;
+}
+
+void
+gatecall_status_done(cobj_ref gcs_obj, uint64_t res)
+{
+    segment_writer sw(gcs_obj);
+    gatecall_status *gcs = (gatecall_status *) sw.addr();
+    gcs->res = res;
+    gcs->signal = gatecall_complete;
+    sys_sync_wakeup(&gcs->signal);
+}
+
+uint64_t 
+gatecall_status_wait(cobj_ref gcs_obj)
+{
+    segment_reader sr(gcs_obj);
+    gatecall_status *gcs = (gatecall_status *) sr.addr();
+    int64_t r = sys_sync_wait(&gcs->signal, gatecall_incomplete ,~0UL);
+    if (r < 0)
+	throw error(r, "unable to wait on signal");
+    return gcs->res;
+}
+
