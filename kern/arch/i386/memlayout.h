ad75c16c029342e3ea61b0d4039deda389ffdfe0
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-04-05 06:09:57 +0000

    placeholder
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4792 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/i386/memlayout.h
+++ kern/arch/i386/memlayout.h
@@ -6,11 +6,7 @@
 /*
  * Virtual memory map
  *
- *   2^64 --------->  +---------------------------------------------+
- *                    |                                             |
- *                    |   Kernel memory (all symbols point here)    |
- *                    |                                             |
- *   KERNBASE ----->  +---------------------------------------------+
+ *   2^32 --------->  +---------------------------------------------+
  *   KSTACKTOP ---->  +---------------------------------------------+
  *                    |            Kernel stack (2 pages)           |
  *                    +---------------------------------------------+
@@ -19,43 +15,41 @@
  *                    |                      :                      |
  *                    +---------------------------------------------+
  *                    |                                             |
+ *                    |   Kernel memory (all symbols point here)    |
+ *                    |                                             |
+ *   KERNBASE ----->  +---------------------------------------------+
+ *                    |                                             |
  *                    |     All of physical memory mapped here      |
  *                    |                                             |
- *   PHYSBASE ----->  +---------------------------------------------+
- *                    |                                             |
- *                    |           2^47 to (2^64 - 2^47)             |
- *                    |         invalid virtual addresses           |
- *                    |                                             |
- *   ULIM --------->  +---------------------------------------------+
+ *   PHYSBASE/ULIM->  +---------------------------------------------+
  *                    |                 user stack                  |
  *                    |                 user data                   |
  *                    |                 user text                   |
  *   0 ------------>  +---------------------------------------------+
  */
 
-#define KERNBASE	UINT64 (0xffffffff80000000)
-#define RELOC(x)	(CAST64 (x) - KERNBASE)
+#define KSTACKTOP	(0xffffffff - PGSIZE + 1)
+#define KERNBASE	0xc0000000
+#define RELOC(x)	(x - KERNBASE)
 
-#define PHYSBASE	UINT64 (0xffff800000000000)
-#define KSTACKTOP	(KERNBASE - PGSIZE)
-
-#define ULIM		UINT64 (0x0000800000000000)
+#define PHYSBASE	0x80000000
+#define ULIM		PHYSBASE
 
 // At IOPHYSMEM (640K) there is a 384K hole for I/O.  From the kernel,
-// IOPHYSMEM can be addressed at KERNBASE + IOPHYSMEM.  The hole ends
+// IOPHYSMEM can be addressed at PHYSBASE + IOPHYSMEM.  The hole ends
 // at physical address EXTPHYSMEM.
 #define IOPHYSMEM	0x0A0000
 #define EXTPHYSMEM	0x100000
 
 // User-mode (below ULIM) address space layout conventions.
-#define UMMAPBASE	UINT64 (0x0000000100000000)
-#define USTACKTOP	UINT64 (0x0000400000000000)
-#define UFDBASE		UINT64 (0x0000400100000000)
-#define UHEAP		UINT64 (0x0000400200000000)
-#define UHEAPTOP	UINT64 (0x0000400300000000)
-#define USTARTENVRO	UINT64 (0x0000400300000000)
-#define UTLSBASE	UINT64 (0x0000400400000000)
-#define UTLSTOP		UINT64 (0x0000400500000000)
-#define USEGMAPENTS	UINT64 (0x0000400600000000)
+#define USTACKTOP	ULIM
+#define UMMAPBASE	0x40000000
+#define UHEAP		0x50000000
+#define UHEAPTOP	0x60000000
+#define USTARTENVRO	0x60001000
+#define UTLSBASE	0x60002000
+#define UTLSTOP		0x6ffff000
+#define UFDBASE		0x61000000
+#define USEGMAPENTS	0x62000000
 
 #endif
