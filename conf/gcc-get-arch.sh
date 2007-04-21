de7a6815528d4ceea7c4872c06d29bbe1f740f0e
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-04-21 23:12:27 +0000

    make gcc-get-arch work when using gcc -m32
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4942 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/gcc-get-arch.sh
+++ conf/gcc-get-arch.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-GCCMACH=`$1 -dumpmachine | cut -d- -f1`
-case "$GCCMACH" in
-    x86_64)
+if [ "$1" = "" ]; then
+    echo "Usage: $0 <gcc command>"
+    exit 1
+fi
+
+F=`mktemp`
+trap "rm -f $F" 0
+
+echo 'main(){}' | "$@" -x c - -c -o $F
+ARCH=`objdump -f $F | grep ^architecture: | cut -d, -f1 | cut -d\  -f2`
+case "$ARCH" in
+    i?86:x86-64)
 	echo amd64
 	;;
     i?86)
