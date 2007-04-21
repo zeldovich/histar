f9ea2043ced16a9662ea641197e077a4afb954ee
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-04-21 21:43:58 +0000

    64-bit atomic compare-exchange for i386
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4941 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ kern/arch/i386/atomic.h
@@ -0,0 +1,7 @@
+#ifndef JOS_MACHINE_ATOMIC_H
+#define JOS_MACHINE_ATOMIC_H
+
+#include <kern/arch/amd64/atomic32.h>
+#include <machine/atomic64.h>
+
+#endif
---
+++ kern/arch/i386/atomic64.h
@@ -0,0 +1,37 @@
+#ifndef JOS_MACHINE_ATOMIC64_H
+#define JOS_MACHINE_ATOMIC64_H
+
+/*
+ * If we support SMP at some point, we should enable this.
+ */
+#if 0
+#define ATOMIC_LOCK "lock ; "
+#else
+#define ATOMIC_LOCK ""
+#endif
+
+typedef struct { volatile uint64_t counter; } atomic64_t;
+
+/*
+ * Atomically compare the value in "v" with "old", and set "v" to "newv"
+ * if equal.
+ *
+ * Return value is the previous value of "v".  So if return value is same
+ * as "old", the swap occurred, otherwise it did not.
+ */
+static __inline__ uint64_t
+atomic_compare_exchange64(atomic64_t *v, uint64_t old, uint64_t newv)
+{
+    uint32_t new_lo = newv & 0xffffffff;
+    uint32_t new_hi = newv >> 32;
+
+    __asm__ __volatile__(
+	ATOMIC_LOCK "cmpxchg8b %1"
+	: "=A" (old), "+m" (v->counter)
+	: "c" (new_hi), "b" (new_lo)
+	: "cc");
+    return old;
+}
+
+#undef ATOMIC_LOCK
+#endif
