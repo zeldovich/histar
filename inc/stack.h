73d912b7716de5247c8164b5db47f81cfcc37170
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-04-22 04:48:48 +0000

    generic stack glue
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@4962 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/stack.h
+++ inc/stack.h
@@ -1,6 +1,7 @@
 #ifndef JOS_INC_STACK_H
 #define JOS_INC_STACK_H
 
+uintptr_t stack_curptr(void);
 void stack_switch(uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4,
 		  void *stacktop, void *fn) __attribute__((noreturn));
 
