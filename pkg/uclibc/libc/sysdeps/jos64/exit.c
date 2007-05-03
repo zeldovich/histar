6a7c3dcf4ee1009a0d228ea0ba165c224763802f
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-05-03 20:21:34 +0000

    garbage-collect zombie processes that have no live parent process to wait() for them
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5137 927ebb28-5305-0410-b3ca-c4b640df69b7

--- pkg/uclibc/libc/sysdeps/jos64/exit.c
+++ pkg/uclibc/libc/sysdeps/jos64/exit.c
@@ -1,6 +1,7 @@
 #include <inc/lib.h>
 #include <inc/fd.h>
 #include <inc/prof.h>
+#include <inc/syscall.h>
 
 #include <signal.h>
 #include <unistd.h>
@@ -12,8 +13,25 @@ process_exit(int64_t rval, int64_t signo)
 
     prof_print(1);
 
-    if (start_env)
+    if (start_env) {
 	process_report_exit(rval, signo);
+
+	if (start_env->ppid) {
+	    /*
+	     * Wait until our parent dies, and GC this zombie process.
+	     * Until then, the parent has a chance to wait() on us.
+	     */
+	    utrap_set_mask(1);
+
+	    struct cobj_ref ppid_ct = COBJ(start_env->ppid, start_env->ppid);
+	    while (sys_obj_get_type(ppid_ct) == kobj_container)
+		sleep(10);
+
+	    uint64_t parent_ct =
+		sys_container_get_parent(start_env->shared_container);
+	    sys_obj_unref(COBJ(parent_ct, start_env->shared_container));
+	}
+    }
 
     thread_halt();
 }
