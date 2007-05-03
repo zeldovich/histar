fda687d897eb636b09dffd76dc5daef3950c333b
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-05-03 20:34:47 +0000

    TIOCSWINSZ/TIOCGWINSZ
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5140 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/pty.h
+++ inc/pty.h
@@ -2,11 +2,13 @@
 #define JOS_INC_PTY_H
 
 #include <termios/kernel_termios.h>
+#include <sys/ioctl.h>
 
 struct pty_seg {
     char master_open;
     uint64_t slave_ref;
     struct __kernel_termios ios;
+    struct winsize winsize;
     pid_t pgrp;
 
     jthread_mutex_t mu;
