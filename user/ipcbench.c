ee53c90b32f483c6b07452c61ad9073b6b57eac2
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-01 22:53:24 +0000

    ipcbench with threads too
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5402 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/ipcbench.c
+++ user/ipcbench.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/time.h>
 
 #ifdef JOS_USER
 #include <inc/syscall.h>
 #endif
+
+struct worker_args {
+    int fd_read;
+    int fd_write;
+    int cflush;
+};
 
 static void __attribute__((noreturn))
 xperror(const char *msg)
@@ -15,11 +22,35 @@ xperror(const char *msg)
     exit(-1);
 }
 
+static void*
+worker_thread(void *arg)
+{
+    struct worker_args *a = arg;
+
+#ifdef JOS_USER
+    sys_self_set_cflush(a->cflush);
+#endif
+
+    for (;;) {
+	uint64_t v;
+	ssize_t cc = read(a->fd_read, &v, sizeof(v));
+	if (cc < 0)
+	    xperror("read");
+
+	if (cc == 0)
+	    return 0;
+
+	v++;
+	if (write(a->fd_write, &v, sizeof(v)) < 0)
+	    xperror("write");
+    }
+}
+
 int
 main(int ac, char **av)
 {
-    if (ac != 2 && ac != 3) {
-	printf("Usage: %s rttcount [cflush]\n", av[0]);
+    if (ac != 2 && ac != 3 && ac != 4) {
+	printf("Usage: %s rttcount [threads] [cflush]\n", av[0]);
 	exit(-1);
     }
 
@@ -29,11 +60,16 @@ main(int ac, char **av)
 	exit(-1);
     }
 
+    int threads = 0;
+    if (ac >= 3)
+	threads = atoi(av[2]);
+
     int cflush = 0;
-    if (ac == 3)
-	cflush = atoi(av[2]);
+    if (ac >= 4)
+	cflush = atoi(av[3]);
 
     printf("RTT count:   %d\n", count);
+    printf("Use threads: %d\n", threads);
     printf("Cache flush: %d\n", cflush);
 
     int to_worker[2];
@@ -41,29 +77,28 @@ main(int ac, char **av)
     if (pipe(to_worker) < 0 || pipe(from_worker) < 0)
 	xperror("pipe");
 
-    pid_t pid = fork();
-
 #ifdef JOS_USER
     sys_self_set_cflush(cflush);
 #endif
 
-    if (pid == 0) {
-	close(to_worker[1]);
-	close(from_worker[0]);
+    struct worker_args a;
+    a.fd_read = to_worker[0];
+    a.fd_write = from_worker[1];
+    a.cflush = cflush;
 
-	for (;;) {
-	    uint64_t v;
-	    ssize_t cc = read(to_worker[0], &v, sizeof(v));
-	    if (cc < 0)
-		xperror("read");
+    if (threads == 0) {
+	pid_t pid = fork();
 
-	    if (cc == 0)
-		exit(0);
+	if (pid == 0) {
+	    close(to_worker[1]);
+	    close(from_worker[0]);
 
-	    v++;
-	    if (write(from_worker[1], &v, sizeof(v)) < 0)
-		xperror("write");
+	    worker_thread(&a);
+	    exit(0);
 	}
+    } else {
+	pthread_t tid;
+	assert(0 == pthread_create(&tid, 0, &worker_thread, &a));
     }
 
     uint64_t v = 0;
@@ -89,6 +124,6 @@ main(int ac, char **av)
     uint64_t diff_usec =
 	(end.tv_sec - start.tv_sec) * 1000000 +
 	end.tv_usec - start.tv_usec;
-    printf("Total time: %ld usec\n", diff_usec);
-    printf("usec per rtt: %ld\n", diff_usec / count);
+    printf("Total time:  %ld usec\n", diff_usec);
+    printf("RTT usec:    %ld\n", diff_usec / count);
 }
