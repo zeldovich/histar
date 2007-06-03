4bb3b83f71aa2feed8f6295026dd345a7e7f8fcb
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-03 19:50:38 +0000

    actually need to be regparm because we point gates directly to jos_longjmp..
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5427 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/setjmp.h
+++ inc/setjmp.h
@@ -4,7 +4,7 @@
 #include <machine/setjmp.h>
 
 int  jos_setjmp(struct jos_jmp_buf *buf);
-void jos_longjmp(struct jos_jmp_buf *buf, uint64_t val)
-	__attribute__((__noreturn__));
+void jos_longjmp(struct jos_jmp_buf *buf, int val)
+	__attribute__((__noreturn__, regparm(2)));
 
 #endif
