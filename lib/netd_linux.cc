ca143f162a20ee154327682391eee3d9d26f8fea
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-05 05:38:20 +0000

    copy from user/netd
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5466 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/netd_linux.cc
+++ lib/netd_linux.cc
@@ -2,15 +2,44 @@ extern "C" {
 #include <inc/lib.h>
 #include <inc/netd.h>
 #include <inc/netdlinux.h>
+#include <inc/syscall.h>
 #include <inc/stdio.h>
+#include <inc/assert.h>
 }
 
 #include <inc/cpplabel.hh>
 #include <inc/labelutil.hh>
 #include <inc/gatesrv.hh>
+#include <inc/netdsrv.hh>
+
+enum { netd_do_taint = 0 };
 
 int
-netd_linux_server_init(void (*handler)(struct netd_op_args *))
+netd_linux_server_init(netd_handler h)
 {
+    try {
+	label cntm;
+	label clear;
+	uint64_t inet_taint = 0;
+	
+	thread_cur_label(&cntm);
+	thread_cur_clearance(&clear);
+	if (netd_do_taint)
+	    cntm.set(inet_taint, 2);
+
+	netd_server_init(start_env->shared_container,
+			 inet_taint, &cntm, &clear, h);
+	netd_server_enable();
+	
+	// Disable signals -- the signal gate has { inet_taint:* }
+	int64_t sig_gt = container_find(start_env->shared_container, kobj_gate, "signal");
+	error_check(sig_gt);
+	error_check(sys_obj_unref(COBJ(start_env->shared_container, sig_gt)));
+
+	thread_set_label(&cntm);
+    } catch (std::exception &e) {
+	panic("%s", e.what());
+    }
+    
     return 0;
 }
