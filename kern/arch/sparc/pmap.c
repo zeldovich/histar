528fc93dcebce4f47f8fdcbb3a85391461b3e811
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-06 16:34:34 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5487 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/pmap.c
+++ kern/arch/sparc/pmap.c
@@ -1,6 +1,63 @@
 #include <kern/arch.h>
 #include <kern/lib.h>
 
+int
+page_map_alloc(struct Pagemap **pm_store)
+{
+    return -E_NO_MEM;
+}
+
+void
+page_map_free(struct Pagemap *pgmap)
+{
+    /* XXX free all page table levels */
+}
+
+int
+pgdir_walk(struct Pagemap *pgmap, const void *va,
+	   int create, ptent_t **pte_store)
+{
+    return -E_INVAL;
+}
+
+int
+check_user_access(const void *ptr, uint64_t nbytes, uint32_t reqflags)
+{
+    return -E_INVAL;
+}
+
+/*
+ * Page table traversal callbacks
+ */
+
+void
+as_arch_collect_dirty_bits(const void *arg, ptent_t *ptep, void *va)
+{
+    const struct Pagemap *pgmap = arg;
+}
+
+void
+as_arch_page_invalidate_cb(const void *arg, ptent_t *ptep, void *va)
+{
+    const struct Pagemap *pgmap = arg;
+}
+
+void
+as_arch_page_map_ro_cb(const void *arg, ptent_t *ptep, void *va)
+{
+    const struct Pagemap *pgmap = arg;
+}
+
+int
+as_arch_putpage(struct Pagemap *pgmap, void *va, void *pp, uint32_t flags)
+{
+    return -E_INVAL;
+}
+
+/*
+ * Page addressing
+ */
+
 void *
 pa2kva(physaddr_t pa)
 {
