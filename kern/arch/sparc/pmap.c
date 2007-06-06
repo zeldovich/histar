93007a0a1d141b642f08c4f845b1e7ba84fe0ad0
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-06 16:39:16 +0000

    sparc stubs
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5488 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/pmap.c
+++ kern/arch/sparc/pmap.c
@@ -1,5 +1,6 @@
 #include <kern/arch.h>
 #include <kern/lib.h>
+#include <inc/error.h>
 
 int
 page_map_alloc(struct Pagemap **pm_store)
@@ -11,6 +12,13 @@ void
 page_map_free(struct Pagemap *pgmap)
 {
     /* XXX free all page table levels */
+}
+
+int
+page_map_traverse(struct Pagemap *pgmap, const void *first, const void *last,
+		  int create, page_map_traverse_cb cb, const void *arg)
+{
+    return -E_INVAL;
 }
 
 int
@@ -26,6 +34,12 @@ check_user_access(const void *ptr, uint64_t nbytes, uint32_t reqflags)
     return -E_INVAL;
 }
 
+void
+pmap_set_current(struct Pagemap *pm)
+{
+    cprintf("pmap_set_current: XXX\n");
+}
+
 /*
  * Page table traversal callbacks
  */
@@ -33,19 +47,19 @@ check_user_access(const void *ptr, uint64_t nbytes, uint32_t reqflags)
 void
 as_arch_collect_dirty_bits(const void *arg, ptent_t *ptep, void *va)
 {
-    const struct Pagemap *pgmap = arg;
+    //const struct Pagemap *pgmap = arg;
 }
 
 void
 as_arch_page_invalidate_cb(const void *arg, ptent_t *ptep, void *va)
 {
-    const struct Pagemap *pgmap = arg;
+    //const struct Pagemap *pgmap = arg;
 }
 
 void
 as_arch_page_map_ro_cb(const void *arg, ptent_t *ptep, void *va)
 {
-    const struct Pagemap *pgmap = arg;
+    //const struct Pagemap *pgmap = arg;
 }
 
 int
--- kern/arch/sparc/trap.c
+++ kern/arch/sparc/trap.c
@@ -1,6 +1,28 @@
 #include <kern/thread.h>
 #include <kern/arch.h>
 #include <kern/lib.h>
+#include <inc/error.h>
+
+void
+thread_arch_jump(struct Thread *t, const struct thread_entry *te)
+{
+    /* XXX */
+
+    for (uint32_t i = 0; i < thread_entry_narg; i++)
+	t->th_tfa.tfa_entry_args.te_arg[i] = te->te_arg[i];
+}
+
+void
+thread_arch_run(const struct Thread *t)
+{
+    panic("thread_arch_run");
+}
+
+int
+thread_arch_utrap(struct Thread *t, uint32_t src, uint32_t num, uint64_t arg)
+{
+    return -E_INVAL;
+}
 
 int
 thread_arch_get_entry_args(const struct Thread *t,
