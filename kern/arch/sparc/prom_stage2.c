2f9751a9359ff5d94a98106c5360839ee864c320
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-29 08:08:46 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5643 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/prom_stage2.c
+++ kern/arch/sparc/prom_stage2.c
@@ -1,7 +1,14 @@
+/* this is the place to simulate an OpenBoot PROM */
+
+unsigned int romvec;
+
 void __main(void);
 
-void __attribute__ ((__section__ (".prom_stage2.text"), noreturn))
+void __attribute__ ((noreturn))
 __main(void)
 { 
+    extern void start(unsigned int);
+    start(romvec);
+
     for (;;) {}
 }
