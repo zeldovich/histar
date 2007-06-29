083eb2109aab5af15e6d069cfa4ef7ad0b991e45
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-29 17:03:19 +0000

    hw saves pc in %l1
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5645 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/asm.h
+++ kern/arch/sparc/asm.h
@@ -2,4 +2,4 @@
 	.text; .align 4; .globl x; .type x,%function; x:
 
 #define TRAP_ENTRY(label) \
-        rd %psr, %l0; rd %wim, %l1; b label; nop;
+        rd %psr, %l0; rd %wim, %l3; b label; nop;
