e4fa519eeda060ec30011e07f3041cea1e924b2e
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-29 18:01:17 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5647 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/mmu.h
+++ kern/arch/sparc/mmu.h
@@ -50,31 +50,6 @@
 
 #define PTE_ADDR(e)	(((e >> PTE_PPN_SHIFT) & PTE_PPN_MASK) << PGSHIFT)
 
-
-/* The Sparc PSR fields are laid out as the following:
- *
- *  ------------------------------------------------------------------------
- *  | impl  | vers  | icc   | resv  | EC | EF | PIL  | S | PS | ET |  CWP  |
- *  | 31-28 | 27-24 | 23-20 | 19-14 | 13 | 12 | 11-8 | 7 | 6  | 5  |  4-0  |
- *  ------------------------------------------------------------------------
- */
-#define	PSR_IMPL_SHIFT	28		/* CPU implementation */
-#define	PSR_IMPL_MASK	0x0f
-#define PSR_VER_SHIFT	24		/* CPU version */
-#define PSR_VER_MASK	0x0f
-#define	PSR_ICC_SHIFT	20		/* Condition codes */
-#define PSR_ICC_MASK	0x0f
-#define PSR_EC		(1 << 13)	/* Enable coprocessor */
-#define PSR_EF		(1 << 12)	/* Enable floating-point */
-#define PSR_PIL_SHIFT	8		/* Interrupt level (masking) */
-#define PSR_PIL_MASK	0x0f
-#define PSR_PIL         (PSR_PIL_MASK << PSR_PIL_SHIFT)
-#define PSR_S		(1 << 7)	/* Supervisor */
-#define PSR_PS		(1 << 6)	/* Previous supervisor */
-#define PSR_ET		(1 << 5)	/* Enable traps */
-#define PSR_CWP_SHIFT	0		/* Current window pointer */
-#define PSR_CWP_MASK	0x0f
-
 /*
  * Trap base register 
  */
