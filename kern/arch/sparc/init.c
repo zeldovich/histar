f3d924358e7ce6be91659e2df0528b6c1907f56c
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-29 22:17:06 +0000

    poke at mmu
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5651 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/init.c
+++ kern/arch/sparc/init.c
@@ -1,11 +1,35 @@
 #include <kern/arch.h>
+#include <kern/lib.h>
 #include <machine/trap.h>
+#include <machine/srmmu.h>
 
 char boot_cmdline[256];
+
+static void
+mmu_init (void)
+{
+    uint32_t ctrl = srmmu_get_mmureg();
+    /* make sure No Fault and Enable are clear */
+    ctrl &= ~(SRMMU_CTRL_E | SRMMU_CTRL_NF);
+    srmmu_set_mmureg(ctrl);
+    /* clear Context Table Pointer and Context */
+    srmmu_set_ctable_ptr(0);
+    srmmu_set_context(0);
+}
+
+static void
+bss_init (void)
+{
+    extern char edata[], end[];
+    memset (edata, 0, end - edata);
+}
 
 void __attribute__((noreturn))
 init (void)
 {
+    mmu_init();
+    bss_init();
+
     for (;;) { }
 }
 
