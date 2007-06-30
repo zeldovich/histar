7132e25ff3a28b58c393583b81e5ef99f34b2d87
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-06-30 02:17:38 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5656 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/dev/amba.c
+++ kern/dev/amba.c
@@ -18,8 +18,8 @@ amba_init(void)
 	cfg_area += LEON3_AHB_CONF_WORDS;
     }
     
-    cfg_area = 	(uint32_t *) (LEON3_IO_AREA | LEON3_CONF_AREA | 
-			      LEON3_AHB_SLAVE_CONF_AREA);
+    cfg_area = (uint32_t *) (LEON3_IO_AREA | LEON3_CONF_AREA | 
+			     LEON3_AHB_SLAVE_CONF_AREA);
     for (uint32_t i = 0; i < LEON3_AHB_SLAVES; i++) {
 	if (LEON_BYPASS_LOAD_PA(cfg_area)) {
 	    /* do something with config record..init some drivers? */
