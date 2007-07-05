3dad8d4727913c796154ddb274b7d8e3891bff47
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-05 06:36:51 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5703 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ kern/arch/sparc/sparc-config.h
@@ -0,0 +1,10 @@
+#ifndef JOS_MACHINE_SPARC_CONFIG_H
+#define JOS_MACHINE_SPARC_CONFIG_H
+
+/* Max number of MMU contexts*/
+#define CTX_NCTX        256
+
+/* Number of register windows */
+#define NWINDOWS        8
+
+#endif
--- kern/arch/sparc/win.h
+++
@@ -1,7 +0,0 @@
-#ifndef JOS_MACHINE_WIN_H
-#define JOS_MACHINE_WIN_H
-
-#define NWINDOWS_KERN 8
-#define NWINDOWS_USER 2
-
-#endif
