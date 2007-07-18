6ea47ad98ad96d43b055391c5555beff89389700
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-18 18:42:18 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5781 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/trap.h
+++ kern/arch/sparc/trap.h
@@ -7,4 +7,7 @@ void init(void) __attribute__((noreturn));
 // Entry into kernel from user space traps
 void trap_handler(struct Trapframe *tf) __attribute__((__noreturn__));
 
+// Low-level trapframe jump in locore.S
+void trapframe_pop(const struct Trapframe *) __attribute__((__noreturn__));
+
 #endif
