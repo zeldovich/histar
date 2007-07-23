1b62adac54486bbbd1bb5652d05a5b2d5b5fac3e
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-23 18:47:35 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5814 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/atomic64.h
+++ kern/arch/sparc/atomic64.h
@@ -3,4 +3,34 @@
 
 typedef struct { volatile uint64_t counter; } jos_atomic64_t;
 
+static __inline__ void
+jos_atomic_inc64(jos_atomic64_t *v)
+{
+}
+
+static __inline__ void
+jos_atomic_dec64(jos_atomic64_t *v)
+{
+}
+
+/* Returns true if result is zero. */
+static __inline__ int
+jos_atomic_dec_and_test64(jos_atomic64_t *v)
+{
+    return 0;
+}
+
+/*
+ * Atomically compare the value in "v" with "old", and set "v" to "newv"
+ * if equal.
+ *
+ * Return value is the previous value of "v".  So if return value is same
+ * as "old", the swap occurred, otherwise it did not.
+ */
+static __inline__ uint64_t
+jos_atomic_compare_exchange64(jos_atomic64_t *v, uint64_t old, uint64_t newv)
+{
+    return 0;
+}
+
 #endif
