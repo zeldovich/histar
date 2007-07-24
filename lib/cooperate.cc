e8c8877c177d89155370cddf205dd2f7975eab8f
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-24 00:03:46 +0000

    compile fix
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5834 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/cooperate.cc
+++ lib/cooperate.cc
@@ -146,7 +146,7 @@ coop_gate_create(uint64_t container,
     scope_guard<int, cobj_ref> as_unref(sys_obj_unref, as);
 
     struct u_segment_mapping usm[coop_as_slots];
-    struct u_address_space uas = { 0, 0, 0, coop_as_slots, coop_as_slots, &usm[0] };
+    struct u_address_space uas = { coop_as_slots, coop_as_slots, &usm[0], 0, 0, 0 };
     coop_gen_as(status_seg, text_seg, data_seg_len, &uas);
     error_check(sys_as_set(as, &uas));
 
@@ -193,10 +193,10 @@ coop_verify(cobj_ref coop_gate, coop_sysarg arg_values[8],
     should_be(r == 1);
 
     struct u_segment_mapping usm_actual[coop_as_slots];
-    struct u_address_space uas_actual = { 0, 0, 0, coop_as_slots, coop_as_slots, &usm_actual[0] };
+    struct u_address_space uas_actual = { coop_as_slots, coop_as_slots, &usm_actual[0], 0, 0, 0 };
 
     struct u_segment_mapping usm_ideal[coop_as_slots];
-    struct u_address_space uas_ideal = { 0, 0, 0, coop_as_slots, coop_as_slots, &usm_ideal[0] };
+    struct u_address_space uas_ideal = { coop_as_slots, coop_as_slots, &usm_ideal[0], 0, 0, 0 };
 
     error_check(sys_as_get(te.te_as, &uas_actual));
     should_be(uas_actual.nent == coop_as_slots);
