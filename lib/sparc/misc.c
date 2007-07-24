872e7e34a791608fe480427e19f58f0a530cb331
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-24 19:17:15 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5850 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/sparc/misc.c
+++ lib/sparc/misc.c
@@ -1,6 +1,7 @@
 #include <inc/lib.h>
 #include <inc/arch.h>
 #include <inc/utrap.h>
+#include <inc/debug_gate.h>
 
 uint64_t 
 arch_read_tsc(void)
@@ -18,4 +19,19 @@ int
 utrap_set_mask(int masked)
 {
     return 0;
+}
+
+void
+debug_gate_breakpoint(void)
+{
+}
+
+void
+arch_fpregs_save(void *a)
+{
+}
+
+void
+arch_fpregs_restore(void *a)
+{
 }
