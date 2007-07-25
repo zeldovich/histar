7e0a3d6a184c82076f953f6ac6c409a02f7686da
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-25 09:07:03 +0000

    SATA FIS definitions
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5875 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ kern/dev/satareg.h
@@ -0,0 +1,44 @@
+#ifndef JOS_DEV_SATAREG_H
+#define JOS_DEV_SATAREG_H
+
+struct sata_fis_reg {
+    uint8_t type;
+    uint8_t __pad0;
+    union {
+	uint8_t command;	/* H2D */
+	uint8_t status;		/* D2H */
+    };
+
+    union {
+	uint8_t features;	/* H2D */
+	uint8_t error;		/* D2H */
+    };
+
+    uint8_t sector;
+    uint8_t cyl_lo;
+    uint8_t cyl_hi;
+    uint8_t dev_head;
+    uint8_t sector_ex;
+    uint8_t cyl_lo_ex;
+    uint8_t cyl_hi_ex;
+    uint8_t features_ex;
+    uint8_t sector_count;
+    uint8_t sector_count_ex;
+    uint8_t __pad1;
+    uint8_t control;
+    uint8_t __pad2[4];
+};
+
+struct sata_fis_devbits {
+    uint8_t type;
+    uint8_t intr;
+    uint8_t status;
+    uint8_t error;
+    uint8_t __pad[4];
+};
+
+#define SATA_FIS_TYPE_REG_H2D	0x27
+#define SATA_FIS_TYPE_REG_D2H	0x34
+#define SATA_FIS_TYPE_DEVBITS	0xA1	/* always D2H */
+
+#endif
