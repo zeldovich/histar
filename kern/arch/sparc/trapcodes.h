af8fbe8b4639e43df9cc4d6d00a40fd6c232f9ed
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-07-27 19:14:58 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@5959 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/trapcodes.h
+++ kern/arch/sparc/trapcodes.h
@@ -33,9 +33,10 @@
 /*
  * Software-defined traps
  */
-#define	T_SYSCALL	0x80
+#define T_SYSCALL	0x80
 #define T_BREAKPOINT	0x81
 #define T_FLUSHWIN	0x82
+#define SOFTWARE_TRAP(num) ((num) - 0x80)
 
 #define NTRAPS		0x100
 
