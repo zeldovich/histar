cb53c4e9498a83c2e3a6229e85ba11b2574a92cb
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-08-05 02:00:53 +0000

    more benchmarks for cobj_get
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6086 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/um/main.c
+++ kern/arch/um/main.c
@@ -6,13 +6,17 @@
 #include <kern/pstate.h>
 #include <kern/prof.h>
 #include <kern/timer.h>
+#include <kern/handle.h>
 #include <inc/error.h>
 
 static void
 test_stuff(void)
 {
+    struct Container *c;
+    struct Segment *s[20];
     struct Label *l1, *l2;
     struct kobject *ko;
+    const struct kobject *cko;
     void *p;
 
     assert(0 == label_alloc(&l1, 1));
@@ -23,6 +27,15 @@ test_stuff(void)
 
     assert(0 == label_set(l1, 456, 0));
     assert(0 == label_set(l2, 789, 3));
+
+    assert(0 == container_alloc(l1, &c));
+    kobject_incref_resv(&c->ct_ko, 0);
+    c->ct_ko.ko_quota_total = CT_QUOTA_INF;
+
+    for (uint32_t i = 0; i < sizeof(s) / sizeof(*s); i++) {
+	assert(0 == segment_alloc(l1, &s[i]));
+	assert(0 == container_put(c, &s[i]->sg_ko));
+    }
 
     uint64_t t0, t1;
     uint64_t count = 1000000;
@@ -84,12 +97,20 @@ test_stuff(void)
     cprintf("kobject get page: %"PRIu64" nsec\n", (t1 - t0) / count);
 
     t0 = timer_user_nsec();
-    for (uint64_t i = 0; i < count; i++) {
-	const struct kobject *cko;
+    for (uint64_t i = 0; i < count; i++)
 	assert(0 == kobject_get(ko->hdr.ko_id, &cko, kobj_label, iflow_none));
-    }
     t1 = timer_user_nsec();
     cprintf("kobject get: %"PRIu64" nsec\n", (t1 - t0) / count);
+
+    for (uint64_t j = 0; j < sizeof(s) / sizeof(*s); j++) {
+	t0 = timer_user_nsec();
+	for (uint64_t i = 0; i < count; i++)
+	    assert(0 == cobj_get(COBJ(c->ct_ko.ko_id, s[j]->sg_ko.ko_id),
+				 kobj_segment, &cko, iflow_none));
+	t1 = timer_user_nsec();
+	cprintf("cobj_get %02"PRIu64"'th: %"PRIu64" nsec\n",
+		j, (t1 - t0) / count);
+    }
 }
 
 int
@@ -107,6 +128,7 @@ main(int ac, char **av)
     sched_init();
     pstate_init();
     prof_init();
+    key_generate();
 
     cprintf("Ready.\n");
     test_stuff();
