abdc87e63c1ec1d4446983a48632adbf82e2eea6
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-04 21:44:52 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6276 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/dj.mk
+++ conf/embedbin/dj.mk
@@ -1,4 +1,4 @@
-KERN_BINFILES   += user/xhelper user/djwebappd user/eprocd-launch user/jntpd
+KERN_BINFILES   += user/xhelper user/djwebappd user/eprocd-launch
 KERN_BINFILES   += user/djd user/djc user/ctallocd user/djechod user/djfsd
 KERN_BINFILES   += user/djperld user/djauthproxy user/djlogin user/djguardcall user/remote-run
 KERN_BINFILES   += user/dhelper user/zhelper.sh
