6ea1cf75e2d70b5a88efe35b61f1111bc8b38a3b
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-05 18:30:33 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6278 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/httpd.mk
+++ conf/embedbin/httpd.mk
@@ -1,6 +1,6 @@
 KERN_BINFILES	+= user/server.pem user/dh.pem user/servkey.pem
 KERN_BINFILES	+= user/httpd user/httpd_mom user/ssld user/ssl_eprocd
-KERN_BINFILES   += user/inetd user/httpd2 user/modulei
+KERN_BINFILES   += user/inetd user/httpd2
 
 KERN_BINFILES	+= user/gs user/gs.tar
 KERN_BINFILES   += user/a2ps user/a2ps.tar
