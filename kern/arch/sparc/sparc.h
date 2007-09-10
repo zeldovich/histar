aad88ae70aad6a9dffa8b2605755d688966ae426
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-10 20:39:00 +0000

    asi value in lda/sta instructions gets encoded in the instruction itself, so need to do a switch for gcc3 which is not smart enough
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6302 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/sparc.h
+++ kern/arch/sparc/sparc.h
@@ -52,23 +52,50 @@ uint32_t
 lda(uint32_t regaddr, uint32_t asi)
 {
     uint32_t retval;
-    __asm__ __volatile__("lda [%1] %2, %0"
-			 : "=r" (retval)
-			 : "r" (regaddr),
-			   "i" (asi)
-			 : "memory");
+
+    switch (asi) {
+#define ASI_CASE(asival)			    \
+    case asival:				    \
+	__asm__ __volatile__("lda [%1] %2, %0"	    \
+			     : "=r" (retval)	    \
+			     : "r" (regaddr),	    \
+			       "i" (asival)	    \
+			     : "memory");	    \
+	break;
+
+    ASI_CASE(ASI_MMUREGS);
+    ASI_CASE(ASI_BYPASS);
+#undef ASI_CASE
+
+    default:
+	panic("Unknown ASI value %d in lda\n", asi);
+    }
+
     return retval;
 }
 
 void
 sta(uint32_t addr, uint32_t val, uint32_t asi)
 {
-    __asm__ __volatile__("sta %0, [%1] %2"
-			 :
-			 : "r" (val),
-			   "r" (addr),
-			   "i" (asi)
-			 : "memory"); 
+    switch (asi) {
+#define ASI_CASE(asival)			    \
+    case asival:				    \
+	__asm__ __volatile__("sta %0, [%1] %2"	    \
+			     :			    \
+			     : "r" (val),	    \
+			       "r" (addr),	    \
+			       "i" (asival)	    \
+			     : "memory");	    \
+	break;
+
+    ASI_CASE(ASI_MMUREGS);
+    ASI_CASE(ASI_BYPASS);
+    ASI_CASE(ASI_DFLUSH);
+#undef ASI_CASE
+
+    default:
+	panic("Unknown ASI value %d in sta\n", asi);
+    }
 }
 
 void
