e7043365b3531cb19676b3c657165a9de35e8c43
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-15 00:00:58 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6328 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/tag.c
+++ kern/arch/sparc/tag.c
@@ -23,19 +23,74 @@ tag_trap(struct Trapframe *tf, uint32_t tbr)
 {
     cprintf("tag trap...\n");
 
-    cprintf("  et  = 0x%x\n", read_et());
-    cprintf("  eoa = 0x%x\n", read_eoa());
+    uint32_t pctag = read_pctag();
+    uint32_t et = read_et();
+    uint32_t cause = (et >> ET_CAUSE_SHIFT) & ET_CAUSE_MASK;
+    uint32_t dtag = (et >> ET_TAG_SHIFT) & ET_TAG_MASK;
+
+    cprintf("  pc tag = %d\n", pctag);
+    cprintf("  d tag  = %d\n", dtag);
+    cprintf("  cause  = %d\n", cause);
     cprintf("  tsr = 0x%x\n", read_tsr());
     cprintf("  tf  = %p\n", tf);
     cprintf("  pc  = 0x%x\n", tf->tf_pc);
-    cprintf("  tbr = 0x%x\n", tbr);
 
-    abort();
+    switch (cause) {
+    case ET_CAUSE_PCV:
+    case ET_CAUSE_DV:
+	panic("Missing PC/data tag valid bits?!");
+
+    case ET_CAUSE_READ:
+	wrtperm(pctag, dtag, TAG_PERM_READ);
+	break;
+
+    case ET_CAUSE_WRITE:
+	wrtperm(pctag, dtag, TAG_PERM_READ | TAG_PERM_WRITE);
+	break;
+
+    case ET_CAUSE_EXEC:
+	wrtperm(pctag, dtag, TAG_PERM_EXEC | TAG_PERM_READ);
+	break;
+
+    default:
+	panic("Unknown cause value from the ET register\n");
+    }
+
+    for (uint32_t i = 0; i < (1 << TAG_PC_BITS); i++)
+	for (uint32_t j = 0; j < (1 << TAG_DATA_BITS); j++)
+	    wrtperm(i, j, TAG_PERM_READ | TAG_PERM_WRITE | TAG_PERM_EXEC);
+
+    static int count = 0;
+    count++;
+    if (count == 4) {
+	cprintf("enough traps\n");
+	abort();
+    }
+
+    tag_trap_return(tf, tbr);
 }
+
+volatile uint32_t v;
 
 void
 tag_init(void)
 {
+    write_rma((uintptr_t) &tag_trap_entry);
+
+    cprintf("Initializing tag permission table.. ");
+
+    /* Per-tag valid bits appear to be useless */
+    for (uint32_t i = 0; i < (1 << TAG_PC_BITS); i++)
+	wrtpcv(i, 1);
+
+    for (uint32_t i = 0; i < (1 << TAG_DATA_BITS); i++)
+	wrtdv(i, 1);
+
+    for (uint32_t i = 0; i < (1 << TAG_PC_BITS); i++)
+	for (uint32_t j = 0; j < (1 << TAG_DATA_BITS); j++)
+	    wrtperm(i, j, 0);
+    cprintf("done.\n");
+
     cprintf("Initializing memory tags.. ");
     for (uint32_t i = 0; i < global_npages; i++)
 	tag_set(pa2kva(ppn2pa(i)), DTAG_NOACCESS, PGSIZE);
@@ -46,22 +101,20 @@ tag_init(void)
 
     cprintf("Tag testing..\n");
 
-    static uint32_t v;
     cprintf("vp = %p\n", &v);
-    cprintf("v tag = %d\n", read_dtag(&v));
-    write_dtag(&v, 6);
-    cprintf("v tag = %d\n", read_dtag(&v));
+    cprintf("v tag = %d\n", read_dtag((void *) &v));
+    write_dtag((void *) &v, 6);
+    cprintf("v tag = %d\n", read_dtag((void *) &v));
 
     cprintf("pc tag = %d\n", read_pctag());
-    write_pctag(7);
+    write_pctag(2);
     cprintf("pc tag = %d\n", read_pctag());
 
     cprintf("tsr = 0x%x\n", read_tsr());
 
-    cprintf("rma = 0x%x\n", read_rma());
-    write_rma((uintptr_t) &tag_trap_entry);
-    cprintf("rma = 0x%x\n", read_rma());
-
+    cprintf("about to cause tag traps...\n");
     write_tsr(0);
+    v = 5;
+
     cprintf("tsr = 0x%x\n", read_tsr());
 }
--- kern/arch/sparc/tag.h
+++ kern/arch/sparc/tag.h
@@ -1,17 +1,34 @@
 #ifndef JOS_MACHINE_TAG_H
 #define JOS_MACHINE_TAG_H
 
-#define TAG_PERM_READ	(1 << 0)
-#define TAG_PERM_WRITE	(1 << 1)
-#define TAG_PERM_EXEC	(1 << 2)
-
 #define TSR_EG		(1 << 27)	/* Exception Globals */
 #define TSR_PEG		(1 << 26)	/* Previous Exception Globals */
 #define TSR_T		(1 << 25)	/* Trust */
 #define TSR_PT		(1 << 24)	/* Previous Trust */
 
-#define TAG_PC_BITS	4
-#define TAG_DATA_BITS	4
+#define ET_OPCODE_SHIFT	0
+#define ET_OPCODE_MASK	0x7fff
+#define ET_CAUSE_SHIFT	15
+#define ET_CAUSE_MASK	0x7
+#define ET_TAG_SHIFT	28
+#define ET_TAG_MASK	0xf
+
+#define ET_CAUSE_PCV	0
+#define ET_CAUSE_DV	1
+#define ET_CAUSE_READ	2
+#define ET_CAUSE_WRITE	3
+#define ET_CAUSE_EXEC	4
+
+/*
+ * Permissions table
+ */
+
+#define TAG_PERM_READ	(1 << 0)
+#define TAG_PERM_WRITE	(1 << 1)
+#define TAG_PERM_EXEC	(1 << 2)
+
+#define TAG_PC_BITS	7
+#define TAG_DATA_BITS	7
 
 /*
  * Pre-defined tag values
