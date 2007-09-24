388ad3f05c7948cbf5e8fd7035c587181c1d1d14
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-24 19:12:17 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6377 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/arch/sparc/init.c
+++ kern/arch/sparc/init.c
@@ -8,7 +8,6 @@
 #include <machine/trap.h>
 #include <machine/pmap.h>
 #include <machine/sparc-common.h>
-#include <machine/tag.h>
 #include <dev/apbucons.h>
 #include <dev/amba.h>
 #include <dev/irqmp.h>
@@ -57,8 +56,6 @@ init (void)
     kobject_init();
     sched_init();
     pstate_init();
-
-    //tag_init();
 
     user_init();
 
