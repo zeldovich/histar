fc6b111ec4411855cca9605bb9bfb668de328592
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-09-30 19:06:45 +0000

    calculates average conns/sec from dj-bench.sh .out files
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6494 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ tools/dj-max-average.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl -w
+# Expects the number of connections completed to be on the second 
+# to last line of the .out files from dj-bench.pl
+
+use strict;
+
+my $ac = @ARGV;
+
+($ac >= 3) || die "usage: name clientmin clientmax [ numruns ] [ duration ]\n";
+
+my $name = $ARGV[0];
+my $clientmin = $ARGV[1];
+my $clientmax = $ARGV[2];
+my $numruns = 3;
+my $duration = 20;
+if ($ac >= 4) {
+    my $numruns = $ARGV[3];
+}
+if ($ac >= 5) {
+    my $duration = $ARGV[4];
+}
+
+my $best_ave = 0;
+my $best_clients = 0;
+for (my $j = $clientmin; $j <= $clientmax; $j++) {
+    my @conns;
+    for (my $i = 0; $i < $numruns; $i++) {
+	my $fname = "$name-c$j-n$i.out";
+	open(F, $fname) || die "unable to open $fname";
+	
+	my $i = 0;
+	my @lines = <F>;
+	my $line;
+	while ($line = pop @lines) {
+	    chomp $line;
+	    if ($i == 1) {
+		push(@conns, $line);
+	    }
+	    if ($line =~ m/.*error.*/i) {
+		print "WARNING ($fname): $line\n";
+	    }
+	    $i++;
+	}
+	close(F);
+    }
+
+    my $total = 0;
+    foreach my $count (@conns) {
+	$total += $count;
+    } 
+    my $ave = $total / $numruns;
+    
+    if ($ave > $best_ave) {
+	$best_ave = $ave;
+	$best_clients = $j;
+    }
+}
+
+my $persec = $best_ave / $duration;
+
+$persec = sprintf("%.2f", $persec);
+$best_ave = sprintf("%.2f", $best_ave);
+
+print "$name\t$best_clients\t$best_ave\t$persec\n";
