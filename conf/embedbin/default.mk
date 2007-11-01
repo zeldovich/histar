e26ef661e30815e63628667f41c28534e7477bc4
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-01 04:43:34 +0000

    include libcrypt for shared libs
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6802 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -3,7 +3,7 @@ KERN_BINFILES	+= user/netd_mom user/jntpd
 
 ## Shared libraries, if enabled
 ifeq ($(SHARED_ENABLE),yes)
-KERN_BINFILES	+= user/ld.so user/libm.so user/libutil.so user/libdl.so
+KERN_BINFILES	+= user/ld.so user/libm.so user/libutil.so user/libdl.so user/libcrypt.so
 endif
 
 ## Pick your TCP stack
