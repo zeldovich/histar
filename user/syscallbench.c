378d8a46a7f1c5c55099fec89fe2e2999e3d6285
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-06 00:17:55 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6836 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/syscallbench.c
+++ user/syscallbench.c
@@ -1,5 +1,7 @@
 #include <inc/syscall.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
 
 #if defined(JOS_ARCH_amd64) || defined(JOS_ARCH_i386)
 #include <machine/x86.h>
@@ -18,13 +20,17 @@ main(int ac, char **av)
     uint64_t tsc0 = GET_TSC();
 
     int rounds = 1000000;
+    if (ac == 2)
+	rounds = atoi(av[1]);
+
     for (int i = 0; i < rounds; i++)
 	sys_self_id();
 
     uint64_t nsec1 = sys_clock_nsec();
     uint64_t tsc1 = GET_TSC();
 
-    printf("Syscall latency: %ld nsec / %ld cycles for %d rounds\n",
+    printf("Syscall latency: %"PRIu64" nsec / %"PRIu64" cycles for %d rounds\n",
 	   nsec1 - nsec0, tsc1 - tsc0, rounds);
-    printf("Average cycles per syscall: %ld\n", (tsc1 - tsc0) / rounds);
+    printf("Average cycles per syscall: %"PRIu64"\n", (tsc1 - tsc0) / rounds);
+    printf("Average nsec per syscall: %"PRIu64"\n", (nsec1 - nsec0) / rounds);
 }
