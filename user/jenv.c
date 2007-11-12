7f828a5328f94aac8ebcd441cbcb65592374f271
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-12 22:42:11 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6877 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/jenv.c
+++ user/jenv.c
@@ -5,8 +5,10 @@
 int
 main(int ac, char **av)
 {
-#define PRINT_64(f) printf("%20s: %"PRIu64"\n", #f, start_env->f);
-#define PRINT_32(f) printf("%20s: %"PRIu32"\n", #f, start_env->f);
+#define PRINT_64(f)  printf("%20s: %"PRIu64"\n", #f, start_env->f);
+#define PRINT_32(f)  printf("%20s: %"PRIu32"\n", #f, start_env->f);
+#define PRINT_OBJ(f) printf("%20s: %"PRIu64".%"PRIu64"\n", #f, \
+			    start_env->f.container, start_env->f.object);
 
     PRINT_64(proc_container);
     PRINT_64(shared_container);
@@ -16,6 +18,11 @@ main(int ac, char **av)
     PRINT_64(user_taint);
     PRINT_64(user_grant);
     PRINT_64(ppid);
+
+    PRINT_OBJ(process_gid_seg);
+    PRINT_OBJ(declassify_gate);
+    PRINT_OBJ(time_seg);
+    PRINT_OBJ(fs_mtab_seg);
 
     PRINT_32(ruid);
     PRINT_32(euid);
