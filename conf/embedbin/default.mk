4877cd8c23461ac4a3d0d3f5263ee81825c8f217
stutsman <stutsman@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-20 22:33:47 +0000

    env is useful for build processes
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6897 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -29,7 +29,7 @@ KERN_BINFILES	+= user/bash user/grep user/egrep user/uniq user/mv user/cat
 KERN_BINFILES	+= user/links user/od user/du user/head user/tail user/sync
 KERN_BINFILES	+= user/md5sum user/sha1sum user/mkdir user/stty user/printf
 KERN_BINFILES	+= user/vim user/echo user/dirname user/basename user/file
-KERN_BINFILES	+= user/sleep
+KERN_BINFILES	+= user/sleep user/env
 
 ## SSH
 KERN_BINFILES	+= user/ptyd user/sshd user/ssh.tar user/scp user/sshdi
