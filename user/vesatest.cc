05bf670977b4f96dbbe9637d81c605fae605aa28
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-21 09:51:17 +0000

    playing with colors
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6924 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/vesatest.cc
+++ user/vesatest.cc
@@ -1,9 +1,12 @@
 extern "C" {
 #include <stdio.h>
+#include <stdlib.h>
 #include <inc/syscall.h>
 }
 
 #include <inc/error.hh>
+
+enum { bufsize = 1024 };
 
 int
 main(int ac, char **av)
@@ -17,10 +20,21 @@ try
     printf("yres = %d\n", fb_mode.yres);
     printf("bpp  = %d\n", fb_mode.bpp);
 
-    for (uint32_t i = 0; i < 256; i++) {
-	uint8_t buf[1024];
-	memset(buf, i, sizeof(buf));
-	error_check(sys_fb_set(sizeof(buf) * i, sizeof(buf), buf));
+    for (uint32_t i = 0; i < 1000000; i++) {
+	uint8_t buf[3];
+
+	buf[0] = random();
+	buf[1] = random();
+	buf[2] = random();
+
+	uint32_t x0 = random() % (fb_mode.xres - 3);
+	uint32_t y0 = random() % (fb_mode.yres - 3);
+	uint32_t xd = random() % MIN(256, fb_mode.xres - 3 - x0);
+	uint32_t yd = random() % MIN(256, fb_mode.yres - 3 - y0);
+
+	for (uint32_t x = x0; x < x0 + xd; x++)
+	    for (uint32_t y = y0; y < y0 + yd; y++)
+		error_check(sys_fb_set(3 * (y * fb_mode.xres + x), 3, buf));
     }
 } catch (std::exception &e) {
     printf("Caught exception: %s\n", e.what());
