3733ebc1673a0797f664205a01996f825fb00da8
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-27 01:38:04 +0000

    symbol names for backtraces
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@6941 927ebb28-5305-0410-b3ca-c4b640df69b7

--- lib/debug.cc
+++ lib/debug.cc
@@ -2,6 +2,7 @@ extern "C" {
 #include <inc/lib.h>
 #include <inc/stdio.h>
 #include <stdio.h>
+#include <dlfcn.h>
 }
 
 #include <inc/backtracer.hh>
@@ -12,9 +13,31 @@ print_backtrace(int use_cprintf)
     backtracer bt;
     for (int i = 0; i < bt.backtracer_depth(); i++) {
 	void *addr = bt.backtracer_addr(i);
+
+	const char *fn = "unknown";
+	const char *sn = "unknown";
+	void *off = 0;
+
+	Dl_info dli;
+	int r = dladdr(addr, &dli);
+	if (r >= 0) {
+	    fn = dli.dli_fname;
+	    sn = dli.dli_sname;
+	    off = (void *) (((uintptr_t)addr) - ((uintptr_t)dli.dli_saddr));
+	}
+
 	if (use_cprintf)
-	    cprintf("  %p\n", addr);
+	    cprintf("  %s:%s+%p (%p)\n", fn, sn, off, addr);
 	else
-	    fprintf(stderr, "  %p\n", addr);
+	    fprintf(stderr, "  %s:%s+%p (%p)\n", fn, sn, off, addr);
     }
 }
+
+extern "C" int
+__dummy_dladdr(const void *addr, Dl_info *info)
+{
+    return -1;
+}
+
+extern "C" __typeof(dladdr) dladdr
+__attribute__((weak, alias ("__dummy_dladdr")));
