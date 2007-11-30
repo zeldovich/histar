64ab68059d886a40cac80406006e6ab6eb712a33
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-11-30 01:50:02 +0000

    forgot to svn add
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7014 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ inc/gccwrap.h
@@ -0,0 +1,12 @@
+#define GCC_WRAPS_COMMON \
+    GCC_WRAP(_Unwind_Backtrace) \
+    GCC_WRAP(_Unwind_Resume) \
+    GCC_WRAP(_Unwind_GetIP)
+
+#if defined(JOS_ARCH_i386)
+#define GCC_WRAPS_ARCH \
+    GCC_WRAP(__moddi3) \
+    GCC_WRAP(__umoddi3) \
+    GCC_WRAP(__divdi3) \
+    GCC_WRAP(__udivdi3)
+#endif
