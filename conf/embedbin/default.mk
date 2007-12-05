9898a688adfc90953b37a222fb4af2e2b2eac212
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-12-05 19:52:42 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7096 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -54,6 +54,15 @@ KERN_BINFILES	+= user/strip user/readelf
 ## ClamAV
 KERN_BINFILES	+= user/clamscan user/clamav_main.cvd user/clamav_daily.cvd
 
+## X11
+#KERN_BINFILES	+= user/xlogo user/xeyes user/xhost user/xclock user/xauth
+#KERN_BINFILES	+= user/libXext.so.6 user/libXmu.so.6 user/libXt.so.6
+#KERN_BINFILES	+= user/libX11.so.6 user/libSM.so.6 user/libICE.so.6
+#KERN_BINFILES	+= user/libXau.so.6 user/libXdmcp.so.6 user/libXaw.so.8
+#KERN_BINFILES	+= user/libXp.so.6 user/libXprintUtil.so.1 user/libXrender.so.1
+#KERN_BINFILES	+= user/libXft.so.2 user/libXpm.so.4 user/libXmuu.so.1
+#KERN_BINFILES	+= user/libxkbfile.so.1
+
 ## Graphics stuff
 #KERN_BINFILES	+= user/libfreetype.so.6 user/libfontconfig.so.1
 #KERN_BINFILES	+= user/fc-cache user/fc-list user/libexpat.so.1
