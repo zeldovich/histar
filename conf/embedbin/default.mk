075dd750c185d55cff0c433bbd45b21f76d64585
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-12-06 09:52:51 +0000

    c++ headers
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7115 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -36,7 +36,7 @@ KERN_BINFILES	+= user/sleep user/env user/strings user/tty user/xargs
 KERN_BINFILES	+= user/rmdir user/diff user/reset user/hostname user/id
 KERN_BINFILES	+= user/less user/dd user/ed user/patch user/comm user/wc
 KERN_BINFILES	+= user/seq user/fgrep user/date user/tee user/pwd
-KERN_BINFILES	+= user/bzip2 user/bunzip2
+KERN_BINFILES	+= user/bzip2 user/bunzip2 user/cmp
 
 ## Networking tools
 KERN_BINFILES	+= user/arp user/ifconfig user/netstat user/rarp user/route
