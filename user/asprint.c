7519c0d2fc5d946fedbb9bc8a515fccf705fefbf
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-12-06 22:15:51 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7120 927ebb28-5305-0410-b3ca-c4b640df69b7

--- user/asprint.c
+++ user/asprint.c
@@ -54,26 +54,41 @@ as_print(struct cobj_ref as)
 static void
 usage(const char *n)
 {
-    printf("Usage: %s container object\n", n);
+    printf("Usage:\n");
+    printf("  %s container object\n", n);
+    printf("  %s filename\n", n);
 }
 
 int
 main(int ac, char **av)
 {
-    if (ac != 3) {
+    if (ac != 2 && ac != 3) {
 	usage(av[0]);
 	return -1;
     }
 
     struct cobj_ref as;
-    if (strtou64(av[1], 0, 10, &as.container) < 0) {
-	usage(av[0]);
-	return -1;
+    if (ac == 3) {
+	if (strtou64(av[1], 0, 10, &as.container) < 0) {
+	    usage(av[0]);
+	    return -1;
+	}
+
+	if (strtou64(av[2], 0, 10, &as.object) < 0) {
+	    usage(av[0]);
+	    return -1;
+	}
     }
 
-    if (strtou64(av[2], 0, 10, &as.object) < 0) {
-	usage(av[0]);
-	return -1;
+    if (ac == 2) {
+	struct fs_inode ino;
+	int r = fs_namei(av[1], &ino);
+	if (r < 0) {
+	    printf("fs_namei(%s): %s\n", av[1], e2s(r));
+	    return -1;
+	}
+
+	as = ino.obj;
     }
 
     as_print(as);
