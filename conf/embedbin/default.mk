90b3421083af171a17bd0f8d610d4dd184f059cb
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2007-12-17 01:16:15 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7140 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/default.mk
+++ conf/embedbin/default.mk
@@ -20,7 +20,7 @@ endif
 KERN_BINFILES	+= user/jls user/asprint user/gl user/jcat user/taintcat
 KERN_BINFILES	+= user/jrm user/jcp user/jecho user/mount user/umount
 KERN_BINFILES	+= user/jsync user/jmkdir user/reboot user/ureboot
-KERN_BINFILES	+= user/jenv user/ps user/wrap user/newpty
+KERN_BINFILES	+= user/jenv user/ps user/wrap user/newpty user/jtrue
 KERN_BINFILES	+= user/auth_log user/auth_dir user/auth_user
 KERN_BINFILES	+= user/login user/adduser user/passwd user/spawn
 
