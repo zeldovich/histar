6fad429dfb2fbb62072622506d87793fd0147c81
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-01-18 19:27:45 +0000

    add shared libs for dstar
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7163 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/embedbin/dj.mk
+++ conf/embedbin/dj.mk
@@ -1,6 +1,15 @@
 KERN_BINFILES   += user/bootstrapd user/djwebappd user/eprocd-launch
 KERN_BINFILES   += user/djd user/djc user/ctallocd user/djechod user/djfsd
-KERN_BINFILES   += user/djperld user/djauthproxy user/djlogin user/djguardcall user/remote-run
+KERN_BINFILES   += user/djperld user/djauthproxy user/djlogin
+KERN_BINFILES	+= user/djguardcall user/remote-run
 KERN_BINFILES   += user/bootstrapc user/zhelper.sh
 
-INITTAB_ENTRIES += /bin/djd:r /bin/ctallocd: /bin/djfsd: /bin/djechod: /bin/djauthproxy: /bin/djwebappd: /bin/djguardcall: /bin/bootstrapd:r
+ifeq ($(SHARED_ENABLE),yes)
+KERN_BINFILES	+= user/libgmp.so.3 user/libasync.so.0 user/libarpc.so.0
+KERN_BINFILES	+= user/libsfscrypt.so.0 user/libsvc.so.0 user/libsfsmisc.so.0
+endif
+
+INITTAB_ENTRIES += /bin/djd:r /bin/ctallocd: /bin/djfsd:
+INITTAB_ENTRIES += /bin/djechod: /bin/djauthproxy: /bin/djwebappd:
+INITTAB_ENTRIES += /bin/djguardcall: /bin/bootstrapd:r
+
