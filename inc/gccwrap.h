c01803e894c17833bb6fd5867165ba14e383a607
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-02-13 19:53:22 +0000

    recent rxvt-unicode
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7278 927ebb28-5305-0410-b3ca-c4b640df69b7

--- inc/gccwrap.h
+++ inc/gccwrap.h
@@ -1,7 +1,8 @@
 #define GCC_WRAPS_COMMON \
     GCC_WRAP(_Unwind_Backtrace) \
     GCC_WRAP(_Unwind_Resume) \
-    GCC_WRAP(_Unwind_GetIP)
+    GCC_WRAP(_Unwind_GetIP) \
+    GCC_WRAP(__popcountdi2)
 
 #if defined(JOS_ARCH_i386)
 #define GCC_WRAPS_ARCH \
