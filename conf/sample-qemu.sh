9e5796fe8f181fa8fe625e968bfd9615a914040d
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-02-14 00:15:36 +0000

    need at least 256 megs now
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7280 927ebb28-5305-0410-b3ca-c4b640df69b7

--- conf/sample-qemu.sh
+++ conf/sample-qemu.sh
@@ -3,7 +3,7 @@ qemu-system-x86_64 \
 	-serial stdio \
 	-hda ./obj/boot/bochs.img \
 	-hdb ./obj/fs/fs.img \
-	-m 128 \
+	-m 256 \
 	-redir tcp:9923::23 \
 	-redir tcp:9922::22 \
 	-redir tcp:9980::80
