c452583bdc8f2e650f202a7177a3ebd3a7054c45
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-04-26 17:51:14 +0000

    long double math stubs
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7476 927ebb28-5305-0410-b3ca-c4b640df69b7

---
+++ pkg/uclibc/libm/libm_stubs.c
@@ -0,0 +1,108 @@
+#include <math.h>
+#include "math_private.h"
+#include <bits/unimpl.h>
+
+long double
+logl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+ldexpl(long double x, int expv)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+floorl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+ceill(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+tanl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+atan2l(long double y, long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+sqrtl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+cosl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+sinl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+rintl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long int
+lrintl(long double x)
+{
+    set_enosys();
+    return 0;
+}
+
+long double
+powl(long double x, long double y)
+{
+    set_enosys();
+    return 0.0;
+}
+
+long double
+remainderl(long double x, long double y)
+{
+    set_enosys();
+    return 0.0;
+}
+
+int
+__isinfl(long double x)
+{
+    set_enosys();
+    return 0;
+}
+
+int
+__isnanl(long double x)
+{
+    set_enosys();
+    return 0.0;
+}
