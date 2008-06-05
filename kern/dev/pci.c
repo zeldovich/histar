b4ca06e14a67e2819bd0d50ad5a95639043e6960
silasb <silasb@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-06-05 15:43:32 +0000

    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7502 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/dev/pci.c
+++ kern/dev/pci.c
@@ -37,10 +37,12 @@ struct pci_driver pci_attach_vendor[] = {
     { 0x10ec, 0x8029, &ne2kpci_attach },
     { 0x8086, 0x1229, &fxp_attach },
     { 0xfefe, 0xefef, &pnic_attach },
+    { 0x8086, 0x100e, &e1000_attach },
     { 0x8086, 0x100f, &e1000_attach },
     { 0x8086, 0x107c, &e1000_attach },
     { 0x8086, 0x108c, &e1000_attach },
     { 0x8086, 0x109a, &e1000_attach },
+    { 0x8086, 0x1079, &e1000_attach },
     { 0, 0, 0 },
 };
 
