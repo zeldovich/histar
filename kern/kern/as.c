0d38e8ff3bfe0badbdec2a5f58c4c47943efd4b3
nickolai <nickolai@927ebb28-5305-0410-b3ca-c4b640df69b7>
2008-06-13 01:48:23 +0000

    careful with page_to_pageinfo now..
    git-svn-id: svn+ssh://market.scs.stanford.edu/home/u1/asbestos/repo/jos64/trunk@7525 927ebb28-5305-0410-b3ca-c4b640df69b7

--- kern/kern/as.c
+++ kern/kern/as.c
@@ -721,7 +721,7 @@ as_invert_mapped(const struct Address_space *as, void *addr,
 	int r = pgdir_walk(as->as_pgmap, addr, 0, &pte);
 	if (r >= 0 && *pte) {
 	    struct page_info *pi = page_to_pageinfo(pa2kva(PTE_ADDR(*pte)));
-	    if (pi->pi_hw_write_pin) {
+	    if (pi && pi->pi_hw_write_pin) {
 		/* page not shared by multiple segments */
 		*segp = pi->pi_seg;
 		*offsetp = (pi->pi_segpg * PGSIZE) + PGOFF(addr);
