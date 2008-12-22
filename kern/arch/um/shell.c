da31d21bd1bdaccb2de2286f39e8b6aca20e7fa4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2008-12-21 19:35:52 -0500

    add cd, ls commands to um kernel shell

--- kern/arch/um/shell.c
+++ kern/arch/um/shell.c
@@ -8,7 +8,10 @@
 #include <kern/prof.h>
 #include <kern/timer.h>
 #include <kern/handle.h>
+#include <kern/uinit.h>
+#include <kern/disk.h>
 #include <inc/error.h>
+#include <inc/string.h>
 
 struct cmd {
     const char *cmd;
@@ -33,15 +36,156 @@ gettok(char **ps)
 	s++;
     }
 
-    *ps = s + 1;
+    *ps = s;
     return *t ? t : 0;
 }
+
+static uint64_t cur_id;
+
+#define retry(expr)						\
+    ({								\
+	int __r;						\
+	for (;;) {						\
+	    __r = (expr);					\
+	    if (__r != -E_RESTART)				\
+		break;						\
+	    pstate_op_check();					\
+	    disk_poll(pstate_part->pd_dk);			\
+	}							\
+	__r;							\
+    })
+
+#define check(expr)						\
+    do {							\
+	int __c = retry(expr);					\
+	if (__c < 0) {						\
+	    printf("%s:%d: %s: %s\n", __FILE__, __LINE__,	\
+		   #expr, e2s(__c));				\
+	    return;						\
+	}							\
+    } while (0)
 
 static void
 cmd_load(char *p)
 {
+    static int loaded;
+
+    if (loaded++) {
+	printf("already loaded\n");
+	return;
+    }
+
     int r = pstate_load();
-    printf("pstate_load: %s\n", e2s(r));
+    printf("pstate_load: %d (%s)\n", r, e2s(r));
+
+    if (r > 0) {
+	cur_id = user_root_ct;
+	printf("changing current object to root (%"PRIu64")\n", cur_id);
+    }
+}
+
+static const char *
+type_to_string(uint32_t type)
+{
+    switch (type) {
+    case kobj_container:	return "container";
+    case kobj_thread:		return "thread";
+    case kobj_gate:		return "gate";
+    case kobj_segment:		return "segment";
+    case kobj_address_space:	return "as";
+    case kobj_device:		return "device";
+    case kobj_label:		return "label";
+    case kobj_dead:		return "dead";
+    default:			return "unknown";
+    }
+}
+
+static void
+cmd_ls(char *p)
+{
+    printf("object id: %"PRIu64"\n", cur_id);
+
+    const struct kobject *ko;
+    check(kobject_get(cur_id, &ko, kobj_any, iflow_none));
+
+    printf("name:      %s\n", ko->hdr.ko_name);
+    printf("type:      %s\n", type_to_string(ko->hdr.ko_type));
+
+    switch (ko->hdr.ko_type) {
+    case kobj_container:
+	for (uint32_t i = 0; i < container_nslots(&ko->ct); i++) {
+	    uint64_t id;
+	    int r = retry(container_get(&ko->ct, &id, i));
+	    if (r < 0)
+		continue;
+
+	    const struct kobject *cko;
+	    r = retry(kobject_get(id, &cko, kobj_any, iflow_none));
+	    if (r < 0)
+		continue;
+
+	    printf("  slot %d: %s %"PRIu64" (%s)\n",
+		   i, type_to_string(cko->hdr.ko_type),
+		   cko->hdr.ko_id, cko->hdr.ko_name);
+	}
+	break;
+
+    default:
+	break;
+    }
+}
+
+static void
+cmd_cd(char *p)
+{
+    char *arg = gettok(&p);
+    if (!arg) {
+	printf("usage: cd name-or-id\n");
+	return;
+    }
+
+    uint64_t id;
+    int r = strtou64(arg, 0, 10, &id);
+    if (r >= 0) {
+	cur_id = id;
+	return;
+    }
+
+    const struct kobject *ko;
+    check(kobject_get(cur_id, &ko, kobj_any, iflow_none));
+
+    if (!strcmp(arg, "/")) {
+	cur_id = user_root_ct;
+	return;
+    }
+
+    if (!strcmp(arg, "..")) {
+	cur_id = ko->hdr.ko_parent;
+	return;
+    }
+
+    if (ko->hdr.ko_type != kobj_container) {
+	printf("cannot cd inside a non-container\n");
+	return;
+    }
+
+    for (uint32_t i = 0; i < container_nslots(&ko->ct); i++) {
+	r = retry(container_get(&ko->ct, &id, i));
+	if (r < 0)
+	    continue;
+
+	const struct kobject *cko;
+	r = retry(kobject_get(id, &cko, kobj_any, iflow_none));
+	if (r < 0)
+	    continue;
+
+	if (!strcmp(arg, cko->hdr.ko_name)) {
+	    cur_id = cko->hdr.ko_id;
+	    return;
+	}
+    }
+
+    printf("%s: not found\n", arg);
 }
 
 static void
@@ -55,6 +199,8 @@ static struct cmd cmds[] = {
     { "cmds", &cmd_cmds },
     { "load", &cmd_load },
     { "bench", &cmd_bench },
+    { "ls", &cmd_ls },
+    { "cd", &cmd_cd },
 };
 
 static void
