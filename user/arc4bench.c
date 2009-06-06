63bdba20c22214b3bd76eebd52c8533b6278b722
Stephen M. Rumble <rumble@scs.stanford.edu>
2009-06-06 19:10:43 -0400

    Add a userspace CPU benchmark (arc4random), just to get some idea of how slow the ARM is.

---
+++ user/arc4bench.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <inc/syscall.h>
+
+typedef unsigned char u_int8_t;
+typedef unsigned char u_char;
+typedef unsigned int u_int32_t;
+typedef unsigned int u_int;
+
+struct arc4_stream {
+	u_int8_t i;
+	u_int8_t j;
+	u_int8_t s[256];
+};
+
+static int rs_initialized;
+static struct arc4_stream rs;
+
+static inline void arc4_init(struct arc4_stream *);
+static inline void arc4_addrandom(struct arc4_stream *, u_char *, int);
+static void arc4_stir(struct arc4_stream *);
+static inline u_int8_t arc4_getbyte(struct arc4_stream *);
+static inline u_int32_t arc4_getword(struct arc4_stream *);
+
+static inline void
+arc4_init(as)
+	struct arc4_stream *as;
+{
+	int     n;
+
+	for (n = 0; n < 256; n++)
+		as->s[n] = n;
+	as->i = 0;
+	as->j = 0;
+}
+
+static inline void
+arc4_addrandom(as, dat, datlen)
+	struct arc4_stream *as;
+	u_char *dat;
+	int     datlen;
+{
+	int     n;
+	u_int8_t si;
+
+	as->i--;
+	for (n = 0; n < 256; n++) {
+		as->i = (as->i + 1);
+		si = as->s[as->i];
+		as->j = (as->j + si + dat[n % datlen]);
+		as->s[as->i] = as->s[as->j];
+		as->s[as->j] = si;
+	}
+	as->j = as->i;
+}
+
+static void
+arc4_stir(as)
+	struct arc4_stream *as;
+{
+	struct {
+		u_int rnd[128];
+	}       rdat;
+	int	n;
+
+	/* fd < 0 or failed sysctl ?  Ah, what the heck. We'll just take
+	 * whatever was on the stack... */
+
+	arc4_addrandom(as, (void *) &rdat, sizeof(rdat));
+
+	/*
+	 * Throw away the first N words of output, as suggested in the
+	 * paper "Weaknesses in the Key Scheduling Algorithm of RC4"
+	 * by Fluher, Mantin, and Shamir.  (N = 256 in our case.)
+	 */
+	for (n = 0; n < 256 * 4; n++)
+		arc4_getbyte(as);
+}
+
+static inline u_int8_t
+arc4_getbyte(as)
+	struct arc4_stream *as;
+{
+	u_int8_t si, sj;
+
+	as->i = (as->i + 1);
+	si = as->s[as->i];
+	as->j = (as->j + si);
+	sj = as->s[as->j];
+	as->s[as->i] = sj;
+	as->s[as->j] = si;
+	return (as->s[(si + sj) & 0xff]);
+}
+
+static inline u_int32_t
+arc4_getword(as)
+	struct arc4_stream *as;
+{
+	u_int32_t val;
+	val = arc4_getbyte(as) << 24;
+	val |= arc4_getbyte(as) << 16;
+	val |= arc4_getbyte(as) << 8;
+	val |= arc4_getbyte(as);
+	return val;
+}
+
+static void
+arc4random_stir()
+{
+	if (!rs_initialized) {
+		arc4_init(&rs);
+		rs_initialized = 1;
+	}
+	arc4_stir(&rs);
+}
+
+static u_int32_t
+arc4random()
+{
+	if (!rs_initialized)
+		arc4random_stir();
+	return arc4_getword(&rs);
+}
+
+int
+main(int argc, char **argv)
+{
+	uint64_t nsec0 = sys_clock_nsec();
+	const int iter = 100000000;
+	int     i;
+
+	for (i = 0; i < iter; i++)
+		arc4random();
+
+	uint64_t nsec1 = sys_clock_nsec();
+	printf("%" PRIu64 " total nanoseconds\n", nsec1 - nsec0);
+	printf("%" PRIu64 " nanoseconds / call\n", (nsec1 - nsec0) / iter);
+
+	return (0);
+}
