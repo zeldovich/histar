4f6adcba5006d80f87bf4a25fb207dc2be77b6f5
Stephen M. Rumble <rumble@scs.stanford.edu>
2009-07-21 21:08:17 -0400

    Junk to make the rpcrouter stuff compile.

--- pkg/rild/support/misc.h
+++ pkg/rild/support/misc.h
@@ -1,6 +1,10 @@
 #ifndef _MISC_H_
 #define _MISC_H_
 
+#include <pthread.h>
+#include <inttypes.h>
+#include <assert.h>
+
 #if 1
 #define LOGD(fmt,args...)	fprintf(stderr, fmt, ##args)
 #define LOGE(fmt,args...)	fprintf(stderr, fmt, ##args)
@@ -23,5 +27,30 @@ typedef int status_t;
 #define NO_MEMORY	-3
 
 #define OS_PATH_SEPARATOR	'/'
+
+extern "C" int pthread_cond_timeout_np(pthread_cond_t *, pthread_mutex_t *, unsigned);
+
+static inline uint32_t
+be32_to_cpu(uint32_t x)
+{
+	return (((x & 0xff000000) >> 24) |
+		((x & 0x00ff0000) >>  8) |
+		((x & 0x0000ff00) <<  8) |
+		((x & 0x000000ff) << 24));
+}
+
+static inline uint32_t
+cpu_to_be32(uint32_t x)
+{
+	return (be32_to_cpu(x));
+}
+
+#define ERR_PTR(_x)	((void *)(intptr_t)(_x))
+#define IS_ERR(_x)	((intptr_t)(_x) < 0)
+#define BUG_ON(_x)	assert(!(_x))
+
+#define MKDEV(_x, _y)	(((_x) << 16) | (_y))
+#define MAJOR(_x)	(((_x) >> 16) & 0xffff)
+#define MINOR(_x)	((_x) & 0xffff)
 
 #endif /* !_MISC_H_ */
