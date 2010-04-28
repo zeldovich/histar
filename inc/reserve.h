7ef081c218bedbf5eddd1e0293171c38291f046c
Stephen M. Rumble <rumble@scs.stanford.edu>
2010-04-28 19:26:26 -0400

    Note units in mJ.

--- inc/reserve.h
+++ inc/reserve.h
@@ -2,9 +2,9 @@
 #define JOS_INC_CAPACITOR_H
 
 struct ReserveInfo {
-    uint64_t rs_level;
-    uint64_t rs_consumed;
-    uint64_t rs_decayed;
+    uint64_t rs_level;		// mJ
+    uint64_t rs_consumed;	// mJ
+    uint64_t rs_decayed;	// mJ
 };
 
 #endif
