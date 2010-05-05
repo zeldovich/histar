756b664abc463a8825b6d44f950c205ac018c438
Stephen M. Rumble <rumble@scs.stanford.edu>
2010-05-04 21:27:13 -0400

    Clean up: use getopt, execvp. Default to using the current thread's reserve, but provide -r to use the root. Previously every thread got the root reserve, even in recursive energywraps.
    Printing reserve info is set using -p and stops on program exit.
    Don't barf unnecessarily if our exec'd program has no args.

--- user/energywrap.cc
+++ user/energywrap.cc
@@ -18,43 +18,84 @@ extern "C" {
 #include <inc/labelutil.hh>
 #include <inc/spawn.hh>
 
-static int printing_stats = 0;
+static int print_stats = 0;
+static int throttle = 0;
+static int use_rootrs = 0;
 
 void __attribute__((noreturn))
 sigchld_handler(int i)
 {
-    if (printing_stats)
+    if (print_stats)
 	sys_toggle_debug(1);
     exit(0);
 }
 
+static void usage(void) __attribute__((noreturn));
+
+static void
+usage()
+{
+    printf("usage: [-p] [-r] main_uW prog_path [prog_args [...]]\n");
+    exit(1);
+}
+
 int
-main(int ac, const char **av)
+main(int ac, char **av)
 try
 {
-    if (ac < 5) {
-	printf("usage: print_stats main_uW prog_path prog_args...\n");
-	return -1;
+    extern int optind;
+    int ch;
+
+    if (ac < 3)
+	usage();
+
+    while ((ch = getopt(ac, av, "pr")) != -1) {
+	switch (ch) {
+	case 'p':
+	    print_stats = 1;
+	    break;
+	case 'r':
+	    use_rootrs = 1;
+	    break;
+	default:
+	    usage();
+	}
     }
-    const int print_stats = atoi(av[1]);
-    const int throttle = atoi(av[2]);
+    ac -= optind;
+    av += optind;
 
-    char *args[ac - 3 + 1];
-    for (int i = 0; i < ac - 3; i++)
-	args[i] = (char *)av[3 + i];
-    args[ac - 3] = NULL;
+    if (ac < 1)
+	usage();
 
-    printf("running app with %ld uW\n", throttle);
+    throttle = atoi(av[0]);
+    ac--;
+    av++;
+
+    char *args[ac + 1];
+    for (int i = 0; i < ac; i++)
+	args[i] = (char *)av[i];
+    args[ac] = NULL;
+
+    printf("running app with %ld uW on %s reserve\n", throttle,
+	(use_rootrs) ? "the root" : "my");
     printf("process container: %ld\n", start_env->proc_container);
 
-    // TODO WANT TO CHANGE THIS TO sys_self_get_active_reserve
-    // find the root reserve
-    int64_t rsid = container_find(start_env->root_container, kobj_reserve, "root_reserve");
-    if (rsid < 0) {
-	perror("couldn't find root_reserve");
-	return rsid;
+    struct cobj_ref srcrs;
+    if (use_rootrs) {
+	// find the root reserve
+	int64_t rsid = container_find(start_env->root_container, kobj_reserve,
+	    "root_reserve");
+	if (rsid < 0) {
+	    perror("couldn't find root_reserve");
+	    return rsid;
+	}
+	srcrs = COBJ(start_env->root_container, rsid);
+    } else {
+	if (sys_self_get_active_reserve(&srcrs) < 0) {
+	    perror("failed to get my reserve");
+	    return -1;
+	}
     }
-    struct cobj_ref rootrs = COBJ(start_env->root_container, rsid);
 
     // ACTUALLY - HAVE THE CHILD PLACE THE RESERVE IN ITS OWN CONTAINER IN SOME WAY
     // === create main cp ===
@@ -75,12 +116,14 @@ try
     //label l;
     //thread_cur_label(&l);
     int64_t r;
-    error_check(r = sys_reserve_create(ctid, l.to_ulabel(), "wrapreserve"));
+    char name[1000];
+    sprintf(name, "wrapreserve_%s_%d\n", args[0], throttle);
+    error_check(r = sys_reserve_create(ctid, l.to_ulabel(), name));
     printf("New reserve is at %lu\n", r);
     cobj_ref rs0 = COBJ(ctid, r);
 
-    // create a limit between the root and rs0
-    error_check(r = sys_limit_create(ctid, rootrs, rs0, l.to_ulabel(), "wraplimit"));
+    // create a limit between the source and rs0
+    error_check(r = sys_limit_create(ctid, srcrs, rs0, l.to_ulabel(), "wraplimit"));
     printf("New limit is at %lu\n", r);
     cobj_ref lm0 = COBJ(ctid, r);
     error_check(sys_limit_set_rate(lm0, LIMIT_TYPE_CONST, throttle));
@@ -95,14 +138,13 @@ try
 	// child
 	signal(SIGCHLD, SIG_DFL);
 	error_check(sys_self_set_active_reserve(rs0));
-	error_check(execv(args[0], args));
+	error_check(execvp(args[0], args));
 	return 0;
     }
 
-    if (print_stats) {
-	printing_stats = 1;
+    if (print_stats)
 	sys_toggle_debug(1);
-    }
+
     while (1) {
 	usleep(1000000);
     }
